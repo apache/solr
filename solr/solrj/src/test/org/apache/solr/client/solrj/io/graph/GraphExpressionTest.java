fd5540851019a60cdb177397b9146d6635f7cbcd
Erik Hatcher <erik.hatcher@lucidworks.com>
2021-03-18 13:59:56 -0400

    remove now unused imports

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/graph/GraphExpressionTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/graph/GraphExpressionTest.java
@@ -23,10 +23,8 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.apache.lucene.util.LuceneTestCase;
