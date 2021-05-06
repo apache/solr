b3ffff450bec10e4fea4690c61b3bec8ea8f2566
Andrzej Bialecki <ab@apache.org>
2021-05-06 11:27:27 +0200

    SOLR-15300: Changed the thresholds, updated the docs.

--- solr/core/src/java/org/apache/solr/handler/admin/ClusterStatus.java
+++ solr/core/src/java/org/apache/solr/handler/admin/ClusterStatus.java
@@ -59,10 +59,8 @@ public class ClusterStatus {
     /** No leader or most replicas down. */
     RED;
 
-    // somewhat higher than 2/3
-    public static final float ORANGE_LEVEL = 0.67f;
-    // somewhat higher than 1/3
-    public static final float RED_LEVEL = 0.34f;
+    public static final float ORANGE_LEVEL = 0.5f;
+    public static final float RED_LEVEL = 0.1f;
 
     public static Health calcShardHealth(float fractionReplicasUp, boolean hasLeader) {
       if (hasLeader) {
