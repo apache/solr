2ee8779246f0e96de68123aa5af2501c6a671c7f
tomglk <>
2021-05-12 16:43:32 +0200

    [SOLR-12697] add DocValuesFieldValueFeatureScorer to read docValues for ltr feature calculation

--- solr/contrib/ltr/src/java/org/apache/solr/ltr/feature/FieldValueFeature.java
+++ solr/contrib/ltr/src/java/org/apache/solr/ltr/feature/FieldValueFeature.java
@@ -23,24 +23,35 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DocValues;
+import org.apache.lucene.index.DocValuesType;
+import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.index.SortedDocValues;
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.util.BytesRef;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.schema.BoolField;
+import org.apache.solr.schema.FieldType;
+import org.apache.solr.schema.NumberType;
+import org.apache.solr.schema.SchemaField;
+import org.apache.solr.search.SolrIndexSearcher;
 
 /**
  * This feature returns the value of a field in the current document
  * Example configuration:
  * <pre>{
-  "name":  "rawHits",
-  "class": "org.apache.solr.ltr.feature.FieldValueFeature",
-  "params": {
-      "field": "hits"
-  }
-}</pre>
+ * "name":  "rawHits",
+ * "class": "org.apache.solr.ltr.feature.FieldValueFeature",
+ * "params": {
+ * "field": "hits",
+ * "defaultValue": -1
+ * }
+ * }</pre>
  */
 public class FieldValueFeature extends Feature {
 
@@ -83,24 +94,42 @@ public class FieldValueFeature extends Feature {
   }
 
   public class FieldValueFeatureWeight extends FeatureWeight {
+    private final SchemaField schemaField;
 
     public FieldValueFeatureWeight(IndexSearcher searcher,
         SolrQueryRequest request, Query originalQuery, Map<String,String[]> efi) {
       super(FieldValueFeature.this, searcher, request, originalQuery, efi);
+      if (searcher instanceof SolrIndexSearcher) {
+        schemaField = ((SolrIndexSearcher) searcher).getSchema().getFieldOrNull(field);
+      } else {
+        schemaField = null;
+      }
     }
 
+    /**
+     * Return a FeatureScorer that uses docValues or storedFields if no docValues are present
+     * @param context the segment this FeatureScorer is working with
+     * @return FeatureScorer for the current segment and field
+     * @throws IOException as defined by abstract class Feature
+     */
     @Override
     public FeatureScorer scorer(LeafReaderContext context) throws IOException {
+      // always prefer docValues
+      if (schemaField != null && schemaField.hasDocValues()) {
+        return new DocValuesFieldValueFeatureScorer(this, context,
+            DocIdSetIterator.all(DocIdSetIterator.NO_MORE_DOCS), schemaField);
+      }
       return new FieldValueFeatureScorer(this, context,
           DocIdSetIterator.all(DocIdSetIterator.NO_MORE_DOCS));
     }
 
+    /**
+     * A FeatureScorer that reads the stored value for a field
+     */
     public class FieldValueFeatureScorer extends FeatureScorer {
+      LeafReaderContext context;
 
-      LeafReaderContext context = null;
-
-      public FieldValueFeatureScorer(FeatureWeight weight,
-          LeafReaderContext context, DocIdSetIterator itr) {
+      public FieldValueFeatureScorer(FeatureWeight weight, LeafReaderContext context, DocIdSetIterator itr) {
         super(weight, itr);
         this.context = context;
       }
@@ -139,6 +168,103 @@ public class FieldValueFeature extends Feature {
                   + name, e);
         }
         return getDefaultValue();
+      }
+
+      @Override
+      public float getMaxScore(int upTo) throws IOException {
+        return Float.POSITIVE_INFINITY;
+      }
+    }
+
+    /**
+     * A FeatureScorer that reads the docValues for a field
+     */
+    public class DocValuesFieldValueFeatureScorer extends FeatureWeight.FeatureScorer {
+      final LeafReaderContext context;
+      final DocIdSetIterator docValues;
+      final FieldType schemaFieldType;
+      DocValuesType docValuesType = DocValuesType.NONE;
+
+      public DocValuesFieldValueFeatureScorer(final FeatureWeight weight, final LeafReaderContext context,
+                                              final DocIdSetIterator itr, final SchemaField schemaField) {
+        super(weight, itr);
+        this.context = context;
+        schemaFieldType = schemaField.getType();
+
+        try {
+          FieldInfo fieldInfo = context.reader().getFieldInfos().fieldInfo(field);
+          // if fieldInfo is null, just use NONE-Type. This causes no problems, because we won't call score() anyway
+          docValuesType = fieldInfo != null ? fieldInfo.getDocValuesType() : DocValuesType.NONE;
+          switch (docValuesType) {
+            case NUMERIC:
+              docValues = DocValues.getNumeric(context.reader(), field);
+              break;
+            case SORTED:
+              docValues = DocValues.getSorted(context.reader(), field);
+              break;
+            case BINARY:
+              docValues = DocValues.getBinary(context.reader(), field);
+              break;
+            case SORTED_NUMERIC:
+            case SORTED_SET:
+            case NONE:
+            default:
+              docValues = null;
+          }
+        } catch (IOException e) {
+          throw new IllegalArgumentException("Could not read docValues for field " + field + " with docValuesType "
+              + docValuesType.name());
+        }
+      }
+
+      @Override
+      public float score() throws IOException {
+        if (docValues != null && docValues.advance(itr.docID()) < DocIdSetIterator.NO_MORE_DOCS) {
+          switch (docValuesType) {
+            case NUMERIC:
+              if (NumberType.FLOAT.equals(schemaFieldType.getNumberType())) {
+                // convert float value that was stored as long back to float
+                return Float.intBitsToFloat((int) ((NumericDocValues) docValues).longValue());
+              } else if (NumberType.DOUBLE.equals(schemaFieldType.getNumberType())) {
+                // handle double value conversion
+                return (float) Double.longBitsToDouble(((NumericDocValues) docValues).longValue());
+              }
+              // just take the long value
+              return ((NumericDocValues) docValues).longValue();
+            case SORTED:
+              int ord = ((SortedDocValues) docValues).ordValue();
+              // try to interpret bytesRef either as number string or as true / false token
+              return handleBytesRef(((SortedDocValues) docValues).lookupOrd(ord));
+            case BINARY:
+            case SORTED_SET:
+            case SORTED_NUMERIC:
+            case NONE:
+            default:
+              throw new IllegalArgumentException("Doc values type " + docValuesType.name() + " of field " + field
+                  + " is not supported!");
+          }
+        }
+        return FieldValueFeature.this.getDefaultValue();
+      }
+
+      private float handleBytesRef(BytesRef bytesRef) {
+        String string = bytesRef.utf8ToString();
+        if (string.length() == 1
+            && (string.charAt(0) == BoolField.TRUE_TOKEN[0] || string.charAt(0) == BoolField.FALSE_TOKEN[0])) {
+          // boolean values in the index are encoded with a single char contained in TRUE_TOKEN or FALSE_TOKEN
+          // (see BoolField)
+          if (string.charAt(0) == BoolField.TRUE_TOKEN[0]) {
+            return 1f;
+          } else {
+            return 0f;
+          }
+        } else {
+          try {
+            return Float.parseFloat(string);
+          } catch (NumberFormatException ex) {
+            throw new FeatureException("Cannot parse value " + string + " of field " + schemaField.getName() + " to float.");
+          }
+        }
       }
 
       @Override
--- solr/contrib/ltr/src/test/org/apache/solr/ltr/TestLTROnSolrCloud.java
+++ solr/contrib/ltr/src/test/org/apache/solr/ltr/TestLTROnSolrCloud.java
@@ -28,6 +28,7 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.cloud.MiniSolrCloudCluster;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.cloud.ZkStateReader;
+import org.apache.solr.ltr.feature.FieldValueFeature;
 import org.apache.solr.ltr.feature.OriginalScoreFeature;
 import org.apache.solr.ltr.feature.SolrFeature;
 import org.apache.solr.ltr.feature.ValueFeature;
@@ -107,21 +108,21 @@ public class TestLTROnSolrCloud extends TestRerankBase {
     final Float original_result7_score = (Float)queryResponse.getResults().get(7).get("score");
 
     final String result0_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS","64.0", "c3","2.0", "original","0.0");
+        "powpularityS","64.0", "c3","2.0", "original","0.0", "dvIntFieldFeature","8.0","dvLongFieldFeature","8.0","dvFloatFieldFeature","0.8","dvDoubleFieldFeature","0.8","dvStrNumFieldFeature","8.0","dvStrBoolFieldFeature","1.0");
     final String result1_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS","49.0", "c3","2.0", "original","1.0");
+        "powpularityS","49.0", "c3","2.0", "original","1.0", "dvIntFieldFeature","7.0","dvLongFieldFeature","7.0","dvFloatFieldFeature","0.7","dvDoubleFieldFeature","0.7","dvStrNumFieldFeature","7.0","dvStrBoolFieldFeature","0.0");
     final String result2_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS","36.0", "c3","2.0", "original","2.0");
+        "powpularityS","36.0", "c3","2.0", "original","2.0", "dvIntFieldFeature","6.0","dvLongFieldFeature","6.0","dvFloatFieldFeature","0.6","dvDoubleFieldFeature","0.6","dvStrNumFieldFeature","6.0","dvStrBoolFieldFeature","1.0");
     final String result3_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS","25.0", "c3","2.0", "original","3.0");
+        "powpularityS","25.0", "c3","2.0", "original","3.0", "dvIntFieldFeature","5.0","dvLongFieldFeature","5.0","dvFloatFieldFeature","0.5","dvDoubleFieldFeature","0.5","dvStrNumFieldFeature","5.0","dvStrBoolFieldFeature","0.0");
     final String result4_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS","16.0", "c3","2.0", "original","4.0");
+        "powpularityS","16.0", "c3","2.0", "original","4.0", "dvIntFieldFeature","4.0","dvLongFieldFeature","4.0","dvFloatFieldFeature","0.4","dvDoubleFieldFeature","0.4","dvStrNumFieldFeature","4.0","dvStrBoolFieldFeature","1.0");
     final String result5_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS", "9.0", "c3","2.0", "original","5.0");
+        "powpularityS", "9.0", "c3","2.0", "original","5.0", "dvIntFieldFeature","3.0","dvLongFieldFeature","3.0","dvFloatFieldFeature","0.3","dvDoubleFieldFeature","0.3","dvStrNumFieldFeature","3.0","dvStrBoolFieldFeature","0.0");
     final String result6_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS", "4.0", "c3","2.0", "original","6.0");
+        "powpularityS", "4.0", "c3","2.0", "original","6.0", "dvIntFieldFeature","2.0","dvLongFieldFeature","2.0","dvFloatFieldFeature","0.2","dvDoubleFieldFeature","0.2","dvStrNumFieldFeature","2.0","dvStrBoolFieldFeature","1.0");
     final String result7_features= FeatureLoggerTestUtils.toFeatureVector(
-        "powpularityS", "1.0", "c3","2.0", "original","7.0");
+        "powpularityS", "1.0", "c3","2.0", "original","7.0", "dvIntFieldFeature","-1.0","dvLongFieldFeature","-2.0","dvFloatFieldFeature","-3.0","dvDoubleFieldFeature","-4.0","dvStrNumFieldFeature","-5.0","dvStrBoolFieldFeature","0.0");
 
 
     // Test feature vectors returned (without re-ranking)
@@ -240,24 +241,40 @@ public class TestLTROnSolrCloud extends TestRerankBase {
     doc.setField("title", title);
     doc.setField("description", description);
     doc.setField("popularity", popularity);
+    if(popularity != 1) {
+      // check that empty values will be read as default
+      doc.setField("dvIntField", popularity);
+      doc.setField("dvLongField", popularity);
+      doc.setField("dvFloatField", ((float) popularity) / 10);
+      doc.setField("dvDoubleField", ((double) popularity) / 10);
+      doc.setField("dvStrNumField", popularity);
+      doc.setField("dvStrBoolField", popularity % 2 == 0 ? "T" : "F");
+    }
     solrCluster.getSolrClient().add(collection, doc);
   }
 
   private void indexDocuments(final String collection)
        throws Exception {
     final int collectionSize = 8;
-    for (int docId = 1; docId <= collectionSize;  docId++) {
+    // put documents in reversed order to check that advanceExact is working correctly
+    for (int docId = collectionSize; docId >= 1;  docId--) {
       final int popularity = docId;
       indexDocument(collection, String.valueOf(docId), "a1", "bloom", popularity);
+      if(docId == collectionSize / 2) {
+        // commit in the middle in order to check that everything works fine for multi-segment case
+        solrCluster.getSolrClient().commit(collection);
+      }
     }
-    solrCluster.getSolrClient().commit(collection);
+    solrCluster.getSolrClient().commit(collection, true, true);
   }
-
 
   private void loadModelsAndFeatures() throws Exception {
     final String featureStore = "test";
-    final String[] featureNames = new String[] {"powpularityS","c3", "original"};
-    final String jsonModelParams = "{\"weights\":{\"powpularityS\":1.0,\"c3\":1.0,\"original\":0.1}}";
+    final String[] featureNames = new String[]{"powpularityS", "c3", "original", "dvIntFieldFeature",
+        "dvLongFieldFeature", "dvFloatFieldFeature", "dvDoubleFieldFeature", "dvStrNumFieldFeature", "dvStrBoolFieldFeature"};
+    final String jsonModelParams = "{\"weights\":{\"powpularityS\":1.0,\"c3\":1.0,\"original\":0.1," +
+        "\"dvIntFieldFeature\":0.1,\"dvLongFieldFeature\":0.1," +
+        "\"dvFloatFieldFeature\":0.1,\"dvDoubleFieldFeature\":0.1,\"dvStrNumFieldFeature\":0.1,\"dvStrBoolFieldFeature\":0.1}}";
 
     loadFeature(
             featureNames[0],
@@ -276,6 +293,42 @@ public class TestLTROnSolrCloud extends TestRerankBase {
             OriginalScoreFeature.class.getName(),
             featureStore,
             null
+    );
+    loadFeature(
+        featureNames[3],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvIntField\"}"
+    );
+    loadFeature(
+        featureNames[4],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvLongField\"}"
+    );
+    loadFeature(
+        featureNames[5],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvFloatField\"}"
+    );
+    loadFeature(
+        featureNames[6],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvDoubleField\",\"defaultValue\":-4.0}"
+    );
+    loadFeature(
+        featureNames[7],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvStrNumField\",\"defaultValue\":-5}"
+    );
+    loadFeature(
+        featureNames[8],
+        FieldValueFeature.class.getName(),
+        featureStore,
+        "{\"field\":\"dvStrBoolField\"}"
     );
 
     loadModel(
