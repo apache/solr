a3bbc177c4d81f4c7fa1693a5647ae33374bc385
Timothy Potter <thelabdude@gmail.com>
2021-05-20 07:57:33 -0600

    Remove stray comment after refactor

--- solr/core/src/test/org/apache/solr/cloud/TestPullReplica.java
+++ solr/core/src/test/org/apache/solr/cloud/TestPullReplica.java
@@ -263,10 +263,9 @@ public class TestPullReplica extends SolrCloudTestCase {
   private List<Replica> restartPullReplica(DocCollection docCollection, int numPullReplicas) throws Exception {
     Slice s = docCollection.getSlices().iterator().next();
     List<Replica> pullReplicas = s.getReplicas(EnumSet.of(Replica.Type.PULL));
-    // make sure a PULL replica recovers this first doc after a restart
-    JettySolrRunner leaderJetty = cluster.getReplicaJetty(s.getLeader());
 
     // find a node with a PULL replica that's not hosting the leader
+    JettySolrRunner leaderJetty = cluster.getReplicaJetty(s.getLeader());
     JettySolrRunner replicaJetty = null;
     for (Replica r : pullReplicas) {
       JettySolrRunner jetty = cluster.getReplicaJetty(r);
