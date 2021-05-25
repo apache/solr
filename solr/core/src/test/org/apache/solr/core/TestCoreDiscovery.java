b274499ec3e92e09e6169c02eaa836d40892584c
Bruno Roustant <bruno.roustant@gmail.com>
2021-05-25 15:42:21 +0200

    CHANGES + unused import

--- solr/core/src/test/org/apache/solr/core/TestCoreDiscovery.java
+++ solr/core/src/test/org/apache/solr/core/TestCoreDiscovery.java
@@ -26,7 +26,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
