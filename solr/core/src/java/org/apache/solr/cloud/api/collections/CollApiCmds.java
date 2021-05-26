39cf2eee148c18d1c9abada4a4788adce1690a60
Nazerke Seidan <nseidan@salesforce.com>
2021-05-26 18:54:38 +0200

    SOLR-15311: Filter ASYNC key in during collection update

--- solr/core/src/java/org/apache/solr/cloud/api/collections/CollApiCmds.java
+++ solr/core/src/java/org/apache/solr/cloud/api/collections/CollApiCmds.java
@@ -38,6 +38,7 @@ import org.apache.solr.common.cloud.ZkNodeProps;
 import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.params.CollectionAdminParams;
 import org.apache.solr.common.params.CollectionParams;
+import org.apache.solr.common.params.CommonAdminParams;
 import org.apache.solr.common.params.CoreAdminParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.NamedList;
@@ -324,6 +325,7 @@ public class CollApiCmds {
 
             if (!updateKey.equals(ZkStateReader.COLLECTION_PROP)
                 && !updateKey.equals(Overseer.QUEUE_OPERATION)
+                && !updateKey.equals(CommonAdminParams.ASYNC)
                 && updateEntry.getValue() != null // handled below in a separate conditional
                 && !updateEntry.getValue().equals(c.get(updateKey))) {
               return false;
