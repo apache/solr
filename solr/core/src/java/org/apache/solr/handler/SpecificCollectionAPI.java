a736cb28d353e048ec0a65653570e3efbfc1ba9c
Jason Gerlowski <gerlowskija@apache.org>
2021-05-28 15:08:47 -0400

    Transpose remaining /v2/c/<collection> APIs
    This commit transfers the remaining /v2/c/<collection> APIs over to
    POJO-based implementations.
    
    Remaining work on this PR includes:
    - tests for the v2->v1 mapping logic in SpecificCollectionAPI
    - split SpecificCollectionAPI up into individual classes for each API
      per David's preference.

--- solr/core/src/java/org/apache/solr/handler/SpecificCollectionAPI.java
+++ solr/core/src/java/org/apache/solr/handler/SpecificCollectionAPI.java
@@ -1,14 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.handler;
 
 import org.apache.solr.api.Command;
 import org.apache.solr.api.EndPoint;
 import org.apache.solr.api.PayloadObj;
+import org.apache.solr.client.solrj.request.beans.AddReplicaPropertyPayload;
 import org.apache.solr.client.solrj.request.beans.BackupCollectionPayload;
 import org.apache.solr.client.solrj.request.beans.BalanceShardUniquePayload;
+import org.apache.solr.client.solrj.request.beans.DeleteReplicaPropertyPayload;
 import org.apache.solr.client.solrj.request.beans.MigrateDocsPayload;
 import org.apache.solr.client.solrj.request.beans.ModifyCollectionPayload;
 import org.apache.solr.client.solrj.request.beans.MoveReplicaPayload;
+import org.apache.solr.client.solrj.request.beans.RebalanceLeadersPayload;
 import org.apache.solr.client.solrj.request.beans.ReloadCollectionPayload;
+import org.apache.solr.client.solrj.request.beans.SetCollectionPropertyPayload;
 import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.params.CollectionParams;
 import org.apache.solr.handler.admin.CollectionsHandler;
@@ -40,6 +60,10 @@ public class SpecificCollectionAPI {
   private static final String V2_MOVE_REPLICA_CMD = "move-replica";
   private static final String V2_MIGRATE_DOCS_CMD = "migrate-docs";
   private static final String V2_BALANCE_SHARD_UNIQUE_CMD = "balance-shard-unique";
+  private static final String V2_REBALANCE_LEADERS_CMD = "rebalance-leaders";
+  private static final String V2_ADD_REPLICA_PROPERTY_CMD = "add-replica-property";
+  private static final String V2_DELETE_REPLICA_PROPERTY_CMD = "delete-replica-property";
+  private static final String V2_SET_COLLECTION_PROPERTY_CMD = "set-collection-property";
 
   public final SpecificCollectionCommands specificCollectionCommands = new SpecificCollectionCommands();
   private final CollectionsHandler collectionsHandler;
@@ -134,6 +158,53 @@ public class SpecificCollectionAPI {
       final Map<String, Object> v1Params = v2Body.toMap(new HashMap<>());
       v1Params.put(ACTION, CollectionParams.CollectionAction.BALANCESHARDUNIQUE.toLower());
       v1Params.put(COLLECTION, obj.getRequest().getPathTemplateValues().get(COLLECTION));
+
+      collectionsHandler.handleRequestBody(wrapParams(obj.getRequest(), v1Params), obj.getResponse());
+    }
+
+    @Command(name = V2_REBALANCE_LEADERS_CMD)
+    public void rebalanceLeaders(PayloadObj<RebalanceLeadersPayload> obj) throws Exception {
+      final RebalanceLeadersPayload v2Body = obj.get();
+      final Map<String, Object> v1Params = v2Body.toMap(new HashMap<>());
+      v1Params.put(ACTION, CollectionParams.CollectionAction.REBALANCELEADERS.toLower());
+      v1Params.put(COLLECTION, obj.getRequest().getPathTemplateValues().get(COLLECTION));
+
+      collectionsHandler.handleRequestBody(wrapParams(obj.getRequest(), v1Params), obj.getResponse());
+    }
+
+    @Command(name = V2_ADD_REPLICA_PROPERTY_CMD)
+    public void addReplicaProperty(PayloadObj<AddReplicaPropertyPayload> obj) throws Exception {
+      final AddReplicaPropertyPayload v2Body = obj.get();
+      final Map<String, Object> v1Params = v2Body.toMap(new HashMap<>());
+      v1Params.put(ACTION, CollectionParams.CollectionAction.ADDREPLICAPROP.toLower());
+      v1Params.put(COLLECTION, obj.getRequest().getPathTemplateValues().get(COLLECTION));
+      v1Params.put("property", v1Params.remove("name"));
+      v1Params.put("property.value", v1Params.remove("value"));
+
+      collectionsHandler.handleRequestBody(wrapParams(obj.getRequest(), v1Params), obj.getResponse());
+    }
+
+    @Command(name = V2_DELETE_REPLICA_PROPERTY_CMD)
+    public void deleteReplicaProperty(PayloadObj<DeleteReplicaPropertyPayload> obj) throws Exception {
+      final DeleteReplicaPropertyPayload v2Body = obj.get();
+      final Map<String, Object> v1Params = v2Body.toMap(new HashMap<>());
+      v1Params.put(ACTION, CollectionParams.CollectionAction.DELETEREPLICAPROP.toLower());
+      v1Params.put(COLLECTION, obj.getRequest().getPathTemplateValues().get(COLLECTION));
+
+      collectionsHandler.handleRequestBody(wrapParams(obj.getRequest(), v1Params), obj.getResponse());
+    }
+
+    @Command(name = V2_SET_COLLECTION_PROPERTY_CMD)
+    public void setCollectionProperty(PayloadObj<SetCollectionPropertyPayload> obj) throws Exception {
+      final SetCollectionPropertyPayload v2Body = obj.get();
+      final Map<String, Object> v1Params = v2Body.toMap(new HashMap<>());
+
+      v1Params.put("propertyName", v1Params.remove("name"));
+      if (v2Body.value != null) {
+        v1Params.put("propertyValue", v2Body.value);
+      }
+      v1Params.put(ACTION, CollectionParams.CollectionAction.COLLECTIONPROP.toLower());
+      v1Params.put(NAME, obj.getRequest().getPathTemplateValues().get(COLLECTION));
 
       collectionsHandler.handleRequestBody(wrapParams(obj.getRequest(), v1Params), obj.getResponse());
     }
