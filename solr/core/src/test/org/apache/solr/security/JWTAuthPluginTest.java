3de77bea1c4d2f5226d9a1776992c218b5f1cdb4
Jan Høydahl <jan.git@cominvent.com>
2021-06-18 10:46:54 +0200

    Redesign integration test, with cluster in local scope variable per test

--- solr/core/src/test/org/apache/solr/security/JWTAuthPluginTest.java
+++ solr/core/src/test/org/apache/solr/security/JWTAuthPluginTest.java
@@ -549,4 +549,11 @@ public class JWTAuthPluginTest extends SolrTestCaseJ4 {
               "-----END CERTIFICATE-----\n", StandardCharsets.UTF_8));
     });
   }
+
+  @Test
+  public void extractCertificate() throws IOException {
+    Path pemFilePath = SolrTestCaseJ4.TEST_PATH().resolve("security").resolve("jwt_plugin_idp_cert.pem");
+    String cert = JWTAuthPluginIntegrationTest.extractCertificateFromPem(pemFilePath);
+    assertEquals(2, CryptoKeys.parseX509Certs(IOUtils.toInputStream(cert, StandardCharsets.UTF_8)).size());
+  }
 }
