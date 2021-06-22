2b1056e5bad39937e2e7e0348f08287d0999ec71
David Smiley <dsmiley@apache.org>
2021-06-22 16:21:51 -0400

    SOLR-15185: HashQParser, make hashCode deterministic (#175)
    Fixes an issue introduced in this JIRA issue; not a regression because wasn't released.
    StreamDecoratorTest.testParallelPriorityStream fails maybe 12% (?) of the time or so for main.
    Also reported in SOLR-15406

--- solr/core/src/java/org/apache/solr/search/HashQParserPlugin.java
+++ solr/core/src/java/org/apache/solr/search/HashQParserPlugin.java
@@ -34,8 +34,10 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.LongValues;
 import org.apache.lucene.search.LongValuesSource;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.util.BytesRef;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.Hash;
 import org.apache.solr.request.SolrQueryRequest;
 
 /**
@@ -114,7 +116,12 @@ public class HashQParserPlugin extends QParserPlugin {
             @Override
             public long longValue() throws IOException {
               //TODO: maybe cache hashCode if same ord as prev doc to save lookupOrd?
-              return atDoc ? values.lookupOrd(values.ordValue()).hashCode() : 0;
+              return atDoc ? hashCode(values.lookupOrd(values.ordValue())) : 0;
+            }
+
+            private int hashCode(BytesRef bytesRef) {
+              // Use deterministic hashCode.  BytesRef.hashCode() varies!
+              return Hash.murmurhash3_x86_32(bytesRef.bytes, bytesRef.offset, bytesRef.length, 0);
             }
           };
           continue;
