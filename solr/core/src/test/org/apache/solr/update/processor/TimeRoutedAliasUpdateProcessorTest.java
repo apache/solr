8519941a0934a6437f747c9364f56f0aad14c819
Christine Poerschke <cpoerschke@apache.org>
2021-09-03 15:26:48 +0100

    Update TimeRoutedAliasUpdateProcessorTest.java

--- solr/core/src/test/org/apache/solr/update/processor/TimeRoutedAliasUpdateProcessorTest.java
+++ solr/core/src/test/org/apache/solr/update/processor/TimeRoutedAliasUpdateProcessorTest.java
@@ -30,6 +30,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
