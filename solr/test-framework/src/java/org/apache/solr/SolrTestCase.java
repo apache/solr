a16f5d018e41528e81ef70255973637a1ba46e7d
Mark Miller <markrmiller@gmail.com>
2021-09-22 11:31:41 -0500

    SOLR-15640: Flush / Shutdown Async Loggers and LogManager after a test.

--- solr/test-framework/src/java/org/apache/solr/SolrTestCase.java
+++ solr/test-framework/src/java/org/apache/solr/SolrTestCase.java
@@ -65,17 +65,6 @@ import com.carrotsearch.randomizedtesting.annotations.ThreadLeakFilters;
 @ThreadLeakLingering(linger = 10000)
 public class SolrTestCase extends LuceneTestCase {
 
-  /**
-   * <b>DO NOT REMOVE THIS LOGGER</b>
-   * <p>
-   * For reasons that aren't 100% obvious, the existence of this logger is neccessary to ensure
-   * that the logging framework is properly initialized (even if concrete subclasses do not 
-   * themselves initialize any loggers) so that the async logger threads can be properly shutdown
-   * on completion of the test suite
-   * </p>
-   * @see <a href="https://issues.apache.org/jira/browse/SOLR-14247">SOLR-14247</a>
-   * @see #afterSolrTestCase()
-   */
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private static final Pattern NAMING_CONVENTION_TEST_SUFFIX = Pattern.compile("(.+\\.)([^.]+)(Test)");
