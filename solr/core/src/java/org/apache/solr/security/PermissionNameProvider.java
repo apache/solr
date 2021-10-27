8d5a4944b6e075ad1b850e82b60411c3bcfb39a6
Jan Høydahl <jan.git@cominvent.com>
2021-10-27 10:45:58 +0200

    Make HEALTH_PERM apply to both node and collection-level

--- solr/core/src/java/org/apache/solr/security/PermissionNameProvider.java
+++ solr/core/src/java/org/apache/solr/security/PermissionNameProvider.java
@@ -48,7 +48,7 @@ public interface PermissionNameProvider {
     SECURITY_EDIT_PERM("security-edit", null),
     SECURITY_READ_PERM("security-read", null),
     METRICS_READ_PERM("metrics-read", null),
-    HEALTH_PERM("health", "*"),
+    HEALTH_PERM("health", unmodifiableSet(new HashSet<>(asList("*", null)))),
     FILESTORE_READ_PERM("filestore-read", null),
     FILESTORE_WRITE_PERM("filestore-write", null),
     PACKAGE_EDIT_PERM("package-edit", null),
