316bb6c9677874933dc34e1d8d9cfc2be39e2f8c
epugh@opensourceconnections.com <>
2021-11-10 16:26:02 -0500

    Revert "This actually works!"
    This reverts commit 3cbd85640046444945cc3711d041294cb0a35f87.

--- solr/solrj/src/test/org/apache/solr/client/solrj/SolrExampleXMLTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/SolrExampleXMLTest.java
@@ -16,17 +16,11 @@
  */
 package org.apache.solr.client.solrj;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.solr.SolrTestCaseJ4.SuppressSSL;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.apache.solr.client.solrj.request.RequestWriter;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.BeforeClass;
-
-import java.io.File;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 /**
  * A subclass of SolrExampleTests that explicitly uses the xml codec for
@@ -34,17 +28,9 @@ import java.util.TreeMap;
  */
 @SuppressSSL(bugUrl = "https://issues.apache.org/jira/browse/SOLR-5776")
 public class SolrExampleXMLTest extends SolrExampleTests {
-
   @BeforeClass
   public static void beforeTest() throws Exception {
-    File tmpSolrHome = createTempDir().toFile();
-    FileUtils.copyDirectory(new File(getFile("solrj/solr/testproducts/conf").getParent()), new File(tmpSolrHome.getAbsoluteFile(), "collection1"));
-
-    FileUtils.copyFile(getFile("solrj/solr/solr.xml"), new File(tmpSolrHome.getAbsoluteFile(), "solr.xml"));
-
-    final SortedMap<ServletHolder, String> extraServlets = new TreeMap<>();
-
-    createAndStartJetty(tmpSolrHome.getAbsolutePath(), "solrconfig.xml", "managed-schema", "/solr", true, extraServlets);
+    createAndStartJetty(legacyExampleCollection1SolrHome());
   }
   
   @Override
