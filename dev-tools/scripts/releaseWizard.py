b9eaf428e7f460b3fae34f8f228af23018b203a0
Houston Putman <houstonputman@gmail.com>
2021-11-16 14:22:05 -0500

    Update dev-tools/scripts/releaseWizard.py
    Co-authored-by: Mike Drob <mdrob@apache.org>

--- dev-tools/scripts/releaseWizard.py
+++ dev-tools/scripts/releaseWizard.py
@@ -83,7 +83,7 @@ def expand_jinja(text, vars=None):
         'script_branch': state.script_branch,
         'release_folder': state.get_release_folder(),
         'git_checkout_folder': state.get_git_checkout_folder(),
-        'ref_guide_svn_folder': state.get_git_checkout_folder(),
+        'ref_guide_svn_folder': state.get_ref_guide_svn_folder(),
         'git_website_folder': state.get_website_git_folder(),
         'dist_url_base': 'https://dist.apache.org/repos/dist/dev/lucene',
         'm2_repository_url': 'https://repository.apache.org/service/local/staging/deploy/maven2',
