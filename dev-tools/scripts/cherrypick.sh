bc2c5876f3d7cdc12e14a14926d95acb9f119bb1
Jan Høydahl <janhoy@users.noreply.github.com>
2022-01-11 23:31:03 +0100

    Reivew comments from mdrob Add multi -b support

--- dev-tools/scripts/cherrypick.sh
+++ dev-tools/scripts/cherrypick.sh
@@ -26,23 +26,22 @@ function LOG() {
 function usage() {
   cat << EOF
 Usage: dev-tools/scripts/cherrypick.sh [<options>] <commit-hash> [<commit-hash>...]
- -b <branch> Sets the branch to cherry-pick to, typically branch_Nx or branch_x_y
+ -b <branch> Sets the branch(es) to cherry-pick to, typically branch_Nx or branch_x_y
  -s          Skips precommit test. WARNING: Always run precommit for code- and doc changes
  -t          Run the full test suite during check, not only precommit
  -n          Skips git pull of target branch. Useful if you are without internet access
  -a          Enters automated mode. Aborts cherry-pick and exits on error
- -r <remote> Specify remote to push to. Defaults to if other than 'origin'
+ -r <remote> Specify remote to push to. Defaults to whatever the branch is tracking.
  -p          Push to remote. Only done if both cherry-pick and tests succeeded
     WARNING: Never push changes to a remote branch before a thorough local test
 
-Simple script for aiding in back-porting one more (trivial) commits to other branches.
+Simple script for aiding in back-porting one or more (trivial) commits to other branches.
 On merge conflict the script will run 'git mergetool'. See 'git mergetool --help'
 for help on configuring your favourite merge tool. Check out Sublime Merge (smerge).
 
 Example:
-  # Backport two commits to both stable and release branch
-  dev-tools/scripts/cherrypick.sh -b branch_9x  deadbeef0000 cafebabe1111
-  dev-tools/scripts/cherrypick.sh -b branch_9_0 deadbeef0000 cafebabe1111
+  # Backport two commits to both stable and release branches
+  dev-tools/scripts/cherrypick.sh -b branch_9x -b branch_9_0 deadbeef0000 cafebabe1111
 EOF
 }
 
@@ -66,14 +65,15 @@ TESTARG="-x test"
 TEST=
 TESTS_PASSED=
 PUSH=
-REMOTE=origin
+REMOTE=
 NOPULL=
 AUTO_MODE=
+unset BRANCHES
 
 while getopts ":b:phstnar:" opt; do
   case ${opt} in
     b)
-      BRANCH=$OPTARG
+      BRANCHES+=("$OPTARG")
       ;;
     r)
       REMOTE=$OPTARG
@@ -108,8 +108,8 @@ shift $((OPTIND -1))
 
 COMMITS=( "$@" )
 
-if [ -z "$BRANCH" ]; then
-  LOG INFO "Lacking -b option, must specify target branch"
+if [ ${#BRANCHES[@]} -eq 0 ]; then
+  LOG INFO "Lacking -b option, must specify target branch. Supports multiple -b options too"
   usage
   exit
 fi
@@ -120,16 +120,27 @@ if [ ${#COMMITS[@]} -eq 0 ]; then
   exit
 fi
 
-LOG INFO "Checking out target branch $BRANCH"
-$GIT_COMMAND checkout "$BRANCH"
-if [[ ! "$NOPULL" ]]; then
-  $GIT_COMMAND pull --ff-only
-else
-  LOG INFO "Skipping git pull"
-fi
+## Loop over branches
+for BRANCH in "${BRANCHES[@]}"; do
+  echo ""
+  LOG "INFO" "============================================";
+  LOG "INFO" "Git branch: $BRANCH"
+  LOG "INFO" "============================================";
+  echo ""
+  LOG INFO "Checking out target branch $BRANCH"
+  $GIT_COMMAND checkout "$BRANCH"
+  if [ $? -gt 0 ]; then
+    LOG ERROR "Failed checking out branch $BRANCH"
+    exit 2
+  fi
+  if [[ ! "$NOPULL" ]]; then
+    $GIT_COMMAND pull --ff-only "$REMOTE"
+  else
+    LOG INFO "Skipping git pull"
+  fi
 
-## main processing loop
-for i in "${COMMITS[@]}"
+  # Loop over commits
+  for i in "${COMMITS[@]}"
   do
     LOG "INFO" "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::";
     LOG "INFO" "Processing commit          : $i"
@@ -160,32 +171,34 @@ for i in "${COMMITS[@]}"
     fi
   done
 
-LOG INFO "All cherry-pick succeeded"
+  LOG INFO "All cherry-picks to branch $BRANCH succeeded"
 
-if [[ "$PRECOMMIT" ]] || [[ "$TEST" ]]; then
-  LOG "INFO" "Testing the cherry-pick by running 'gradlew check ${TESTARG}'"
-  ./gradlew check -q "${TESTARG}"
-  if [ $? -gt 0 ]; then
-    LOG "WARN" "Tests failed. Please fix and push manually"
-    exit 2
+  if [[ "$PRECOMMIT" ]] || [[ "$TEST" ]]; then
+    LOG "INFO" "Testing the cherry-pick on $BRANCH by running 'gradlew check ${TESTARG}'"
+    ./gradlew check -q "${TESTARG}"
+    if [ $? -gt 0 ]; then
+      LOG "WARN" "Tests failed. Please fix and push manually"
+      exit 2
+    fi
+    TESTS_PASSED="true"
+    LOG "INFO" "Tests passed on $BRANCH"
+  else
+    LOG "INFO" "Skipping tests"
   fi
-  TESTS_PASSED="true"
-  LOG "INFO" "Tests passed"
-else
-  LOG "INFO" "Skipping tests"
-fi
 
-if [[ "$PUSH" ]]; then
-  if [[ ! $TESTS_PASSED ]]; then
-    LOG "WARN" "Cannot auto push if tests are disabled" 
-    exit 1
+  if [[ "$PUSH" ]]; then
+    if [[ ! $TESTS_PASSED ]]; then
+      LOG "WARN" "Cannot auto push if tests are disabled"
+      exit 1
+    fi
+    LOG "INFO" "Pushing changes to $REMOTE/$BRANCH"
+    $GIT_COMMAND push "$REMOTE" "$BRANCH"
+    if [ $? -gt 0 ]; then
+      LOG "WARN" "PUSH to $REMOTE/$BRANCH failed, please clean up an proceed manually"
+      exit 2
+    fi
+    LOG "INFO" "Pushed $BRANCH to remote. Cherry-pick complete."
   fi
-  LOG "INFO" "Pushing changes to $REMOTE/$BRANCH"
-  $GIT_COMMAND push "$REMOTE"
-  if [ $? -gt 0 ]; then
-    LOG "WARN" "PUSH to $REMOTE/$BRANCH failed, please clean up an proceed manually"
-    exit 2
-  fi
-  LOG "INFO" "Pushed to remote. Cherry-pick complete."
-fi
+done
 
+LOG "INFO" "SUCCESS"
