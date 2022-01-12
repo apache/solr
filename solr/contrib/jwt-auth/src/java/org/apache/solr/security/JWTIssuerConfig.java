15bbd5970dd990af677220a9b72894f2dfbf5a7f
Jan Høydahl <janhoy@users.noreply.github.com>
2022-01-12 17:25:30 +0100

    Make spotless happy

--- solr/contrib/jwt-auth/src/java/org/apache/solr/security/JWTIssuerConfig.java
+++ solr/contrib/jwt-auth/src/java/org/apache/solr/security/JWTIssuerConfig.java
@@ -429,7 +429,8 @@ public class JWTIssuerConfig {
 
   /**
    * Config object for a OpenId Connect well-known config.
-   * <p>Typically exposed through <code>/.well-known/openid-configuration endpoint</code></p>
+   *
+   * <p>Typically exposed through <code>/.well-known/openid-configuration endpoint</code>
    */
   public static class WellKnownDiscoveryConfig {
     private final Map<String, Object> securityConf;
