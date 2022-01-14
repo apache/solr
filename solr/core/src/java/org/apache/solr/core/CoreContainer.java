88567030cbc28f778436e2aac8b08dd4230da07a
Jan Høydahl <janhoy@users.noreply.github.com>
2022-01-14 23:40:22 +0100

    Unused import

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -126,7 +126,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
