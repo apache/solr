99cb91bd852e60c0ce4811365de1fbb02c95a876
Michael Gibney <michael@michaelgibney.net>
2022-01-19 15:23:36 -0500

    simplify by removing the `testParallelFail()` test
    it was jumping through hoops to try to recreate superficial
    aspects of the initial problem, but for no real purpose, and
    at the expense of much additional complexity.

--- solr/core/src/test/org/apache/solr/cloud/ParallelCommitExecutionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ParallelCommitExecutionTest.java
@@ -35,7 +35,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.lucene.util.TestUtil;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.request.CollectionAdminRequest;
-import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -48,7 +47,6 @@ public class ParallelCommitExecutionTest extends SolrCloudTestCase {
   /** A basic client for operations at the cloud level, default collection will be set */
   private static CloudSolrClient CLOUD_CLIENT;
   private static int expectCount;
-  private static volatile int tooHigh;
 
   private static volatile CountDownLatch countdown;
   private static final AtomicInteger countup = new AtomicInteger();
@@ -87,49 +85,19 @@ public class ParallelCommitExecutionTest extends SolrCloudTestCase {
     }
   }
 
-  private static void initSyncVars(boolean tooHigh) {
+  private static void initSyncVars() {
     final int ct;
-    if (tooHigh) {
-      ParallelCommitExecutionTest.tooHigh = TOO_HIGH_INCREMENT;
-      ct = expectCount + TOO_HIGH_INCREMENT;
-    } else {
-      ParallelCommitExecutionTest.tooHigh = 0;
-      ct = expectCount;
-    }
+    ct = expectCount;
     countdown = new CountDownLatch(ct);
     countup.set(0);
   }
 
   @Test
   public void testParallelOk() throws Exception {
-    initSyncVars(false);
-    UpdateResponse rsp = CLOUD_CLIENT.commit(true, true);
-    // nocommit: why are we getting status:0 in response that clearly failed? It looks like no matter
-    // how many distrib commits fail, as long as the local commit succeeds, it reports success. (if the
-    // local commit fails, it apparently does report failure).
-    // that's why we're doing this "countup" thing -- because there's apparently no other way to know
-    // whether the commit succeeded or not?
-    // we expect `countup` to be incremented by exactly `expectCount` number of parallel commits
+    initSyncVars();
+    CLOUD_CLIENT.commit(true, true);
+    assertEquals(0, countdown.getCount());
     assertEquals(expectCount, countup.get());
-  }
-
-  private static final int TOO_HIGH_INCREMENT = 1;
-  @Test
-  public void testParallelFail() throws Exception {
-    // artificially set countdown too high; we're really testing the test code, since there's no way
-    // to mock the "incorrect" behavior (i.e., "serial" requests)
-    initSyncVars(true);
-    UpdateResponse rsp = CLOUD_CLIENT.commit(true, true);
-
-    // If we could mock a "real" scenario, we'd expect the distrib requests to timeout waiting
-    // on the countdown latch (and thus not increment `countup`), but the "local" shard would
-    // then succeed, resulting in `countup` being incremented exactly once. Here we've set `countdown`
-    // artificially high, but to approximately mock this, we get the "local" shard to recognize
-    // the "tooHigh" scenario, and succeed anyway, incrementing `countup`.
-    assertEquals(1, countup.get());
-    // sanity check: because we actually expect things to be working properly, we expect `countdown`
-    // to be exactly "TOO_HIGH_INCREMENT" too high.
-    assertEquals(TOO_HIGH_INCREMENT, countdown.getCount());
   }
 
   public static void waitForRecoveriesToFinish(CloudSolrClient client) throws Exception {
@@ -156,11 +124,10 @@ public class ParallelCommitExecutionTest extends SolrCloudTestCase {
     public void processCommit(CommitUpdateCommand cmd) throws IOException {
       super.processCommit(cmd);
       countdown.countDown();
-      // In the "tooHigh" scenario, for the one-and-only local (non-distrib) shard, force success (after waiting) to
-      // mock the "serial blocking" behavior.
-      final boolean mustSucceed = tooHigh == TOO_HIGH_INCREMENT && cmd.getReq().getParams().get("update.distrib") == null;
       try {
-        if (!countdown.await(5, TimeUnit.SECONDS) && !mustSucceed) {
+        // NOTE: this ensures that all commits are executed in parallel; no commit can complete successfully
+        // until all commits have entered the `processCommit(...)` method.
+        if (!countdown.await(5, TimeUnit.SECONDS)) {
           throw new RuntimeException("done waiting");
         }
         countup.incrementAndGet();
