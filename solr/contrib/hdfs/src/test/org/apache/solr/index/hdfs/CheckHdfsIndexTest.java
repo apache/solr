2e2005b8cea668296185c74d0c7ce0069af64376
Istvan Farkas <ent128k@gmail.com>
2022-01-23 11:20:21 -0500

    HDFS Contrib Module: fixes after merging main to feature branch. - added missing dependencies - updated test resource files in Solr contrib to match those of test framework (will be consolidated later to remove duplications)

--- solr/contrib/hdfs/src/test/org/apache/solr/index/hdfs/CheckHdfsIndexTest.java
+++ solr/contrib/hdfs/src/test/org/apache/solr/index/hdfs/CheckHdfsIndexTest.java
@@ -24,6 +24,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.lucene.index.BaseTestCheckIndex;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.LuceneTestCase;
 import org.apache.lucene.util.QuickPatchThreadsFilter;
 import org.apache.solr.SolrIgnoredThreadsFilter;
 import org.apache.solr.SolrTestCaseJ4;
