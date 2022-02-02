0f031d1cae17cbe03fe054df45cbef67bce99aed
Mike Drob <mdrob@apache.org>
2022-02-02 09:16:32 -0600

    SOLR-15969 Supress SimpleTextCodec on Backup Operations (#587)
    Co-authored-by: Kevin Risden <krisden@apache.org>
    (cherry picked from commit a1ae39c1f6739458ac61530aaad9523c2a9a1e6c)

--- solr/test-framework/src/java/org/apache/solr/cloud/AbstractBasicDistributedZk2TestBase.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/AbstractBasicDistributedZk2TestBase.java
@@ -21,6 +21,7 @@ import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.lucene.mockfile.FilterPath;
+import org.apache.lucene.util.LuceneTestCase;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -43,6 +44,7 @@ import org.junit.Test;
  * This test simply does a bunch of basic things in solrcloud mode and asserts things
  * work as expected.
  */
+@LuceneTestCase.SuppressCodecs({"SimpleText"}) // Backups do checksum validation against a footer value not present in 'SimpleText'
 public abstract class AbstractBasicDistributedZk2TestBase extends AbstractFullDistribZkTestBase {
   private static final String SHARD2 = "shard2";
   private static final String SHARD1 = "shard1";
