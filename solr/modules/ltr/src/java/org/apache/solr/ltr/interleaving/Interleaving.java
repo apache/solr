077214042aeeb460e8e689eab2217ed34ce778fc
Christine Poerschke <cpoerschke@apache.org>
2022-02-21 16:29:16 +0000

    SOLR-14920, SOLR-16029: contrib/ltr/src/java: apply & enforce 'spotless' code formatting (#189)

--- solr/modules/ltr/src/java/org/apache/solr/ltr/interleaving/Interleaving.java
+++ solr/modules/ltr/src/java/org/apache/solr/ltr/interleaving/Interleaving.java
@@ -22,25 +22,27 @@ import org.apache.solr.common.SolrException;
 import org.apache.solr.ltr.interleaving.algorithms.TeamDraftInterleaving;
 
 /**
- * Interleaving considers two ranking models: modelA and modelB.
- * For a given query, each model returns its ranked list of documents La = (a1,a2,...) and Lb = (b1, b2, ...).
- * An Interleaving algorithm creates a unique ranked list I = (i1, i2, ...).
- * This list is created by interleaving elements from the two lists la and lb as described by the implementation algorithm.
+ * Interleaving considers two ranking models: modelA and modelB.<br>
+ * For a given query, each model returns its ranked list of documents La = (a1,a2,...) and Lb = (b1,
+ * b2, ...).<br>
+ * An Interleaving algorithm creates a unique ranked list I = (i1, i2, ...).<br>
+ * This list is created by interleaving elements from the two lists la and lb as described by the
+ * implementation algorithm.<br>
  * Each element Ij is labelled TeamA if it is selected from La and TeamB if it is selected from Lb.
  */
 public interface Interleaving {
 
-   String TEAM_DRAFT = "TeamDraft";
+  String TEAM_DRAFT = "TeamDraft";
 
-   InterleavingResult interleave(ScoreDoc[] rerankedA, ScoreDoc[] rerankedB);
+  InterleavingResult interleave(ScoreDoc[] rerankedA, ScoreDoc[] rerankedB);
 
-   static Interleaving getImplementation(String algorithm) {
-      switch(algorithm) {
-         case TEAM_DRAFT:
-            return new TeamDraftInterleaving();
-         default:
-            throw new SolrException(SolrException.ErrorCode.BAD_REQUEST,
-                "Unknown Interleaving algorithm: " + algorithm);
-      }
-   }
+  static Interleaving getImplementation(String algorithm) {
+    switch (algorithm) {
+      case TEAM_DRAFT:
+        return new TeamDraftInterleaving();
+      default:
+        throw new SolrException(
+            SolrException.ErrorCode.BAD_REQUEST, "Unknown Interleaving algorithm: " + algorithm);
+    }
+  }
 }
