7e628970044e2e79089aebbe506ee74a6e2ed4da
Kevin Risden <risdenk@users.noreply.github.com>
2022-03-04 12:44:43 -0500

    SOLR-14920: Spotless formatting for core - test only - followup (#720)

--- solr/core/src/test/org/apache/solr/request/TestFaceting.java
+++ solr/core/src/test/org/apache/solr/request/TestFaceting.java
@@ -628,20 +628,17 @@ public class TestFaceting extends SolrTestCaseJ4 {
               "facet.field",
               "{!key=foo " + "facet.sort=index" + "}" + fname,
               "facet.field",
-              "{!key=bar " + "facet.missing=true" + "}" + fname)
+              "{!key=bar " + "facet.missing=true" + "}" + fname),
           // foo is in index order w/o missing
-          ,
           "*[count(//lst[@name='foo']/int)=4]",
           "//lst[@name='foo']/int[1][@name='Chauvinist'][.='1']",
           "//lst[@name='foo']/int[2][@name='Obnoxious'][.='1']",
           "//lst[@name='foo']/int[3][@name='Pig'][.='0']",
-          "//lst[@name='foo']/int[4][@name='Tool'][.='2']"
+          "//lst[@name='foo']/int[4][@name='Tool'][.='2']",
           // bar is in count order by default and includes missing
-          ,
           "*[count(//lst[@name='bar']/int)=5]",
-          "//lst[@name='bar']/int[1][@name='Tool'][.='2']"
+          "//lst[@name='bar']/int[1][@name='Tool'][.='2']",
           // don't assume tie breaker for slots 3 & 4, behavior undefined?
-          ,
           "//lst[@name='bar']/int[4][@name='Pig'][.='0']",
           "//lst[@name='bar']/int[5][not(@name)][.='1']");
 
@@ -660,17 +657,14 @@ public class TestFaceting extends SolrTestCaseJ4 {
               "facet.field",
               "{!key=foo " + "facet.mincount=2" + "}" + fname,
               "facet.field",
-              "{!key=bar}" + fname)
+              "{!key=bar}" + fname),
           // only Tool for foo
-          ,
           "*[count(//lst[@name='foo']/int)=1]",
-          "//lst[@name='foo']/int[1][@name='Tool'][.='2']"
+          "//lst[@name='foo']/int[1][@name='Tool'][.='2']",
           // all for bar
-          ,
           "*[count(//lst[@name='bar']/int)=4]",
-          "//lst[@name='bar']/int[1][@name='Tool'][.='2']"
+          "//lst[@name='bar']/int[1][@name='Tool'][.='2']",
           // don't assume tie breaker for slots 3 & 4, behavior undefined?
-          ,
           "//lst[@name='bar']/int[4][@name='Pig'][.='0']");
 
       assertQ(
@@ -688,21 +682,17 @@ public class TestFaceting extends SolrTestCaseJ4 {
               "facet.field",
               "{!key=foo " + "facet.missing=true" + "}" + fname,
               "facet.field",
-              "{!key=bar}" + fname)
+              "{!key=bar}" + fname),
           // foo includes missing
-          ,
           "*[count(//lst[@name='foo']/int)=5]",
-          "//lst[@name='foo']/int[1][@name='Tool'][.='2']"
+          "//lst[@name='foo']/int[1][@name='Tool'][.='2']",
           // don't assume tie breaker for slots 3 & 4, behavior undefined?
-          ,
           "//lst[@name='foo']/int[4][@name='Pig'][.='0']",
-          "//lst[@name='foo']/int[5][not(@name)][.='1']"
+          "//lst[@name='foo']/int[5][not(@name)][.='1']",
           // bar does not
-          ,
           "*[count(//lst[@name='bar']/int)=4]",
-          "//lst[@name='bar']/int[1][@name='Tool'][.='2']"
+          "//lst[@name='bar']/int[1][@name='Tool'][.='2']",
           // don't assume tie breaker for slots 3 & 4, behavior undefined?
-          ,
           "//lst[@name='bar']/int[4][@name='Pig'][.='0']");
 
       assertQforUIF(
