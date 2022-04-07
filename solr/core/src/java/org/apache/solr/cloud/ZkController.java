d19e675359f84208360f65daaafb443378c0f06b
David Smiley <dsmiley@salesforce.com>
2022-04-06 23:50:44 -0400

    SOLR-16145: NPE fix in rejoinOverseerElection
    Introduced by SOLR-13410

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -2346,15 +2346,16 @@ public class ZkController implements Closeable {
 
   public void rejoinOverseerElection(String electionNode, boolean joinAtHead) {
     try {
+      final ElectionContext context = overseerElector.getContext();
       if (electionNode != null) {
         // Check whether we came to this node by mistake
-        if (overseerElector.getContext() != null
-            && overseerElector.getContext().leaderSeqPath == null
-            && !overseerElector.getContext().leaderSeqPath.endsWith(electionNode)) {
+        if (context != null
+            && context.leaderSeqPath != null
+            && !context.leaderSeqPath.endsWith(electionNode)) {
           log.warn(
               "Asked to rejoin with wrong election node : {}, current node is {}",
               electionNode,
-              overseerElector.getContext().leaderSeqPath);
+              context.leaderSeqPath);
           // however delete it . This is possible when the last attempt at deleting the election
           // node failed.
           if (electionNode.startsWith(getNodeName())) {
@@ -2377,7 +2378,7 @@ public class ZkController implements Closeable {
           return;
         }
       } else {
-        overseerElector.retryElection(overseerElector.getContext(), joinAtHead);
+        overseerElector.retryElection(context, joinAtHead);
       }
     } catch (Exception e) {
       throw new SolrException(ErrorCode.SERVER_ERROR, "Unable to rejoin election", e);
