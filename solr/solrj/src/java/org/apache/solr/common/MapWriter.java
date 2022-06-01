446eb67acde70ff5962923b274d1e58de0752023
Noble Paul <noble.paul@gmail.com>
2022-06-01 14:46:13 +1000

    formatting

--- solr/solrj/src/java/org/apache/solr/common/MapWriter.java
+++ solr/solrj/src/java/org/apache/solr/common/MapWriter.java
@@ -18,7 +18,11 @@
 package org.apache.solr.common;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Collections;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import org.apache.solr.common.util.Utils;
