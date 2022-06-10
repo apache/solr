db04c5eeb7e01dd0a096b8153befc207f4458a32
Jason Gerlowski <gerlowskija@apache.org>
2022-06-10 14:13:47 -0400

    SOLR-15182: Replace emptySpec.json apispec file
    Solr's been in the slow process of moving its v2 APIs away from the
    existing apispec/mapping framework towards one that relies on more
    explicit annotations to specify API properties.
    
    This commit removes emptySpec.json: replacing it with a static constant
    in ApiBag.  This is the last apispec file!
    
    This commit does NOT remove any of the framework that allowed parsing
    and registering these apispec JSON files.  There should be a good bit of
    code that can go, but I wanted to separate its removal into a different
    commit for now.

--- solr/core/src/java/org/apache/solr/api/ApiBag.java
+++ solr/core/src/java/org/apache/solr/api/ApiBag.java
@@ -17,27 +17,9 @@
 
 package org.apache.solr.api;
 
-import static org.apache.solr.client.solrj.SolrRequest.SUPPORTED_METHODS;
-import static org.apache.solr.common.params.CommonParams.NAME;
-import static org.apache.solr.common.util.StrUtils.formatString;
-import static org.apache.solr.common.util.ValidatingJsonMap.ENUM_OF;
-import static org.apache.solr.common.util.ValidatingJsonMap.NOT_NULL;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
-import java.io.IOException;
-import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.SpecProvider;
@@ -57,6 +39,25 @@ import org.apache.solr.security.AuthorizationContext;
 import org.apache.solr.security.PermissionNameProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static org.apache.solr.client.solrj.SolrRequest.SUPPORTED_METHODS;
+import static org.apache.solr.common.params.CommonParams.NAME;
+import static org.apache.solr.common.util.StrUtils.formatString;
+import static org.apache.solr.common.util.ValidatingJsonMap.ENUM_OF;
+import static org.apache.solr.common.util.ValidatingJsonMap.NOT_NULL;
 
 public class ApiBag {
   private final boolean isCoreSpecific;
@@ -379,34 +380,41 @@ public class ApiBag {
     return b.build();
   }
 
-  public static final SpecProvider EMPTY_SPEC = () -> ValidatingJsonMap.EMPTY;
   public static final String HANDLER_NAME = "handlerName";
+  public static final SpecProvider EMPTY_SPEC = () -> ValidatingJsonMap.EMPTY;
   public static final Set<String> KNOWN_TYPES =
-      ImmutableSet.of("string", "boolean", "list", "int", "double", "object");
+          ImmutableSet.of("string", "boolean", "list", "int", "double", "object");
+  // A Spec template for GET AND POST APIs using the /$handlerName template-variable.
+  public static final SpecProvider HANDLER_NAME_SPEC_PROVIDER = () -> {
+    final ValidatingJsonMap spec = new ValidatingJsonMap();
+    spec.put("methods", Lists.newArrayList("GET", "POST"));
+    final ValidatingJsonMap urlMap = new ValidatingJsonMap();
+    urlMap.put("paths", Collections.singletonList("$" + HANDLER_NAME));
+    spec.put("url", urlMap);
+    return spec;
+  };
+
 
   public PathTrie<Api> getRegistry(String method) {
     return apis.get(method);
   }
 
   public void registerLazy(PluginBag.PluginHolder<SolrRequestHandler> holder, PluginInfo info) {
-    String specName = info.attributes.get("spec");
-    if (specName == null) specName = "emptySpec";
     register(
-        new LazyLoadedApi(Utils.getSpec(specName), holder),
+        new LazyLoadedApi(HANDLER_NAME_SPEC_PROVIDER, holder),
         Collections.singletonMap(HANDLER_NAME, info.attributes.get(NAME)));
   }
 
   public static SpecProvider constructSpec(PluginInfo info) {
     Object specObj = info == null ? null : info.attributes.get("spec");
-    if (specObj == null) specObj = "emptySpec";
-    if (specObj instanceof Map) {
+    if (specObj != null && specObj instanceof Map) {
       // Value from Map<String,String> can be a Map because in PluginInfo(String, Map) we assign a
       // Map<String, Object>
       // assert false : "got a map when this should only be Strings";
       Map<?, ?> map = (Map<?, ?>) specObj;
       return () -> ValidatingJsonMap.getDeepCopy(map, 4, false);
     } else {
-      return Utils.getSpec((String) specObj);
+      return HANDLER_NAME_SPEC_PROVIDER;
     }
   }
 
