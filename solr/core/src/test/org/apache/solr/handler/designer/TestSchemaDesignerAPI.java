11c01c89926ebaca05b66fc6766125f1afbac626
Christine Poerschke <cpoerschke@apache.org>
2022-06-17 18:53:41 +0100

    Apply suggestions from code review

--- solr/core/src/test/org/apache/solr/handler/designer/TestSchemaDesignerAPI.java
+++ solr/core/src/test/org/apache/solr/handler/designer/TestSchemaDesignerAPI.java
@@ -66,9 +66,13 @@ public class TestSchemaDesignerAPI extends SolrCloudTestCase implements SchemaDe
 
   @BeforeClass
   public static void createCluster() throws Exception {
+    System.setProperty("managed.schema.mutable", "true");
     configureCluster(1)
         .addConfig(DEFAULT_CONFIGSET_NAME, new File(ExternalPaths.DEFAULT_CONFIGSET).toPath())
         .configure();
+    // SchemaDesignerAPI depends on the blob store
+    CollectionAdminRequest.createCollection(BLOB_STORE_ID, 1, 1).process(cluster.getSolrClient());
+    cluster.waitForActiveCollection(BLOB_STORE_ID, 1, 1);
   }
 
   @AfterClass
