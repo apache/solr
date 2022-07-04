4f3dfda51274675964821926f322959afe275b42
Elia <e.porciani@sease.io>
2022-07-04 16:49:02 +0200

    error message changed

--- solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
+++ solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
@@ -132,7 +132,7 @@ public class SchemaCodecFactory extends CodecFactory implements SolrCoreAware {
                 }
               } else {
                 throw new SolrException(
-                    ErrorCode.SERVER_ERROR, knnAlgorithm + " KNN algorithm provided is not supported");
+                    ErrorCode.SERVER_ERROR, knnAlgorithm + " KNN algorithm is not supported");
               }
             }
             return super.getKnnVectorsFormatForField(field);
