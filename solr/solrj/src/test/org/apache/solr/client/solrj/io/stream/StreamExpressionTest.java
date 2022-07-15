2623dfc50417dc58fd1bb4cbb0a341206a9c9a49
Dan Rosher <d.rosher@cv-library.co.uk>
2022-07-15 11:00:22 +0100

    SOLR-16287 : MapStream - remap tuple value(s)

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamExpressionTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamExpressionTest.java
@@ -2940,7 +2940,8 @@ public class StreamExpressionTest extends SolrCloudTestCase {
       context = new StreamContext();
       context.setSolrClientCache(cache);
       stream.setStreamContext(context);
-      assertTopicRun(stream, "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11","12", "13");
+      assertTopicRun(
+          stream, "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13");
 
       // Test text extraction
 
