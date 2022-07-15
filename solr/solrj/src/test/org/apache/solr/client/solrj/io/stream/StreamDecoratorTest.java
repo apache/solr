e06e5893202bf51722033828bb0677fbf3335a61
Dan Rosher <d.rosher@cv-library.co.uk>
2022-07-15 10:53:49 +0100

    SOLR-16287 : MapStream - remap tuple value(s)

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamDecoratorTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/StreamDecoratorTest.java
@@ -4338,7 +4338,7 @@ public class StreamDecoratorTest extends SolrCloudTestCase {
 
     classifyStream = new SolrStream(url, paramsLoc);
     idToLabel = getIdToLabel(classifyStream, "probability_d");
-    assertEquals(idToLabel.size(), 2);
+    assertEquals(idToLabel.size(), 4);
     assertEquals(1.0, idToLabel.get("2"), 0.001);
     assertEquals(0, idToLabel.get("3"), 0.001);
 
@@ -4367,7 +4367,7 @@ public class StreamDecoratorTest extends SolrCloudTestCase {
 
     classifyStream = new SolrStream(url, paramsLoc);
     idToLabel = getIdToLabel(classifyStream, "probability_d");
-    assertEquals(idToLabel.size(), 2);
+    assertEquals(idToLabel.size(), 6);
     assertEquals(0, idToLabel.get("4"), 0.001);
     assertEquals(1.0, idToLabel.get("5"), 0.001);
 
@@ -4385,7 +4385,7 @@ public class StreamDecoratorTest extends SolrCloudTestCase {
     paramsLoc.set("expr", expr);
     classifyStream = new SolrStream(url, paramsLoc);
     idToLabel = getIdToLabel(classifyStream, "probability_d");
-    assertEquals(idToLabel.size(), 2);
+    assertEquals(idToLabel.size(), 8);
     assertEquals(0, idToLabel.get("4"), 0.001);
     assertEquals(1.0, idToLabel.get("5"), 0.001);
 
