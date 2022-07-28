50c486f92c8f25068ad24be5c0318cc471b4fb99
Dan Rosher <d.rosher@cv-library.co.uk>
2022-07-28 10:08:10 +0100

    use custom sorting on dotproduct as enough for comparison

--- solr/core/src/java/org/apache/solr/search/function/distance/NVectorFunction.java
+++ solr/core/src/java/org/apache/solr/search/function/distance/NVectorFunction.java
@@ -18,6 +18,7 @@
 package org.apache.solr.search.function.distance;
 
 import static org.apache.solr.util.NVectorUtil.NVectorDist;
+import static org.apache.solr.util.NVectorUtil.NVectorDotProduct;
 
 import java.io.IOException;
 import java.util.Map;
@@ -27,11 +28,10 @@ import org.apache.lucene.queries.function.FunctionValues;
 import org.apache.lucene.queries.function.ValueSource;
 import org.apache.lucene.queries.function.docvalues.DoubleDocValues;
 import org.apache.lucene.queries.function.valuesource.MultiValueSource;
-import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.*;
 import org.apache.solr.common.SolrException;
 
 public class NVectorFunction extends ValueSource {
-
   private final MultiValueSource nvector1;
   private final MultiValueSource nvector2;
   private final double radius;
@@ -48,11 +48,28 @@ public class NVectorFunction extends ValueSource {
 
   @Override
   public FunctionValues getValues(Map<Object, Object> context, LeafReaderContext readerContext)
-      throws IOException {
+          throws IOException {
+
+    final FunctionValues fv = getDotProductValues(context,readerContext);
+
+    return new DoubleDocValues(this) {
+
+      @Override
+      public double doubleVal(int doc) throws IOException {
+        double dotProduct = fv.doubleVal(doc);
+        return NVectorDist(dotProduct, radius);
+      }
+
+      @Override
+      public String toString(int doc) throws IOException {
+        return fv.toString(doc);
+      }
+    };
+  }
+  public FunctionValues getDotProductValues(Map<Object, Object> context, LeafReaderContext readerContext) throws IOException {
 
     final FunctionValues nvector_v1 = nvector1.getValues(context, readerContext);
     final FunctionValues nvector_v2 = nvector2.getValues(context, readerContext);
-
     return new DoubleDocValues(this) {
 
       @Override
@@ -61,7 +78,7 @@ public class NVectorFunction extends ValueSource {
         double[] nvector_dv2 = new double[nvector2.dimension()];
         nvector_v1.doubleVal(doc, nvector_dv1);
         nvector_v2.doubleVal(doc, nvector_dv2);
-        return NVectorDist(nvector_dv1, nvector_dv2, radius);
+        return NVectorDotProduct(nvector_dv1, nvector_dv2);
       }
 
       @Override
@@ -74,7 +91,6 @@ public class NVectorFunction extends ValueSource {
   protected String name() {
     return "nvector";
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -84,20 +100,113 @@ public class NVectorFunction extends ValueSource {
         && nvector1.equals(that.nvector1)
         && nvector2.equals(that.nvector2);
   }
-
   @Override
   public int hashCode() {
     return Objects.hash(nvector1, nvector2, radius);
   }
-
   @Override
   public void createWeight(Map<Object, Object> context, IndexSearcher searcher) throws IOException {
     nvector1.createWeight(context, searcher);
     nvector2.createWeight(context, searcher);
   }
-
   @Override
   public String description() {
     return name() + '(' + nvector1 + ',' + nvector2 + ')';
   }
+  @Override
+  public SortField getSortField(boolean reverse){ return new NVectorValueSourceSortField(reverse);  }
+  class NVectorValueSourceSortField extends SortField {
+    public NVectorValueSourceSortField(boolean reverse) {
+      super(description(), SortField.Type.REWRITEABLE, reverse);
+    }
+
+    @Override
+    public SortField rewrite(IndexSearcher searcher) throws IOException {
+      Map<Object, Object> context = ValueSource.newContext(searcher);
+      createWeight(context, searcher);
+      return new SortField(getField(), new NVectorValueSourceComparatorSource(context), getReverse());
+    }
+  }
+  class NVectorValueSourceComparatorSource extends FieldComparatorSource {
+    private final Map<Object, Object> context;
+
+    public NVectorValueSourceComparatorSource(Map<Object, Object> context) {
+      this.context = context;
+    }
+
+    @Override
+    public FieldComparator<Double> newComparator(
+            String fieldname, int numHits, boolean enableSkipping, boolean reversed) {
+      return new NVectorValueSourceComparator(context, numHits);
+    }
+  }
+  //Please note: The comparisons are INVERTED here, for performance on sorting we compare
+  // with the dot product, which is negatively correlated to acos(dot_product). Converting the dot product to distance with d=R*acos(dot_product), for example:
+  // where d = distance, dp = dot_product
+  // d:19628.29698448594 dp:-0.9981573955675561
+  //d:18583.651644725564 dp:-0.9748645959351536
+  //d:18180.44788490305 dp:-0.9588220684898193
+  //d:18052.6639699517 dp:-0.9529332319157707
+  //d:17882.7920545206 dp:-0.9445116994940388
+  //....
+  //d:1930.2292960794525 dp:0.954454358625817
+  //d:1843.0056018566079 dp:0.9584495044607074
+  //d:455.63526860635756 dp:0.9974437510266485
+  //d:336.9277320011129 dp:0.9986019397287412
+  //d:0.0 dp:1.0
+  // A HIGHER dot_product equates to a closer (LOWER) distance hence we invert
+  class NVectorValueSourceComparator extends SimpleFieldComparator<Double> {
+    private final double[] values;
+    private FunctionValues docVals;
+    private double bottom;
+    private final Map<Object, Object> fcontext;
+    private double topValue;
+
+    NVectorValueSourceComparator(Map<Object, Object> fcontext, int numHits) {
+      this.fcontext = fcontext;
+      values = new double[numHits];
+    }
+
+    @Override
+    public int compare(int slot1, int slot2) {
+      return Double.compare(values[slot2],values[slot1]);
+    }
+
+    @Override
+    public int compareBottom(int doc) throws IOException {
+      return Double.compare(docVals.doubleVal(doc),bottom);
+    }
+
+    @Override
+    public void copy(int slot, int doc) throws IOException {
+      values[slot] = docVals.doubleVal(doc);
+    }
+
+    @Override
+    public void doSetNextReader(LeafReaderContext context) throws IOException {
+      docVals = getDotProductValues(fcontext, context);
+    }
+
+    @Override
+    public void setBottom(final int bottom) {
+      this.bottom = values[bottom];
+    }
+
+    @Override
+    public void setTopValue(final Double value) {
+      this.topValue = value;
+    }
+
+    @Override
+    public Double value(int slot) {
+      return values[slot];
+    }
+
+    @Override
+    public int compareTop(int doc) throws IOException {
+      return Double.compare(docVals.doubleVal(doc),topValue);
+    }
+  }
 }
+
+
