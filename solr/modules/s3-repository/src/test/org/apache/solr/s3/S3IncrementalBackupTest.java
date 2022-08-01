f5a453a2bce286915fe24eb2b7ffd7f8bf92e681
Mike Drob <mdrob@apache.org>
2022-08-01 13:21:05 -0500

    Avoid reading local aws config (#957)
    (cherry picked from commit 01c059811a04d2713b34fd0bd54c2a96a1173938)

--- solr/modules/s3-repository/src/test/org/apache/solr/s3/S3IncrementalBackupTest.java
+++ solr/modules/s3-repository/src/test/org/apache/solr/s3/S3IncrementalBackupTest.java
@@ -93,6 +93,8 @@ public class S3IncrementalBackupTest extends AbstractIncrementalBackupTest {
     System.setProperty("aws.accessKeyId", "foo");
     System.setProperty("aws.secretAccessKey", "bar");
 
+    AbstractS3ClientTest.setS3ConfFile();
+
     configureCluster(NUM_NODES) // nodes
         .addConfig("conf1", getFile("conf/solrconfig.xml").getParentFile().toPath())
         .withSolrXml(
