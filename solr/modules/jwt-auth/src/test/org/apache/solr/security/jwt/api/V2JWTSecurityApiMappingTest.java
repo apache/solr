9b610c80135c1c940869061c40c31b68ab7ba2ec
Jason Gerlowski <gerlowskija@apache.org>
2022-08-05 11:25:15 -0400

    Fix split-package in tests as well

--- solr/modules/jwt-auth/src/test/org/apache/solr/security/jwt/api/V2JWTSecurityApiMappingTest.java
+++ solr/modules/jwt-auth/src/test/org/apache/solr/security/jwt/api/V2JWTSecurityApiMappingTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,16 +15,16 @@
  * limitations under the License.
  */
 
-package org.apache.solr.handler.admin.api;
+package org.apache.solr.security.jwt.api;
 
-import java.util.HashMap;
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.api.AnnotatedApi;
 import org.apache.solr.api.Api;
 import org.apache.solr.api.ApiBag;
-import org.apache.solr.security.jwt.api.ModifyJWTAuthPluginConfigAPI;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 public class V2JWTSecurityApiMappingTest extends SolrTestCaseJ4 {
 
