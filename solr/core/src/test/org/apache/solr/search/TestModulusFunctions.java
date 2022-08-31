bbc1ba4010154b752195c8f975f7849ad7e01da0
Dan Rosher <d.rosher@cv-library.co.uk>
2022-08-31 15:00:05 +0100

    added unit test

---
+++ solr/core/src/test/org/apache/solr/search/TestModulusFunctions.java
@@ -0,0 +1,71 @@
+package org.apache.solr.search;
+
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestModulusFunctions extends SolrTestCaseJ4 {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        initCore("solrconfig.xml", "schema15.xml");
+    }
+
+    @After
+    public void cleanup() {
+        clearIndex();
+        assertU(optimize());
+    }
+
+    @Test
+    public void testModulus() {
+        int l = 1 << 24;
+        int[] a = new int[]{l - 3, l - 2, l - 1, l, l + 1, l + 2};
+
+        //The existing implementation casts int to float, with 24bit mantissa
+        float[] f = new float[]{1, 2, 0, 1, 1, 0};   //these...
+
+        //The newer implementation casts int to double, with 52bit mantissa
+        double[] d = new double[]{1, 2, 0, 1, 2, 0}; //...differ
+
+        for (int i = 0; i < a.length; i++) {
+            assertEquals("int -> float modulus", f[i], (float) a[i] % 3, 0.0);
+            assertEquals("int -> double modulus", d[i], (double) a[i] % 3, 0.0);
+        }
+    }
+
+    @Test
+    public void testMod() throws Exception {
+        int a = 1 << 24;
+        for (int i = 0, b = a - 3; b < a + 3; b++,i++) {
+            assertU(adoc("id", Integer.toString(i), "foo_i", Integer.toString(b)));
+        }
+
+        assertU(commit());
+        assertJQ(
+                req(
+                        "defType", "lucene",
+                        "q", "*:*",
+                        "fl", "id,foo_i,m:mod(foo_i,3)"),
+
+                "/response/docs/[0]/foo_i==16777213",
+                "/response/docs/[0]/m==1.0",
+
+                "/response/docs/[1]/foo_i==16777214",
+                "/response/docs/[1]/m==2.0",
+
+                "/response/docs/[2]/foo_i==16777215",
+                "/response/docs/[2]/m==0.0",
+
+                "/response/docs/[3]/foo_i==16777216",
+                "/response/docs/[3]/m==1.0",
+
+                "/response/docs/[4]/foo_i==16777217",
+                "/response/docs/[4]/m==2.0",
+
+                "/response/docs/[5]/foo_i==16777218",
+                "/response/docs/[5]/m==0.0");
+    }
+
+}
