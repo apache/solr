c1384e1427a5163824086bc51ef1c1376d0892df
Jason Gerlowski <gerlowskija@apache.org>
2022-09-09 13:13:28 -0400

    Remove unneeded Jersey loggers

--- solr/core/src/java/org/apache/solr/jersey/JerseyApplications.java
+++ solr/core/src/java/org/apache/solr/jersey/JerseyApplications.java
@@ -17,12 +17,12 @@
 
 package org.apache.solr.jersey;
 
-import java.util.Map;
-import javax.inject.Singleton;
 import org.apache.solr.core.PluginBag;
 import org.apache.solr.core.SolrCore;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.inject.Singleton;
 
 /**
  * JAX-RS "application" configurations for Solr's {@link org.apache.solr.core.CoreContainer} and
@@ -60,14 +60,12 @@ public class JerseyApplications {
           });
 
       // Logging - disabled by default but useful for debugging Jersey execution
-      setProperties(
-          Map.of(
-              "jersey.config.server.tracing.type",
-              "ALL",
-              "jersey.config.server.tracing.threshold",
-              "VERBOSE"));
-      register(ApplicationEventLogger.class);
-      register(RequestEventLogger.class);
+//      setProperties(
+//          Map.of(
+//              "jersey.config.server.tracing.type",
+//              "ALL",
+//              "jersey.config.server.tracing.threshold",
+//              "VERBOSE"));
     }
   }
 
