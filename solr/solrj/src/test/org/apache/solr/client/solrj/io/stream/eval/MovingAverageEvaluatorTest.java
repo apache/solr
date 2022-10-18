422affdb01692ad13e6b685a3cfe0fa4e08c8e75
acsbendi <acsbendi28@gmail.com>
2022-10-18 16:12:18 +0200

    Fixed formatting.

--- solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/eval/MovingAverageEvaluatorTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/io/stream/eval/MovingAverageEvaluatorTest.java
@@ -16,6 +16,10 @@
  */
 package org.apache.solr.client.solrj.io.stream.eval;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.solr.SolrTestCase;
 import org.apache.solr.client.solrj.io.Tuple;
 import org.apache.solr.client.solrj.io.eval.MovingAverageEvaluator;
@@ -23,33 +27,26 @@ import org.apache.solr.client.solrj.io.eval.StreamEvaluator;
 import org.apache.solr.client.solrj.io.stream.expr.StreamFactory;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public class MovingAverageEvaluatorTest extends SolrTestCase {
 
-    StreamFactory factory;
-    Map<String, Object> values;
+  StreamFactory factory;
+  Map<String, Object> values;
 
-    public MovingAverageEvaluatorTest() {
-        super();
+  public MovingAverageEvaluatorTest() {
+    super();
 
-        factory =
-                new StreamFactory()
-                        .withFunctionName("movingAvg", MovingAverageEvaluator.class);
-        values = new HashMap<>();
-    }
+    factory = new StreamFactory().withFunctionName("movingAvg", MovingAverageEvaluator.class);
+    values = new HashMap<>();
+  }
 
-    @Test
-    public void doesNotFailWithEmptyList() throws Exception {
-        StreamEvaluator evaluator = factory.constructEvaluator("movingAvg(a,30)");
-        Object result;
+  @Test
+  public void doesNotFailWithEmptyList() throws Exception {
+    StreamEvaluator evaluator = factory.constructEvaluator("movingAvg(a,30)");
+    Object result;
 
-        values.clear();
-        values.put("a", new ArrayList<Double>());
-        result = evaluator.evaluate(new Tuple(values));
-        assertEquals(Collections.emptyList(), result);
-    }
+    values.clear();
+    values.put("a", new ArrayList<Double>());
+    result = evaluator.evaluate(new Tuple(values));
+    assertEquals(Collections.emptyList(), result);
+  }
 }
