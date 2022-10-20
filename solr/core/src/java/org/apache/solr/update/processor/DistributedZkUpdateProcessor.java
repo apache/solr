cf83f1293c7a513fd99ee29c99e4b56b7b831077
Kevin Risden <krisden@apache.org>
2022-10-20 17:23:16 -0400

    SOLR-16427: Evaluate and fix errorprone rules - JavaLangClash

--- solr/core/src/java/org/apache/solr/update/processor/DistributedZkUpdateProcessor.java
+++ solr/core/src/java/org/apache/solr/update/processor/DistributedZkUpdateProcessor.java
@@ -1261,12 +1261,12 @@ public class DistributedZkUpdateProcessor extends DistributedUpdateProcessor {
     // send in a background thread
 
     cmdDistrib.finish();
-    List<SolrCmdDistributor.Error> errors = cmdDistrib.getErrors();
+    List<SolrCmdDistributor.SolrError> errors = cmdDistrib.getErrors();
     // TODO - we may need to tell about more than one error...
 
-    List<SolrCmdDistributor.Error> errorsForClient = new ArrayList<>(errors.size());
+    List<SolrCmdDistributor.SolrError> errorsForClient = new ArrayList<>(errors.size());
     Set<String> replicasShouldBeInLowerTerms = new HashSet<>();
-    for (final SolrCmdDistributor.Error error : errors) {
+    for (final SolrCmdDistributor.SolrError error : errors) {
 
       if (error.req.node instanceof SolrCmdDistributor.ForwardNode) {
         // if it's a forward, any fail is a problem -
