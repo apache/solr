119586f00c183515db49fadec6e7baa942ff7ac6
Joshua Ouma <joshuaoumag@gmail.com>
2022-10-26 21:16:44 +0300

    Revert "Revert "SOLR-16427: Evaluate and fix errorprone rules - AlmostJavadoc""
    This reverts commit f74802b6d050170357b7c17517d63e9448eca76c.

--- solr/prometheus-exporter/src/java/org/apache/solr/prometheus/exporter/MetricsQueryTemplate.java
+++ solr/prometheus-exporter/src/java/org/apache/solr/prometheus/exporter/MetricsQueryTemplate.java
@@ -23,21 +23,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MetricsQueryTemplate {
-  /*
-  A regex with named groups is used to match template references to template + vars using the basic pattern:
-
-      $jq:<TEMPLATE>( <UNIQUE>, <KEYSELECTOR>, <METRIC>, <TYPE> )
-
-  For instance,
-
-      $jq:core(requests_total, endswith(".requestTimes"), count, COUNTER)
-
-  TEMPLATE = core
-  UNIQUE = requests_total (unique suffix for this metric, results in a metric named "solr_metrics_core_requests_total")
-  KEYSELECTOR = endswith(".requestTimes") (filter to select the specific key for this metric)
-  METRIC = count
-  TYPE = COUNTER
-  */
+  /**
+   * A regex with named groups is used to match template references to template + vars using the
+   * basic pattern:
+   *
+   * <p>$jq:<TEMPLATE>( <UNIQUE>, <KEYSELECTOR>, <METRIC>, <TYPE> )
+   *
+   * <p>For instance,
+   *
+   * <p>$jq:core(requests_total, endswith(".requestTimes"), count, COUNTER)
+   *
+   * <p>TEMPLATE = core UNIQUE = requests_total (unique suffix for this metric, results in a metric
+   * named "solr_metrics_core_requests_total") KEYSELECTOR = endswith(".requestTimes") (filter to
+   * select the specific key for this metric) METRIC = count TYPE = COUNTER
+   */
   private static final Pattern matchJqTemplate =
       Pattern.compile(
           "^\\$jq:(?<TEMPLATE>.*?)\\(\\s?(?<UNIQUE>[^,]*),\\s?(?<KEYSELECTOR>[^,]*)(,\\s?(?<METRIC>[^,]*)\\s?)?(,\\s?(?<TYPE>[^,]*)\\s?)?\\)$");
