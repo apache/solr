e85550f730fd94f2f679ff5fb2cfaf4d684cdb37
Joshua Ouma <joshuaoumag@gmail.com>
2022-10-27 00:48:27 +0300

    Removed unused imports

--- solr/core/src/test/org/apache/solr/handler/admin/api/ReplaceNodeAPITest.java
+++ solr/core/src/test/org/apache/solr/handler/admin/api/ReplaceNodeAPITest.java
@@ -16,8 +16,6 @@
  */
 package org.apache.solr.handler.admin.api;
 
-import static org.apache.solr.SolrTestCaseJ4.assumeWorkingMockito;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
