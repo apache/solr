84faa265050027977890f71d33b1ee8e9fb45ce7
Joshua Ouma <joshuaoumag@gmail.com>
2022-10-28 15:37:12 +0300

    Added a method for getting files

--- solr/core/src/java/org/apache/solr/handler/admin/api/ZookeeperAPI.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/ZookeeperAPI.java
@@ -17,18 +17,130 @@
 
 package org.apache.solr.handler.admin.api;
 
-import javax.inject.Inject;
-import javax.ws.rs.Path;
-import org.apache.solr.api.JerseyResource;
-import org.apache.solr.core.CoreContainer;
+import static org.apache.solr.client.solrj.impl.BinaryResponseParser.BINARY_CONTENT_TYPE_V2;
+import static org.apache.solr.common.params.CommonParams.OMIT_HEADER;
+import static org.apache.solr.common.params.CommonParams.PATH;
+import static org.apache.solr.common.params.CommonParams.WT;
+import static org.apache.solr.security.PermissionNameProvider.Name.ZK_READ_PERM;
 
-@Path("/cluster/zookeeper/")
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import org.apache.solr.api.JerseyResource;
+import org.apache.solr.cloud.ZkController;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
+import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.core.CoreContainer;
+import org.apache.solr.handler.admin.ZookeeperInfoHandler.FilterType;
+import org.apache.solr.handler.admin.ZookeeperInfoHandler.PageOfCollections;
+import org.apache.solr.handler.admin.ZookeeperInfoHandler.PagedCollectionSupport;
+import org.apache.solr.handler.admin.ZookeeperInfoHandler.ZKPrinter;
+import org.apache.solr.jersey.PermissionName;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.RawResponseWriter;
+import org.apache.solr.response.SolrQueryResponse;
+
+@Path("/cluster/zookeeper")
 public class ZookeeperAPI extends JerseyResource {
 
   private final CoreContainer coreContainer;
+  private final SolrQueryRequest solrQueryRequest;
+  private final SolrQueryResponse solrQueryResponse;
+
+  private PagedCollectionSupport pagingSupport;
 
   @Inject
-  public ZookeeperAPI(CoreContainer coreContainer) {
+  public ZookeeperAPI(
+      CoreContainer coreContainer,
+      SolrQueryRequest solrQueryRequest,
+      SolrQueryResponse solrQueryResponse) {
     this.coreContainer = coreContainer;
+    this.solrQueryRequest = solrQueryRequest;
+    this.solrQueryResponse = solrQueryResponse;
+  }
+
+  @GET
+  @Path("/files")
+  @Produces({"application/json", "application/xml", BINARY_CONTENT_TYPE_V2})
+  @PermissionName(ZK_READ_PERM)
+  public NamedList<Object> listZookeeperFiles() throws Exception {
+    // final ZookeeperFilesResponse response =
+    // instantiateJerseyResponse(ZookeeperFilesResponse.class);
+    final SolrParams params = solrQueryRequest.getParams();
+    Map<String, String> map = new HashMap<>(1);
+    map.put(WT, "raw");
+    map.put(OMIT_HEADER, "true");
+    solrQueryRequest.setParams(SolrParams.wrapDefaults(new MapSolrParams(map), params));
+    synchronized (this) {
+      if (pagingSupport == null) {
+        pagingSupport = new PagedCollectionSupport();
+        ZkController zkController = coreContainer.getZkController();
+        if (zkController != null) {
+          // get notified when the ZK session expires (so we can clear the cached collections and
+          // rebuild)
+          zkController.addOnReconnectListener(pagingSupport);
+        }
+      }
+    }
+
+    String path = params.get(PATH);
+
+    if (params.get("addr") != null) {
+      throw new SolrException(ErrorCode.BAD_REQUEST, "Illegal parameter \"addr\"");
+    }
+
+    boolean detail = false;
+
+    String dumpS = params.get("dump");
+    boolean dump = dumpS != null && dumpS.equals("true");
+
+    int start = params.getInt("start", 0); // Note start ignored if rows not specified
+    int rows = params.getInt("rows", -1);
+
+    String filterType = params.get("filterType");
+    if (filterType != null) {
+      filterType = filterType.trim().toLowerCase(Locale.ROOT);
+      if (filterType.length() == 0) filterType = null;
+    }
+    FilterType type = (filterType != null) ? FilterType.valueOf(filterType) : FilterType.none;
+
+    String filter = (type != FilterType.none) ? params.get("filter") : null;
+    if (filter != null) {
+      filter = filter.trim();
+      if (filter.length() == 0) filter = null;
+    }
+
+    ZKPrinter printer = new ZKPrinter(coreContainer.getZkController());
+    printer.detail = detail;
+    printer.dump = dump;
+    boolean isGraphView = "graph".equals(params.get("view"));
+    // There is no znode /clusterstate.json (removed in Solr 9), but we do as if there's one and
+    // return collection listing. Need to change services.js if cleaning up here, collection list is
+    // used from Admin UI Cloud - Graph
+    boolean paginateCollections = (isGraphView && "/clusterstate.json".equals(path));
+    printer.page = paginateCollections ? new PageOfCollections(start, rows, type, filter) : null;
+    printer.pagingSupport = pagingSupport;
+
+    try {
+      if (paginateCollections) {
+        // List collections and allow pagination, but no specific znode info like when looking at a
+        // normal ZK path
+        printer.printPaginatedCollections();
+      } else {
+        printer.print(path);
+      }
+    } finally {
+      printer.close();
+    }
+    solrQueryResponse.getValues().add(RawResponseWriter.CONTENT, printer);
+    return solrQueryResponse.getValues();
   }
 }
