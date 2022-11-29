bb85e65193814cf3f5283ab24fa0f9f671ba9bf0
Eric Pugh <epugh@opensourceconnections.com>
2022-11-29 13:29:30 -0500

    Revert "extra try catch we don't need, clarify that we use longer timeouts"
    This reverts commit f855e7ca5133ed5cb67f55d4f36aab58ab8008f6.

--- solr/core/src/test/org/apache/solr/handler/ReplicationTestHelper.java
+++ solr/core/src/test/org/apache/solr/handler/ReplicationTestHelper.java
@@ -66,8 +66,12 @@ public final class ReplicationTestHelper {
   }
 
   public static SolrClient createNewSolrClient(String baseUrl) {
-    // set up the client wiht longer timeouts...
-    return SolrTestCaseJ4.getHttpSolrClient(baseUrl, 15000, 90000);
+    try {
+      // set up the client...
+      return SolrTestCaseJ4.getHttpSolrClient(baseUrl, 15000, 90000);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
   }
 
   public static int index(SolrClient s, Object... fields) throws Exception {
