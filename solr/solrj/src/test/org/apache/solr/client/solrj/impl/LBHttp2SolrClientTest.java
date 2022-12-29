e7e116da0276e96bc267f530afb6c889bf7a10e8
Eric Pugh <epugh@opensourceconnections.com>
2022-12-29 07:20:16 -0500

    polish test with names

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/LBHttp2SolrClientTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/LBHttp2SolrClientTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.solr.client.solrj.impl;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.solr.SolrTestCase;
@@ -34,34 +33,34 @@ public class LBHttp2SolrClientTest extends SolrTestCase {
    * immutable SolrClient
    */
   @Test
-  public void testLBHttp2SolrClientSetQueryParams() throws IOException {
+  public void testLBHttp2SolrClientSetQueryParams() {
     String url = "http://127.0.0.1:8080";
-    Set<String> queryParams = new HashSet<>(2);
-    queryParams.add("param1");
+    Set<String> urlParamNames = new HashSet<>(2);
+    urlParamNames.add("param1");
 
     try (Http2SolrClient http2SolrClient =
-            new Http2SolrClient.Builder(url).withTheseParamNamesInTheUrl(queryParams).build();
+            new Http2SolrClient.Builder(url).withTheseParamNamesInTheUrl(urlParamNames).build();
         LBHttp2SolrClient testClient =
             new LBHttp2SolrClient.Builder(http2SolrClient, url).build()) {
 
       assertArrayEquals(
-          "Wrong queryParams found in lb client.",
-          queryParams.toArray(),
+          "Wrong urlParamNames found in lb client.",
+          urlParamNames.toArray(),
           testClient.getUrlParamNames().toArray());
       assertArrayEquals(
-          "Wrong queryParams found in base client.",
-          queryParams.toArray(),
+          "Wrong urlParamNames found in base client.",
+          urlParamNames.toArray(),
           http2SolrClient.getUrlParamNames().toArray());
 
       testClient.addQueryParams("param2");
-      queryParams.add("param2");
+      urlParamNames.add("param2");
       assertArrayEquals(
-          "Wrong queryParams found in lb client.",
-          queryParams.toArray(),
-          testClient.getQueryParams().toArray());
+          "Wrong urlParamNames found in lb client.",
+          urlParamNames.toArray(),
+          testClient.getUrlParamNames().toArray());
       assertArrayEquals(
-          "Wrong queryParams found in base client.",
-          queryParams.toArray(),
+          "Wrong urlParamNames found in base client.",
+          urlParamNames.toArray(),
           http2SolrClient.getUrlParamNames().toArray());
     }
   }
