ac578775e39dc0fc4abd89a1bc1bfe9424533e3c
Eric Pugh <epugh@opensourceconnections.com>
2022-12-31 17:11:47 -0500

    SOLR-10470: setParallelCacheRefreshes should be deprecated in favor of SolrClientBuilder methods (#1248)
    Introduce Builder setter for parallelCacheRefreshes on cloud SolrClients.  Deprecated direct setter setParallelCacheRefreshes on cloud SolrClients.  Inlined logic for creating object locks.

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrClient.java
@@ -126,7 +126,7 @@ public abstract class CloudSolrClient extends SolrClient {
 
   }
 
-  private volatile List<Object> locks = objectList(3);
+  protected volatile List<Object> locks = objectList(3);
 
   /** Constructs {@link CloudSolrClient} instances from provided configuration. */
   public static class Builder extends CloudHttp2SolrClient.Builder {
@@ -1222,7 +1222,10 @@ public abstract class CloudSolrClient extends SolrClient {
   /**
    * If caches are expired they are refreshed after acquiring a lock. use this to set the number of
    * locks
+   *
+   * @deprecated use {@link CloudHttp2SolrClient.Builder#setParallelCacheRefreshes(int)} instead
    */
+  @Deprecated
   public void setParallelCacheRefreshes(int n) {
     locks = objectList(n);
   }
