1d4b5207fd261b406e2d6595e48fbf78dcf63e6e
Jason Gerlowski <gerlowskija@apache.org>
2023-01-13 10:18:06 -0500

    SOLR-16347: Skip JerseyApp creation when v2 disabled
    Prior to this commit, the "disable.v2.api" flag covered some aspects of
    request routing, but wasn't consulted during requestHandler
    parsing/bootstrapping or CoreContainer initialization.
    
    This commit creates a small utility method in V2ApiUtils to simplify
    the enabled/disabled check, and uses it in a few additional places to
    make sure Solr isn't doing any unnecessary work when v2 has explicitly
    been turned off.

--- solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
+++ solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
@@ -50,6 +50,7 @@ import org.apache.solr.common.util.SuppressForbidden;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.NodeRoles;
 import org.apache.solr.core.SolrCore;
+import org.apache.solr.handler.api.V2ApiUtils;
 import org.apache.solr.logging.MDCLoggingContext;
 import org.apache.solr.logging.MDCSnapshot;
 import org.apache.solr.security.AuditEvent;
@@ -94,7 +95,7 @@ public class SolrDispatchFilter extends BaseSolrFilter implements PathExcluder {
 
   private List<Pattern> excludePatterns;
 
-  public final boolean isV2Enabled = !"true".equals(System.getProperty("disable.v2.api", "false"));
+  public final boolean isV2Enabled = V2ApiUtils.isEnabled();
 
   public HttpClient getHttpClient() {
     try {
