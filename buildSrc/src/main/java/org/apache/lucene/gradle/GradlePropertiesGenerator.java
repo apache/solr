a888803fc8dc4f5a1b25fd7fef16b72538234020
Colvin Cowie <colvin.cowie.dev@gmail.com>
2023-02-01 20:27:25 +0000

    Fix up formatting and move localSettings help into the properties file

--- buildSrc/src/main/java/org/apache/lucene/gradle/GradlePropertiesGenerator.java
+++ buildSrc/src/main/java/org/apache/lucene/gradle/GradlePropertiesGenerator.java
@@ -17,80 +17,70 @@
 package org.apache.lucene.gradle;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 import java.util.stream.Collectors;
-
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static java.nio.file.StandardOpenOption.APPEND;
 
 /**
  * Standalone class that generates a populated gradle.properties from a template.
- * <p>
- * Has no dependencies outside of standard java libraries
+ *
+ * <p>Has no dependencies outside of standard java libraries
  */
 public class GradlePropertiesGenerator {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.err.println("Usage: java GradlePropertiesGenerator.java <source> <destination>");
-            System.exit(2);
-        }
-
-        try {
-            new GradlePropertiesGenerator().run(Paths.get(args[0]), Paths.get(args[1]));
-        } catch (Exception e) {
-            System.err.println("ERROR: " + e.getMessage());
-            System.exit(3);
-        }
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.err.println("Usage: java GradlePropertiesGenerator.java <source> <destination>");
+      System.exit(2);
     }
 
-    public void run(Path source, Path destination) throws IOException {
-        if (!Files.exists(source)) {
-            throw new IOException("template file not found: " + source);
-        }
-        if (Files.exists(destination)) {
-            System.out.println(destination + " already exists, skipping generation.");
-        }
-
-        // Approximate a common-sense default for running gradle/tests with parallel
-        // workers: half the count of available cpus but not more than 12.
-        var cpus = Runtime.getRuntime().availableProcessors();
-        var maxWorkers = (int) Math.max(1d, Math.min(cpus * 0.5d, 12));
-        var testsJvms = (int) Math.max(1d, Math.min(cpus * 0.5d, 12));
-
-        var replacements = Map.of("org.gradle.workers.max", maxWorkers, "tests.jvms", testsJvms);
-
-        // Java properties doesn't preserve comments, so going line by line instead
-        // The properties file isn't big, nor is the map of replacements, so not worrying about speed here.
-        System.out.println("Generating gradle.properties");
-        final var lines = Files.readAllLines(source).stream().map(line -> {
-            if (!line.startsWith("#") && line.contains("=")) {
-                final String key = line.split("=")[0];
-                var value = replacements.get(key);
-                if (value != null) {
-                  final String newLine = key + '=' + value;
-                  System.out.println("Setting " + newLine);
-                  return newLine;
-              }
-            }
-            return line;
-        }).collect(Collectors.toList());
-
-        Files.write(destination, lines, StandardOpenOption.CREATE_NEW);
+    try {
+      new GradlePropertiesGenerator().run(Paths.get(args[0]), Paths.get(args[1]));
+    } catch (Exception e) {
+      System.err.println("ERROR: " + e.getMessage());
+      System.exit(3);
     }
+  }
+
+  public void run(Path source, Path destination) throws IOException {
+    if (!Files.exists(source)) {
+      throw new IOException("template file not found: " + source);
+    }
+    if (Files.exists(destination)) {
+      System.out.println(destination + " already exists, skipping generation.");
+    }
+
+    // Approximate a common-sense default for running gradle/tests with parallel
+    // workers: half the count of available cpus but not more than 12.
+    var cpus = Runtime.getRuntime().availableProcessors();
+    var maxWorkers = (int) Math.max(1d, Math.min(cpus * 0.5d, 12));
+    var testsJvms = (int) Math.max(1d, Math.min(cpus * 0.5d, 12));
+
+    var replacements = Map.of("org.gradle.workers.max", maxWorkers, "tests.jvms", testsJvms);
+
+    // Java properties doesn't preserve comments, so going line by line instead
+    // The properties file isn't big, nor is the map of replacements, so not worrying about speed
+    // here.
+    System.out.println("Generating gradle.properties");
+    final var lines =
+        Files.readAllLines(source).stream()
+            .map(
+                line -> {
+                  if (!line.startsWith("#") && line.contains("=")) {
+                    final String key = line.split("=")[0];
+                    var value = replacements.get(key);
+                    if (value != null) {
+                      final String newLine = key + '=' + value;
+                      System.out.println("Setting " + newLine);
+                      return newLine;
+                    }
+                  }
+                  return line;
+                })
+            .collect(Collectors.toList());
+
+    Files.write(destination, lines, StandardOpenOption.CREATE_NEW);
+  }
 }
