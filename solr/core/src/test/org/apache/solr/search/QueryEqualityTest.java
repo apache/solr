ef6bcfe00ebb1e795f98b24c89a353b335763155
Mikhail Khludnev <mkhludnev@users.noreply.github.com>
2023-02-04 13:46:33 +0300

    fix

--- solr/core/src/test/org/apache/solr/search/QueryEqualityTest.java
+++ solr/core/src/test/org/apache/solr/search/QueryEqualityTest.java
@@ -1072,8 +1072,8 @@ public class QueryEqualityTest extends SolrTestCaseJ4 {
   public void testFuncIsnan() throws Exception {
     SolrQueryRequest req = req("num", "12.3456", "zero", "0");
     try {
-      assertFuncEquals("isnan(12.3456)","isnan(12.3456)", "isnan($num)");
-      assertFuncEquals("isnan(div(0,0))", "isnan(div($zero,$zero))");
+      assertFuncEquals(req, "isnan(12.3456)", "isnan(12.3456)", "isnan($num)");
+      assertFuncEquals(req, "isnan(div(0,0))", "isnan(div($zero,$zero))");
     } finally {
       req.close();
     }
