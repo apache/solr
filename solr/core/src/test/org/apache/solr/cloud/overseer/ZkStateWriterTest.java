1c17ec796066f66d30664bef0724b755955f87f3
Noble Paul <noble.paul@gmail.com>
2023-02-05 22:53:24 +1100

    do not use deprecated ctor

--- solr/core/src/test/org/apache/solr/cloud/overseer/ZkStateWriterTest.java
+++ solr/core/src/test/org/apache/solr/cloud/overseer/ZkStateWriterTest.java
@@ -227,20 +227,31 @@ public class ZkStateWriterTest extends SolrTestCaseJ4 {
         ZkWriteCommand c1 =
             new ZkWriteCommand(
                 "c1",
-                new DocCollection("c1", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0));
+                new DocCollection(
+                    "c1", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0, null));
         ZkWriteCommand c2 =
             new ZkWriteCommand(
                 "c2",
-                new DocCollection("c2", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0));
+                new DocCollection(
+                    "c2", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0, null));
         ZkWriteCommand c3 =
             new ZkWriteCommand(
                 "c3",
-                new DocCollection("c3", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0));
+                new DocCollection(
+                    "c3", new HashMap<>(), new HashMap<>(), DocRouter.DEFAULT, 0, null));
         Map<String, Object> prsProps = new HashMap<>();
         prsProps.put("perReplicaState", Boolean.TRUE);
         ZkWriteCommand prs1 =
             new ZkWriteCommand(
-                "prs1", new DocCollection("prs1", new HashMap<>(), prsProps, DocRouter.DEFAULT, 0));
+                "prs1",
+                new DocCollection(
+                    "prs1",
+                    new HashMap<>(),
+                    prsProps,
+                    DocRouter.DEFAULT,
+                    0,
+                    new PerReplicaStatesFetcher.LazyPrsSupplier(
+                        zkClient, DocCollection.getCollectionPath("prs1"))));
         ZkStateWriter writer = new ZkStateWriter(reader, new Stats());
 
         // First write is flushed immediately
