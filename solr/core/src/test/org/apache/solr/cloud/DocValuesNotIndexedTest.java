94ad03f3454d21b31be3e8a0bef8542c4609c1e7
Eric Pugh <epugh@opensourceconnections.com>
2023-02-19 20:36:23 -0500

    tidy

--- solr/core/src/test/org/apache/solr/cloud/DocValuesNotIndexedTest.java
+++ solr/core/src/test/org/apache/solr/cloud/DocValuesNotIndexedTest.java
@@ -154,7 +154,7 @@ public class DocValuesNotIndexedTest extends SolrCloudTestCase {
         multiUpdateRequest.process(cluster.getSolrClient(), COLLECTION);
     assertNull("Error adding fields", multipleUpdatesResponse.getResponse().get("errors"));
 
-    //cluster.getSolrClient().setDefaultCollection(COLLECTION);
+    // cluster.getSolrClient().setDefaultCollection(COLLECTION);
   }
 
   @AfterClass
@@ -165,7 +165,7 @@ public class DocValuesNotIndexedTest extends SolrCloudTestCase {
   @Before
   public void clean() throws IOException, SolrServerException {
     CloudSolrClient client = cluster.getSolrClient();
-    client.deleteByQuery(COLLECTION,"*:*");
+    client.deleteByQuery(COLLECTION, "*:*");
     client.commit(COLLECTION);
     resetFields(fieldsToTestSingle);
     resetFields(fieldsToTestMulti);
