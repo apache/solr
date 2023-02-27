cf8dee1978652903ac0ba3258de6ac146f9329dc
Kevin Risden <krisden@apache.org>
2023-02-27 15:41:18 -0500

    SOLR-16427: Evaluate and fix errorprone rules - part 5 - MutablePublicArray

--- solr/solrj/src/java/org/apache/solr/common/util/StrUtils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/StrUtils.java
@@ -27,6 +27,7 @@ import org.apache.solr.common.SolrException;
 
 /** */
 public class StrUtils {
+  @SuppressWarnings("MutablePublicArray")
   public static final char[] HEX_DIGITS = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
   };
