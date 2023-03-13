d35f257d0398471a871e7a9f2095800be491b721
Jason Gerlowski <gerlowskija@apache.org>
2023-03-13 12:37:16 -0400

    Make tidy happy

--- solr/core/src/java/org/apache/solr/handler/admin/api/CollectionPropertyAPI.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/CollectionPropertyAPI.java
@@ -28,7 +28,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import org.apache.solr.common.cloud.CollectionProperties;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.jersey.JacksonReflectMapWriter;
