f8e61ba5c524197d1e11af21354c730b159167a2
patsonluk <patson@fullstory.com>
2023-03-13 13:02:49 -0700

    Adding Breakpoint injection to CommonTestInjection

--- solr/solrj/src/java/org/apache/solr/common/util/CommonTestInjection.java
+++ solr/solrj/src/java/org/apache/solr/common/util/CommonTestInjection.java
@@ -19,6 +19,9 @@ package org.apache.solr.common.util;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +35,7 @@ public class CommonTestInjection {
 
   private static volatile Map<String, String> additionalSystemProps = null;
   private static volatile Integer delay = null;
+  private final static ConcurrentMap<String, Breakpoint> breakpoints = new ConcurrentHashMap<>();
 
   public static void reset() {
     additionalSystemProps = null;
@@ -72,5 +76,63 @@ public class CommonTestInjection {
       }
     }
     return true;
+  }
+
+  /**
+   * This should ONLY be set from unit test cases.
+   *
+   * If defined, code execution would break at certain code execution point at the invocation of injectBreakpoint
+   * with matching key until the provided method in the {@link Breakpoint} implementation is executed.
+   *
+   * Setting the breakpoint to null would remove the breakpoint
+   *
+   * @see CommonTestInjection#injectBreakpoint(String)
+   * @param key could simply be the fully qualified class name or more granular like class name + other id
+   *    (such as method name). This should batch the key used in {@link Breakpoint#injectBreakpoint(String)}
+   * @param breakpoint  The Breakpoint implementation, null to remove the breakpoint
+   */
+  public static void setBreakpoint(String key, Breakpoint breakpoint) {
+    if (breakpoint != null) {
+      breakpoints.put(key, breakpoint);
+    } else {
+      breakpoints.remove(key);
+    }
+  }
+
+  /**
+   * Injects a breakpoint that pauses the existing code execution, executes the code defined in the breakpoint
+   * implementation and then resumes afterwards. The breakpoint implementation is looked up by the corresponding key
+   * used in {@link CommonTestInjection#setBreakpoint(String, Breakpoint)}
+   *
+   * An example usages :
+   * <ol>
+   *   <li>Inject a precise wait until a race condition is fulfilled before proceeding with original code execution</li>
+   *   <li>Inject a flag to catch exception statement which handles the exception without re-throwing. This could verify
+   *   caught exception does get triggered</li>
+   * </ol>
+   *
+   *
+   *
+   * This should always be a part of an assert statement (ie assert injectBreakpoint(key)) such that it will be skipped
+   * for normal code execution
+   *
+   * @see CommonTestInjection#setBreakpoint(String, Breakpoint)
+   * @param key could simply be the fully qualified class name or more granular like class name + other id
+   *    (such as method name). This should only be set by corresponding unit test cases with CommonTestInjection#setBreakpoint
+   */
+  public static boolean injectBreakpoint(String key) {
+    Breakpoint breakpoint = breakpoints.get(key);
+    if (breakpoint != null) {
+      breakpoint.executeAndResume();
+    }
+    return true;
+  }
+
+  public interface Breakpoint {
+    /**
+     * Code execution should break at where the breakpoint was injected, then it would execute this method and
+     * resumes the execution afterwards.
+     */
+    void executeAndResume();
   }
 }
