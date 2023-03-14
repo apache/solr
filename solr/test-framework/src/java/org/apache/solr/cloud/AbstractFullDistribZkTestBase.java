f5b600c43c72b5638e77a10eacf28c04ec540635
Eric Pugh <epugh@opensourceconnections.com>
2023-03-14 07:27:22 -0400

    fix test failures

--- solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/AbstractFullDistribZkTestBase.java
@@ -2292,24 +2292,23 @@ public abstract class AbstractFullDistribZkTestBase extends AbstractDistribZkTes
   }
 
   protected CloudSolrClient getSolrClientForCollection(String collectionName) {
-
-    return solrClientForCollectionCache.computeIfAbsent(
-        collectionName,
-        k -> {
-          CloudSolrClient solrClient =
-              getCloudSolrClient(
-                  zkServer.getZkAddress(), collectionName, random().nextBoolean(), 5000, 120000);
-          solrClient.connect();
-          solrClientForCollectionCache.put(collectionName, solrClient);
-          if (log.isInfoEnabled()) {
-            log.info(
-                "Created solrClient for collection {} with updatesToLeaders={} and parallelUpdates={}",
-                collectionName,
-                solrClient.isUpdatesToLeaders(),
-                solrClient.isParallelUpdates());
-          }
-          return solrClient;
-        });
+    synchronized (this) {
+      if (!solrClientForCollectionCache.containsKey(collectionName)) {
+        CloudSolrClient solrClient =
+            getCloudSolrClient(
+                zkServer.getZkAddress(), collectionName, random().nextBoolean(), 5000, 120000);
+        solrClient.connect();
+        solrClientForCollectionCache.put(collectionName, solrClient);
+        if (log.isInfoEnabled()) {
+          log.info(
+              "Created solrClient for collection {} with updatesToLeaders={} and parallelUpdates={}",
+              collectionName,
+              solrClient.isUpdatesToLeaders(),
+              solrClient.isParallelUpdates());
+        }
+      }
+    }
+    return solrClientForCollectionCache.get(collectionName);
   }
 
   public static String getUrlFromZk(ClusterState clusterState, String collection) {
