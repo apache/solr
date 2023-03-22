ae176548f20183402e7fa6726f11de9ddb2c31c8
Marcus Eagan (marcussorealheis) <marcuseagan@gmail.com>
2023-03-22 00:29:53 -0700

    add some license action.

--- solr/solr-ui-login/README.md
+++ solr/solr-ui-login/README.md
@@ -1,3 +1,4 @@
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- solr/solr-ui-login/src/AuthenticationService.js
+++ solr/solr-ui-login/src/AuthenticationService.js
@@ -1,15 +1,46 @@
-// AuthenticationService.js
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const SOLR_HOST_URL = process.env.REACT_APP_SOLR_HOST; 
+const SOLR_PORT = process.env.REACT_APP_SOLR_PORT;
 
 export const AuthenticationService = {
     login: async function(username, password) {
-      // Implement the logic to log in using the given username and password.
-      // This might involve calling an API endpoint and handling the response.
+        // Note: this direction would only work for basic auth. 
+        // Need to do more thinking on other auth classes and dependencies like session storage.
+        const response = await fetch(`${SOLR_HOST_URL}:${SOLR_PORT}/solr/admin/authentication`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+    
+        // If the response is successful, store the JWT in local storage.
+        if (response.ok) {
+            return true;
+            // force users to http://localhost:8983/solr/#/home
+        } else {
+            return false
+        }
     },
   
     logout: function() {
-      // Implement the logic to log out the user.
+        localStorage.removeItem("jwt");
     },
   
-    // ... other authentication-related functions, such as JWT handling and redirection
   };
   
