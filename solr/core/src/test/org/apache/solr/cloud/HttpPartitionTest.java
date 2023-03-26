a6b2748dd2af5e68e21d046e1b574b2c8cd2e089
Eric Pugh <epugh@opensourceconnections.com>
2023-03-26 09:07:32 -0400

    lint

--- solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/HttpPartitionTest.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.lucene.tests.util.LuceneTestCase;
 import org.apache.solr.JSONTestUtil;
 import org.apache.solr.SolrTestCaseJ4.SuppressSSL;
