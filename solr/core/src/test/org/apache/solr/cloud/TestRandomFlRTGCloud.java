9d509044238297b0b2fe7bcb7aef946f3f3034f1
Kevin Risden <krisden@apache.org>
2023-03-29 15:08:13 -0400

    Simplify CLIENTS by computing client as needed

--- solr/core/src/test/org/apache/solr/cloud/TestRandomFlRTGCloud.java
+++ solr/core/src/test/org/apache/solr/cloud/TestRandomFlRTGCloud.java
@@ -31,6 +31,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -53,7 +54,9 @@ import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.IOUtils;
 import org.apache.solr.common.util.NamedList;
+import org.apache.solr.common.util.Pair;
 import org.apache.solr.embedded.JettySolrRunner;
 import org.apache.solr.response.transform.DocTransformer;
 import org.apache.solr.response.transform.RawValueTransformerFactory;
@@ -76,9 +79,8 @@ public class TestRandomFlRTGCloud extends SolrCloudTestCase {
 
   /** A collection specific client for operations at the cloud level */
   private static CloudSolrClient COLLECTION_CLIENT;
-  /** Per node, we have a map of clients using specific writerTypes, keyed by wt */
-  private static final List<Map<String, SolrClient>> CLIENTS =
-      Collections.synchronizedList(new ArrayList<>(5));
+  /** We have a map of clients using specific writerTypes, keyed by pair of baseurl and wt */
+  private static final Map<Pair<String, String>, SolrClient> CLIENTS = new ConcurrentHashMap<>();
 
   /** Always included in fl, so we can check what doc we're looking at */
   private static final FlValidator ID_VALIDATOR = new SimpleFieldValueValidator("id");
@@ -175,18 +177,6 @@ public class TestRandomFlRTGCloud extends SolrCloudTestCase {
         .process(COLLECTION_CLIENT);
 
     cluster.waitForActiveCollection(COLLECTION_NAME, numShards, repFactor * numShards);
-
-    for (JettySolrRunner jetty : cluster.getJettySolrRunners()) {
-      Map<String, SolrClient> solrClientByWriterType = new HashMap<>();
-      List<String> writerTypes = Arrays.asList("javabin", "json", "xml");
-      for (String wt : writerTypes) {
-        HttpSolrClient.Builder builder =
-            new HttpSolrClient.Builder(jetty.getBaseUrl() + "/" + COLLECTION_NAME + "/");
-        builder = withResponseParser(builder, wt);
-        solrClientByWriterType.put(wt, builder.build());
-      }
-      CLIENTS.add(solrClientByWriterType);
-    }
   }
 
   @AfterClass
@@ -195,10 +185,8 @@ public class TestRandomFlRTGCloud extends SolrCloudTestCase {
       COLLECTION_CLIENT.close();
       COLLECTION_CLIENT = null;
     }
-    for (Map<String, SolrClient> solrClientByWriterType : CLIENTS) {
-      for (SolrClient client : solrClientByWriterType.values()) {
-        client.close();
-      }
+    for (SolrClient client : CLIENTS.values()) {
+      IOUtils.closeQuietly(client);
     }
     CLIENTS.clear();
   }
@@ -648,32 +636,31 @@ public class TestRandomFlRTGCloud extends SolrCloudTestCase {
    * in our cluster. This method doesn't care which wt is defined.
    */
   public static SolrClient getRandomClient(Random rand) {
-    int numClients = CLIENTS.size();
-    int idx = TestUtil.nextInt(rand, 0, numClients);
-    if (idx == numClients) {
-      // Return the CloudSolrClient, it only uses javabin writerType.
-      return COLLECTION_CLIENT;
-    } else {
-      // Grabbing the first
-      int rnd = random().nextInt(CLIENTS.get(idx).values().toArray().length);
-      return (SolrClient) CLIENTS.get(idx).values().toArray()[rnd];
-    }
+    List<String> writerTypes = List.of("javabin", "json", "xml");
+    int writerTypeIdx = TestUtil.nextInt(rand, 0, writerTypes.size() - 1);
+    return getRandomClient(rand, writerTypes.get(writerTypeIdx));
   }
   /**
    * returns a random SolrClient -- either a CloudSolrClient, or an HttpSolrClient pointed at a node
    * in our cluster. We have different CLIENTS created for each node based on their wt setting.
    */
   public static SolrClient getRandomClient(Random rand, String wt) {
-    int numClients = CLIENTS.size();
+    List<JettySolrRunner> jettySolrRunners = cluster.getJettySolrRunners();
+    int numClients = jettySolrRunners.size();
     int idx = TestUtil.nextInt(rand, 0, numClients);
-    // return (idx == numClients) ? COLLECTION_CLIENT : CLIENTS.get(idx);
     if (idx == numClients) {
-      // Return the CloudSolrClient that only ever uses javabin.
+      // Return the CloudSolrClient, it only uses javabin writerType.
       return COLLECTION_CLIENT;
     } else {
-      // Pick from the set of node clients by the random idx,
-      // and then select the matching wt configured SolrClient.
-      return CLIENTS.get(idx).get(wt);
+      JettySolrRunner jetty = jettySolrRunners.get(idx);
+      String jettyBaseUrl = jetty.getBaseUrl().toString();
+      return CLIENTS.computeIfAbsent(
+          new Pair<>(jettyBaseUrl, wt),
+          k ->
+              withResponseParser(
+                      new HttpSolrClient.Builder(jetty.getBaseUrl() + "/" + COLLECTION_NAME + "/"),
+                      wt)
+                  .build());
     }
   }
 
