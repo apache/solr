8350b56013ebb5bda485e4016abcb469cdbb4cb0
Noble Paul <noble.paul@gmail.com>
2023-04-10 23:24:24 +1000

    removed unused comments

--- solr/solrj/src/java/org/apache/solr/common/util/Utils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/Utils.java
@@ -216,13 +216,6 @@ public class Utils {
   public static byte[] toJSON(Object o) {
     if (o == null) return new byte[0];
     CharArr out = new CharArr();
-    //    if (!(o instanceof List) && !(o instanceof Map)) {
-    //      if (o instanceof MapWriter) {
-    //        o = ((MapWriter) o).toMap(new LinkedHashMap<>());
-    //      } else if (o instanceof IteratorWriter) {
-    //        o = ((IteratorWriter) o).toList(new ArrayList<>());
-    //      }
-    //    }
     new MapWriterJSONWriter(out, 2).write(o); // indentation by default
     return toUTF8(out);
   }
