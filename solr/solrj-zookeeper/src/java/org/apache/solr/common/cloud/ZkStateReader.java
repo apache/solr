3ff627b437c1ef7af08eb75c79b74211cae3c333
Noble Paul <noblepaul@users.noreply.github.com>
2023-04-12 10:54:29 +1000

    SOLR-16741: CLUSTERSTATUS API returns wrong value for state ,leader for PRS collections (#1549)

--- solr/solrj-zookeeper/src/java/org/apache/solr/common/cloud/ZkStateReader.java
+++ solr/solrj-zookeeper/src/java/org/apache/solr/common/cloud/ZkStateReader.java
@@ -84,7 +84,7 @@ public class ZkStateReader implements SolrCloseable {
   public static final String STATE_PROP = "state";
   // if this flag equals to false and the replica does not exist in cluster state, set state op
   // become no op (default is true)
-  public static final String FORCE_SET_STATE_PROP = "force_set_state";
+  public static final String FORCE_SET_STATE_PROP = Replica.ReplicaStateProps.FORCE_SET_STATE;
   /** SolrCore name. */
   public static final String CORE_NAME_PROP = "core";
 
