8067caa5ba6f096ca7e3fc7f5e6313c45926eed2
Jason Gerlowski <gerlowskija@apache.org>
2023-04-19 10:35:06 -0400

    Make tidy happy

--- solr/core/src/java/org/apache/solr/handler/admin/api/CreateCollectionAPI.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/CreateCollectionAPI.java
@@ -392,7 +392,9 @@ public class CreateCollectionAPI extends AdminAPIBase {
     }
 
     public void validate() {
-      if (replicationFactor != null && nrtReplicas != null && (!replicationFactor.equals(nrtReplicas))) {
+      if (replicationFactor != null
+          && nrtReplicas != null
+          && (!replicationFactor.equals(nrtReplicas))) {
         throw new SolrException(
             SolrException.ErrorCode.BAD_REQUEST,
             "Cannot specify both replicationFactor and nrtReplicas as they mean the same thing");
