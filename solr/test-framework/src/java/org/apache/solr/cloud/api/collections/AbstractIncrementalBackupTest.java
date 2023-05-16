e8ac46eb782ed492b34be4c02cbdf553bfb92963
Jason Gerlowski <gerlowskija@apache.org>
2023-05-16 12:17:27 -0400

    SOLR-16394: Tweak backup listing, deletion to be more REST-ful (#1639)
    This commit tweaks the v2 bindings for our "delete" and "list backups" APIs to be
    more intuitive for users.  DELETEBACKUP now has two v2 bindings: one to delete
    backups by ID (`DELETE /api/backups/backupName/versions/<id>`), and one to
    delete all but the most recent 'N' backups
    (`DELETE /api/backups/backupName/versions?retainLatest=<N>`).
    
    Backup listing is now available at `GET /api/backups/backupName/versions`

--- solr/test-framework/src/java/org/apache/solr/cloud/api/collections/AbstractIncrementalBackupTest.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/api/collections/AbstractIncrementalBackupTest.java
@@ -57,7 +57,6 @@ import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.cloud.Replica;
 import org.apache.solr.common.cloud.Slice;
 import org.apache.solr.common.cloud.ZkStateReader;
-import org.apache.solr.common.util.NamedList;
 import org.apache.solr.core.DirectoryFactory;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.core.TrackingBackupRepository;
@@ -227,6 +226,7 @@ public abstract class AbstractIncrementalBackupTest extends SolrCloudTestCase {
     assertEquals(firstBatchNumDocs, getNumDocsInCollection(backupCollectionName));
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   @Nightly
   public void testBackupIncremental() throws Exception {
@@ -318,7 +318,8 @@ public abstract class AbstractIncrementalBackupTest extends SolrCloudTestCase {
               .setRepositoryName(BACKUP_REPO_NAME)
               .setLocation(backupLocation)
               .process(cluster.getSolrClient());
-      assertEquals(2, ((NamedList) resp.getResponse().get("deleted")).get("numIndexFiles"));
+      assertEquals(
+          2, ((Map<String, Object>) resp.getResponse().get("deleted")).get("numIndexFiles"));
 
       new UpdateRequest().deleteByQuery("*:*").commit(cluster.getSolrClient(), getCollectionName());
       indexDocs(getCollectionName(), false);
