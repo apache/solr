1c3b89196c221bef65881be4b706b15f8d656d6a
Noble Paul <noblepaul@users.noreply.github.com>
2023-06-07 14:35:38 +1000

    use STRINGREF feauture (#1695)

--- solr/core/src/test/org/apache/solr/util/TestCborDataFormat.java
+++ solr/core/src/test/org/apache/solr/util/TestCborDataFormat.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
+import com.fasterxml.jackson.dataformat.cbor.CBORGenerator;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -196,7 +197,7 @@ public class TestCborDataFormat extends SolrCloudTestCase {
 
     byte[] b = Files.readAllBytes(filmsJson);
     byte[] bytes = serializeToCbor(b);
-    assertEquals(290672, bytes.length);
+    assertEquals(210439, bytes.length);
     LongAdder docsSz = new LongAdder();
     new CborLoader(null, (document) -> docsSz.increment()).stream(new ByteArrayInputStream(bytes));
     assertEquals(films.size(), docsSz.intValue());
@@ -209,9 +210,10 @@ public class TestCborDataFormat extends SolrCloudTestCase {
     // Read JSON file as a JsonNode
     JsonNode jsonNode = jsonMapper.readTree(is);
     // Create a CBOR ObjectMapper
-    CBORFactory jf = new CBORFactory();
-    ObjectMapper cborMapper = new ObjectMapper(jf);
     baos = new ByteArrayOutputStream();
+
+    ObjectMapper cborMapper =
+        new ObjectMapper(CBORFactory.builder().enable(CBORGenerator.Feature.STRINGREF).build());
     JsonGenerator jsonGenerator = cborMapper.createGenerator(baos);
 
     jsonGenerator.writeTree(jsonNode);
