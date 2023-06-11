65d30073353dda77bf6d5cd7bb19345738876ac9
Eric Pugh <epugh@opensourceconnections.com>
2023-06-11 13:04:37 -0400

    lint

--- solr/core/src/java/org/apache/solr/cli/ConfigTool.java
+++ solr/core/src/java/org/apache/solr/cli/ConfigTool.java
@@ -48,13 +48,13 @@ public class ConfigTool extends ToolBase {
   @Override
   public List<Option> getOptions() {
     return List.of(
-            Option.builder("c")
-                    .longOpt("name")
-                    .argName("NAME")
-                    .hasArg()
-                    .required(true)
-                    .desc("Name of the collection.")
-                    .build(),
+        Option.builder("c")
+            .longOpt("name")
+            .argName("NAME")
+            .hasArg()
+            .required(true)
+            .desc("Name of the collection.")
+            .build(),
         Option.builder("action")
             .argName("ACTION")
             .hasArg()
