436befe83308f1395ad7bd326abd5edbf9f2676b
Noble Paul <noble.paul@gmail.com>
2023-07-05 22:01:23 +1000

    tidy

--- solr/core/src/java/org/apache/solr/servlet/CoordinatorHttpSolrCall.java
+++ solr/core/src/java/org/apache/solr/servlet/CoordinatorHttpSolrCall.java
@@ -124,7 +124,6 @@ public class CoordinatorHttpSolrCall extends HttpSolrCall {
   /**
    * Overrides the MDC context as the core set was synthetic core, which does not reflect the
    * collection being operated on
-   *
    */
   private static void setMDCLoggingContext(String collectionName) {
     MDCLoggingContext.setCollection(collectionName);
