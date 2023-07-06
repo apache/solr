2a2bd77cf29371aac72ff685bae3b4c0199b91ab
Eric Pugh <epugh@opensourceconnections.com>
2023-07-06 13:58:45 -0400

    add post to the help output

--- solr/core/src/java/org/apache/solr/cli/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/cli/SolrCLI.java
@@ -424,7 +424,7 @@ public class SolrCLI implements CLIO {
 
     print("Usage: solr COMMAND OPTIONS");
     print(
-        "       where COMMAND is one of: start, stop, restart, status, healthcheck, create, create_core, create_collection, delete, version, zk, auth, assert, config, export, api, package");
+        "       where COMMAND is one of: start, stop, restart, status, healthcheck, create, create_core, create_collection, delete, version, zk, auth, assert, config, export, api, package, post");
     print("");
     print("  Standalone server example (start Solr running in the background on port 8984):");
     print("");
