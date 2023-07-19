c76925bf60118195792635be84f3fb804cf39af7
Noble Paul <noble.paul@gmail.com>
2023-07-19 16:03:04 +1000

    Revert "Avoid creating a replica as a part of collection creation"
    This reverts commit 381bf1f9b6bdd28599f10bdedb7a8b15656274e5.
    
    wrong branch

--- solr/core/src/java/org/apache/solr/servlet/CoordinatorHttpSolrCall.java
+++ solr/core/src/java/org/apache/solr/servlet/CoordinatorHttpSolrCall.java
@@ -250,7 +250,7 @@ public class CoordinatorHttpSolrCall extends HttpSolrCall {
     SolrQueryResponse rsp = new SolrQueryResponse();
     try {
       CollectionAdminRequest.Create collCreationRequest =
-          CollectionAdminRequest.createCollection(syntheticCollectionName, confName, 1, 0);
+          CollectionAdminRequest.createCollection(syntheticCollectionName, confName, 1,0);
       collCreationRequest.setWaitForFinalState(true);
       SolrParams params = collCreationRequest.getParams();
       if (log.isInfoEnabled()) {
