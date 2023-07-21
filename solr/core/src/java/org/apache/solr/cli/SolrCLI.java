729b98cf6d3158c8a6f4767c6b1ce765097d54e6
Eric Pugh <epugh@opensourceconnections.com>
2023-07-21 10:25:52 -0400

    fix checking logic to avoid NPE

--- solr/core/src/java/org/apache/solr/cli/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/cli/SolrCLI.java
@@ -120,8 +120,10 @@ public class SolrCLI implements CLIO {
 
   /** Runs a tool. */
   public static void main(String[] args) throws Exception {
-    final boolean hasNoCommand = args == null || args.length == 0 || args[0] == null || args[0].trim().length() == 0;
-    final boolean isHelpCommand = Arrays.asList("-h", "--help", "/?").contains(args[0]);
+    final boolean hasNoCommand =
+        args == null || args.length == 0 || args[0] == null || args[0].trim().length() == 0;
+    final boolean isHelpCommand =
+        !hasNoCommand && Arrays.asList("-h", "--help", "/?").contains(args[0]);
 
     if (hasNoCommand || isHelpCommand) {
       printHelp();
