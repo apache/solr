1fb10b4fb8b5e0beab0c83645b185c8d6eac9828
David Smiley <dsmiley@salesforce.com>
2023-08-21 08:58:11 -0400

    Stop using SolrTestCaseHS

--- solr/core/src/test/org/apache/solr/search/json/TestJsonRequestWithEdismaxDefType.java
+++ solr/core/src/test/org/apache/solr/search/json/TestJsonRequestWithEdismaxDefType.java
@@ -15,64 +15,49 @@
  * limitations under the License.
  */
 package org.apache.solr.search.json;
-import java.io.File;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+
+import static org.apache.solr.SolrTestCaseJ4.params;
+import static org.apache.solr.SolrTestCaseJ4.sdoc;
+
 import org.apache.lucene.tests.util.LuceneTestCase;
-import org.apache.solr.JSONTestUtil;
-import org.apache.solr.SolrTestCaseHS;
+import org.apache.solr.SolrTestCase;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
+import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.cloud.ConfigRequest;
-import org.apache.solr.util.SolrClientTestRule;
-import org.apache.solr.util.ExternalPaths;
 import org.apache.solr.util.EmbeddedSolrServerTestRule;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.apache.solr.util.ExternalPaths;
+import org.apache.solr.util.SolrClientTestRule;
 import org.junit.ClassRule;
-import org.junit.Test;
-import java.nio.file.Path;
 
+public class TestJsonRequestWithEdismaxDefType extends SolrTestCase {
 
-public class TestJsonRequestWithEdismaxDefType extends SolrTestCaseHS {
-  private static final String COLLECTION_NAME = "collection1";
-  private static final int NUM_TECHPRODUCTS_DOCS = 32;
-  private static final int NUM_IN_STOCK = 17;
-  private static final int NUM_ELECTRONICS = 12;
-  private static final int NUM_CURRENCY = 4;
-  private static final int NUM_MEMORY = 3;
-  private static final int NUM_CORSAIR = 3;
-  private static final int NUM_BELKIN = 2;
-  private static final int NUM_CANON = 2;
   @ClassRule
   public static final SolrClientTestRule solrClientTestRule = new EmbeddedSolrServerTestRule();
-  
+
   public void test() throws Exception {
     solrClientTestRule.startSolr(LuceneTestCase.createTempDir());
-    
 
-    solrClientTestRule
-        .newCollection(COLLECTION_NAME)
-        .withConfigSet(ExternalPaths.TECHPRODUCTS_CONFIGSET)
-        .create();
+    solrClientTestRule.newCollection().withConfigSet(ExternalPaths.TECHPRODUCTS_CONFIGSET).create();
 
-    SolrClient client = solrClientTestRule.getSolrClient(COLLECTION_NAME);
+    SolrClient client = solrClientTestRule.getSolrClient();
 
-    client.request(new ConfigRequest("{"
-        + "  'update-requesthandler':{"
-        + "    'name':'/query',"
-        + "    'class':'solr.SearchHandler',"
-        + "    'defaults' : {'defType':'edismax'}"
-        + "  }"
-        + "}"));
+    client.request(
+        new ConfigRequest(
+            "{"
+                + "  'update-requesthandler':{"
+                + "    'name':'/query',"
+                + "    'class':'solr.SearchHandler',"
+                + "    'defaults' : {'defType':'edismax'}"
+                + "  }"
+                + "}"));
 
     addDocs(client);
 
     doQuery(client);
   }
-  
+
   private static void addDocs(SolrClient client) throws Exception {
-    //client.deleteByQuery("*:*");
     client.add(sdoc("id", "1", "cat_s", "A", "where_s", "NY"));
     client.add(sdoc("id", "2", "cat_s", "B", "where_s", "NJ"));
     client.add(sdoc("id", "3"));
@@ -83,28 +68,12 @@ public class TestJsonRequestWithEdismaxDefType extends SolrTestCaseHS {
     client.add(sdoc("id", "6", "cat_s", "B", "where_s", "NY"));
     client.commit();
   }
-  
+
   private static void doQuery(SolrClient client) throws Exception {
     final var jsonQuery =
         "{\"query\":{\"bool\":{\"should\":[{\"lucene\":{\"query\":\"id:1\"}}, \"id:2\"]}}}";
-    SolrTestCaseHS.assertJQ(client, params("json", jsonQuery, "qt", "/query"), "response/numFound==2");
+    final var req = new QueryRequest(params("json", jsonQuery, "qt", "/query"), METHOD.POST);
+    final var rsp = req.process(client);
+    assertEquals(2, rsp.getResults().getNumFound());
   }
-
-  // write an elevation config file to boost some docs
-  private void writeElevationConfigFile(File file, String query, String... ids) throws Exception {
-    try (PrintWriter out =
-        new PrintWriter(Files.newBufferedWriter(file.toPath(), StandardCharsets.UTF_8))) {
-      out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
-      out.println("<elevate>");
-      out.println("<query text=\"" + query + "\">");
-      for (String id : ids) {
-        out.println(" <doc id=\"" + id + "\"/>");
-      }
-      out.println("</query>");
-      out.println("</elevate>");
-      out.flush();
-    }
-
-  }
-
 }
