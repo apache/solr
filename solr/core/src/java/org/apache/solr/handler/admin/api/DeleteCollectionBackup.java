37a14d23039714ef3cc3864d6c44f72eecc1d3df
Jason Gerlowski <gerlowskija@apache.org>
2023-08-24 07:59:08 -0400

    SOLR-16825: Migrate v2 definitions to 'api' module (#1859)
    Since Solr's OAS (OpenApi Spec) is now generated prior to compiling
    SolrJ, it can only contain those APIs defined in our new 'api'
    module.
    
    This commit moves a number of v2 API definitions to this new module,
    including:
      - delete replica property
      - delete replica
      - delete (i.e. "clear") node
      - delete collection snapshot
      - delete collection backup
    
    Moving these APIs also means that a v2 SolrRequest implementation is
    generated for each.

--- solr/core/src/java/org/apache/solr/handler/admin/api/DeleteCollectionBackup.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/DeleteCollectionBackup.java
@@ -17,7 +17,6 @@
 
 package org.apache.solr.handler.admin.api;
 
-import static org.apache.solr.client.solrj.impl.BinaryResponseParser.BINARY_CONTENT_TYPE_V2;
 import static org.apache.solr.cloud.Overseer.QUEUE_OPERATION;
 import static org.apache.solr.common.SolrException.ErrorCode.BAD_REQUEST;
 import static org.apache.solr.common.params.CommonAdminParams.ASYNC;
@@ -30,7 +29,6 @@ import static org.apache.solr.common.params.CoreAdminParams.MAX_NUM_BACKUP_POINT
 import static org.apache.solr.common.params.CoreAdminParams.NAME;
 import static org.apache.solr.security.PermissionNameProvider.Name.COLL_EDIT_PERM;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,14 +36,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import javax.inject.Inject;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import org.apache.solr.client.api.endpoint.DeleteCollectionBackupApi;
+import org.apache.solr.client.api.model.BackupDeletionData;
+import org.apache.solr.client.api.model.BackupDeletionResponseBody;
+import org.apache.solr.client.api.model.PurgeUnusedFilesRequestBody;
+import org.apache.solr.client.api.model.PurgeUnusedResponse;
 import org.apache.solr.client.api.model.SolrJerseyResponse;
-import org.apache.solr.client.api.model.SubResponseAccumulatingJerseyResponse;
 import org.apache.solr.client.solrj.SolrResponse;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.cloud.ZkNodeProps;
@@ -54,7 +50,6 @@ import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.SimpleOrderedMap;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.handler.api.V2ApiUtils;
-import org.apache.solr.jersey.JacksonReflectMapWriter;
 import org.apache.solr.jersey.PermissionName;
 import org.apache.solr.jersey.SolrJacksonMapper;
 import org.apache.solr.request.SolrQueryRequest;
@@ -65,12 +60,12 @@ import org.apache.solr.response.SolrQueryResponse;
  *
  * <p>These APIs are equivalent to the v1 '/admin/collections?action=DELETEBACKUP' command.
  */
-public class DeleteCollectionBackupAPI extends BackupAPIBase {
+public class DeleteCollectionBackup extends BackupAPIBase implements DeleteCollectionBackupApi {
 
   private final ObjectMapper objectMapper;
 
   @Inject
-  public DeleteCollectionBackupAPI(
+  public DeleteCollectionBackup(
       CoreContainer coreContainer,
       SolrQueryRequest solrQueryRequest,
       SolrQueryResponse solrQueryResponse) {
@@ -79,17 +74,10 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
     this.objectMapper = SolrJacksonMapper.getObjectMapper();
   }
 
-  @Path("/backups/{backupName}/versions/{backupId}")
-  @DELETE
-  @Produces({"application/json", "application/xml", BINARY_CONTENT_TYPE_V2})
+  @Override
   @PermissionName(COLL_EDIT_PERM)
   public BackupDeletionResponseBody deleteSingleBackupById(
-      @PathParam("backupName") String backupName,
-      @PathParam(BACKUP_ID) String backupId,
-      // Optional parameters below
-      @QueryParam(BACKUP_LOCATION) String location,
-      @QueryParam(BACKUP_REPOSITORY) String repositoryName,
-      @QueryParam(ASYNC) String asyncId)
+      String backupName, String backupId, String location, String repositoryName, String asyncId)
       throws Exception {
     final var response = instantiateJerseyResponse(BackupDeletionResponseBody.class);
     recordCollectionForLogAndTracing(null, solrQueryRequest);
@@ -108,17 +96,14 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
     return response;
   }
 
-  @Path("/backups/{backupName}/versions")
-  @DELETE
-  @Produces({"application/json", "application/xml", BINARY_CONTENT_TYPE_V2})
+  @Override
   @PermissionName(COLL_EDIT_PERM)
   public BackupDeletionResponseBody deleteMultipleBackupsByRecency(
-      @PathParam("backupName") String backupName,
-      @QueryParam("retainLatest") Integer versionsToRetain,
-      // Optional parameters below
-      @QueryParam(BACKUP_LOCATION) String location,
-      @QueryParam(BACKUP_REPOSITORY) String repositoryName,
-      @QueryParam(ASYNC) String asyncId)
+      String backupName,
+      Integer versionsToRetain,
+      String location,
+      String repositoryName,
+      String asyncId)
       throws Exception {
     final var response = instantiateJerseyResponse(BackupDeletionResponseBody.class);
     recordCollectionForLogAndTracing(null, solrQueryRequest);
@@ -138,13 +123,10 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
     return response;
   }
 
-  @Path("/backups/{backupName}/purgeUnused")
-  @PUT
-  @Produces({"application/json", "application/xml", BINARY_CONTENT_TYPE_V2})
+  @Override
   @PermissionName(COLL_EDIT_PERM)
   public PurgeUnusedResponse garbageCollectUnusedBackupFiles(
-      @PathParam("backupName") String backupName, PurgeUnusedFilesRequestBody requestBody)
-      throws Exception {
+      String backupName, PurgeUnusedFilesRequestBody requestBody) throws Exception {
     final var response = instantiateJerseyResponse(PurgeUnusedResponse.class);
     recordCollectionForLogAndTracing(null, solrQueryRequest);
 
@@ -163,29 +145,20 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
             Boolean.TRUE,
             requestBody.location,
             requestBody.repositoryName,
-            requestBody.asyncId);
+            requestBody.async);
     final var remoteResponse =
         submitRemoteMessageAndHandleResponse(
             response,
             CollectionParams.CollectionAction.DELETEBACKUP,
             remoteMessage,
-            requestBody.asyncId);
+            requestBody.async);
 
     final Object remoteDeleted = remoteResponse.getResponse().get("deleted");
     if (remoteDeleted != null) {
-      response.deleted = objectMapper.convertValue(remoteDeleted, PurgeUnusedStats.class);
+      response.deleted =
+          objectMapper.convertValue(remoteDeleted, PurgeUnusedResponse.PurgeUnusedStats.class);
     }
     return response;
-  }
-
-  public static class PurgeUnusedResponse extends SubResponseAccumulatingJerseyResponse {
-    @JsonProperty public PurgeUnusedStats deleted;
-  }
-
-  public static class PurgeUnusedStats implements JacksonReflectMapWriter {
-    @JsonProperty public Integer numBackupIds;
-    @JsonProperty public Integer numShardBackupIds;
-    @JsonProperty public Integer numIndexFiles;
   }
 
   public static ZkNodeProps createRemoteMessage(
@@ -231,48 +204,26 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
               BACKUP_ID));
     }
 
-    final var deleteApi = new DeleteCollectionBackupAPI(coreContainer, req, rsp);
+    final var deleteApi = new DeleteCollectionBackup(coreContainer, req, rsp);
     V2ApiUtils.squashIntoSolrResponseWithoutHeader(rsp, invokeApi(deleteApi, req.getParams()));
   }
 
-  public static class BackupDeletionResponseBody extends SubResponseAccumulatingJerseyResponse {
-    @JsonProperty public String collection;
-    @JsonProperty public List<CreateCollectionBackupAPI.BackupDeletionData> deleted;
-  }
-
   @SuppressWarnings("unchecked")
-  public static List<CreateCollectionBackupAPI.BackupDeletionData> fromRemoteResponse(
+  public static List<BackupDeletionData> fromRemoteResponse(
       ObjectMapper objectMapper, SolrResponse response) {
     final var deleted = (List<SimpleOrderedMap<Object>>) response.getResponse().get("deleted");
     if (deleted == null) {
       return null;
     }
 
-    final List<CreateCollectionBackupAPI.BackupDeletionData> statList = new ArrayList<>();
+    final List<BackupDeletionData> statList = new ArrayList<>();
     for (SimpleOrderedMap<Object> remoteStat : deleted) {
-      statList.add(
-          objectMapper.convertValue(
-              remoteStat, CreateCollectionBackupAPI.BackupDeletionData.class));
+      statList.add(objectMapper.convertValue(remoteStat, BackupDeletionData.class));
     }
     return statList;
   }
 
-  /**
-   * Request body for the {@link DeleteCollectionBackupAPI#garbageCollectUnusedBackupFiles(String,
-   * PurgeUnusedFilesRequestBody)} API.
-   */
-  public static class PurgeUnusedFilesRequestBody implements JacksonReflectMapWriter {
-    @JsonProperty(BACKUP_LOCATION)
-    public String location;
-
-    @JsonProperty(BACKUP_REPOSITORY)
-    public String repositoryName;
-
-    @JsonProperty(ASYNC)
-    public String asyncId;
-  }
-
-  private static SolrJerseyResponse invokeApi(DeleteCollectionBackupAPI api, SolrParams params)
+  private static SolrJerseyResponse invokeApi(DeleteCollectionBackup api, SolrParams params)
       throws Exception {
     if (params.get(MAX_NUM_BACKUP_POINTS) != null) {
       return api.deleteMultipleBackupsByRecency(
@@ -285,7 +236,7 @@ public class DeleteCollectionBackupAPI extends BackupAPIBase {
       final var requestBody = new PurgeUnusedFilesRequestBody();
       requestBody.location = params.get(BACKUP_LOCATION);
       requestBody.repositoryName = params.get(BACKUP_REPOSITORY);
-      requestBody.asyncId = params.get(ASYNC);
+      requestBody.async = params.get(ASYNC);
       return api.garbageCollectUnusedBackupFiles(params.get(NAME), requestBody);
     } else { // BACKUP_ID != null
       return api.deleteSingleBackupById(
