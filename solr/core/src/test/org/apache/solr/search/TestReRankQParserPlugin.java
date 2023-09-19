33fc6991b6dece2dd23c641400e2b325745c2a1e
Joel Bernstein <jbernste@apache.org>
2023-09-19 10:38:41 -0400

    SOLR-16931: ReRankScaler explain breaks with debug=true and in distributed mode (#1936)

--- solr/core/src/test/org/apache/solr/search/TestReRankQParserPlugin.java
+++ solr/core/src/test/org/apache/solr/search/TestReRankQParserPlugin.java
@@ -1522,7 +1522,7 @@ public class TestReRankQParserPlugin extends SolrTestCaseJ4 {
     params.add("start", "0");
     params.add("rows", "6");
     params.add("df", "text");
-    params.add("debugQuery", "true");
+    params.add("debug", "true");
     assertQ(
         req(params),
         "*[count(//doc)=6]",
