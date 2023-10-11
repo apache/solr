b74fcaf7ce283aca30ebc85a1498226c3ebfea2d
Jason Gerlowski <gerlowskija@apache.org>
2023-10-11 14:14:21 -0400

    SOLR-16825: Migrate v2 definitions to 'api' module, pt 6 (#1978)
    This commit covers various GET /schema APIss well as delete-shard.
    
    Extracting annotated interfaces for these APIs includes them in the SolrRequest-
    generation we now do in SolrJ

--- solr/api/src/java/org/apache/solr/client/api/model/SchemaNameResponse.java
+++ solr/api/src/java/org/apache/solr/client/api/model/SchemaNameResponse.java
@@ -14,18 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.solr.client.api.model;
 
-package org.apache.solr.client.api.util;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Constants {
-  private Constants() {
-    /* Private ctor prevents instantiation */
-  }
-
-  public static final String STORE_TYPE_PATH_PARAMETER = "storeType";
-  public static final String STORE_NAME_PATH_PARAMETER = "storeName";
-  public static final String STORE_PATH_PREFIX =
-      "/{" + STORE_TYPE_PATH_PARAMETER + ":cores|collections}/{" + STORE_NAME_PATH_PARAMETER + "}";
-
-  public static final String BINARY_CONTENT_TYPE_V2 = "application/vnd.apache.solr.javabin";
+public class SchemaNameResponse extends SolrJerseyResponse {
+  @JsonProperty("name")
+  public String name;
 }
--- solr/core/src/java/org/apache/solr/handler/admin/api/GetSchema.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/GetSchema.java
@@ -17,45 +17,95 @@
 
 package org.apache.solr.handler.admin.api;
 
-import static org.apache.solr.client.solrj.impl.BinaryResponseParser.BINARY_CONTENT_TYPE_V2;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.lang.invoke.MethodHandles;
 import javax.inject.Inject;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 import org.apache.solr.api.JerseyResource;
-import org.apache.solr.client.api.model.SolrJerseyResponse;
+import org.apache.solr.client.api.endpoint.GetSchemaApi;
+import org.apache.solr.client.api.model.SchemaInfoResponse;
+import org.apache.solr.client.api.model.SchemaNameResponse;
+import org.apache.solr.client.api.model.SchemaSimilarityResponse;
+import org.apache.solr.client.api.model.SchemaUniqueKeyResponse;
+import org.apache.solr.client.api.model.SchemaVersionResponse;
+import org.apache.solr.client.api.model.SchemaZkVersionResponse;
 import org.apache.solr.cloud.ZkSolrResourceLoader;
+import org.apache.solr.common.SolrException;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.jersey.PermissionName;
+import org.apache.solr.schema.IndexSchema;
 import org.apache.solr.schema.ManagedIndexSchema;
 import org.apache.solr.schema.ZkIndexSchemaReader;
 import org.apache.solr.security.PermissionNameProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Path("/{a:cores|collections}/{collectionName}/schema")
-public class GetSchemaZkVersionAPI extends JerseyResource {
+public class GetSchema extends JerseyResource implements GetSchemaApi {
 
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  private SolrCore solrCore;
+
+  protected final IndexSchema indexSchema;
+  protected final SolrCore solrCore;
 
   @Inject
-  public GetSchemaZkVersionAPI(SolrCore solrCore) {
+  public GetSchema(SolrCore solrCore, IndexSchema indexSchema) {
     this.solrCore = solrCore;
+    this.indexSchema = indexSchema;
   }
 
-  @GET
-  @Path("/zkversion")
-  @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML, BINARY_CONTENT_TYPE_V2})
+  @Override
   @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
-  public SchemaZkVersionResponse getSchemaZkVersion(
-      @DefaultValue("-1") @QueryParam("refreshIfBelowVersion") Integer refreshIfBelowVersion)
+  public SchemaInfoResponse getSchemaInfo() {
+    final var response = instantiateJerseyResponse(SchemaInfoResponse.class);
+
+    response.schema = indexSchema.getNamedPropertyValues();
+
+    return response;
+  }
+
+  @Override
+  @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
+  public SchemaNameResponse getSchemaName() throws Exception {
+    final SchemaNameResponse response = instantiateJerseyResponse(SchemaNameResponse.class);
+    if (null == indexSchema.getSchemaName()) {
+      throw new SolrException(SolrException.ErrorCode.NOT_FOUND, "Schema has no name");
+    }
+
+    response.name = indexSchema.getSchemaName();
+    return response;
+  }
+
+  @Override
+  @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
+  public SchemaSimilarityResponse getSchemaSimilarity() {
+    final var response = instantiateJerseyResponse(SchemaSimilarityResponse.class);
+
+    response.similarity = indexSchema.getSimilarityFactory().getNamedPropertyValues();
+
+    return response;
+  }
+
+  @Override
+  @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
+  public SchemaUniqueKeyResponse getSchemaUniqueKey() {
+    final var response = instantiateJerseyResponse(SchemaUniqueKeyResponse.class);
+
+    response.uniqueKey = indexSchema.getUniqueKeyField().getName();
+
+    return response;
+  }
+
+  @Override
+  @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
+  public SchemaVersionResponse getSchemaVersion() {
+    final var response = instantiateJerseyResponse(SchemaVersionResponse.class);
+
+    response.version = indexSchema.getVersion();
+
+    return response;
+  }
+
+  @Override
+  @PermissionName(PermissionNameProvider.Name.SCHEMA_READ_PERM)
+  public SchemaZkVersionResponse getSchemaZkVersion(Integer refreshIfBelowVersion)
       throws Exception {
     final SchemaZkVersionResponse response =
         instantiateJerseyResponse(SchemaZkVersionResponse.class);
@@ -77,10 +127,5 @@ public class GetSchemaZkVersionAPI extends JerseyResource {
     }
     response.zkversion = zkVersion;
     return response;
-  }
-
-  public static class SchemaZkVersionResponse extends SolrJerseyResponse {
-    @JsonProperty("zkversion")
-    public int zkversion;
   }
 }
