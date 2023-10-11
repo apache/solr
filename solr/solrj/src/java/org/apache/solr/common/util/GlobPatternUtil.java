bfe31fa6fb770992bfb6a86b3e32d3c0bd29db88
Justin Sweeney <justinsweeney@fullstory.com>
2023-10-11 10:48:31 -0400

    Tidying code

--- solr/solrj/src/java/org/apache/solr/common/util/GlobPatternUtil.java
+++ solr/solrj/src/java/org/apache/solr/common/util/GlobPatternUtil.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class GlobPatternUtil {
 
-    public static boolean matches(String pattern, String input) {
-        return FileSystems.getDefault().getPathMatcher("glob:" + pattern).matches(Paths.get(input));
-    }
+  public static boolean matches(String pattern, String input) {
+    return FileSystems.getDefault().getPathMatcher("glob:" + pattern).matches(Paths.get(input));
+  }
 }
