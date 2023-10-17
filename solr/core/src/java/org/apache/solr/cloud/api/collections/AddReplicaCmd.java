7d7cd41c2fba44ae611637567715c754529f1946
Vincent P. <vincent.primault@gmail.com>
2023-10-17 18:28:10 +0000

    fix

--- solr/core/src/java/org/apache/solr/cloud/api/collections/AddReplicaCmd.java
+++ solr/core/src/java/org/apache/solr/cloud/api/collections/AddReplicaCmd.java
@@ -39,6 +39,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.apache.solr.client.solrj.cloud.SolrCloudManager;
