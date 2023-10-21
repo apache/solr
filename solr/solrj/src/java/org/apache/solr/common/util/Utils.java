7fe1d3a00fafadccbe8e85d980596c9344a18db9
Florin Babes <florin.babes@emag.ro>
2023-10-21 16:48:33 +0300

    code in Utils reformatted

--- solr/solrj/src/java/org/apache/solr/common/util/Utils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/Utils.java
@@ -222,7 +222,8 @@ public class Utils {
   }
 
   /**
-   * @param indentSize The number of space characters to use as an indent (default 2). 0=newlines   but no spaces, -1=no indent at all.
+   * @param indentSize The number of space characters to use as an indent (default 2). 0=newlines
+   *     but no spaces, -1=no indent at all.
    */
   public static byte[] toJSON(Object o, int indentSize) {
     if (o == null) return new byte[0];
@@ -230,13 +231,15 @@ public class Utils {
     new JSONWriter(out, indentSize).write(o); // indentation by default
     return toUTF8(out);
   }
-  
+
   /**
-   * @param indentSize The number of space characters to use as an indent (default 2). 0=newlines   but no spaces, -1=no indent at all.
+   * @param indentSize The number of space characters to use as an indent (default 2). 0=newlines
+   *     but no spaces, -1=no indent at all.
    */
   public static String toJSONString(Object o, int indentSize) {
     return new String(toJSON(o, indentSize), StandardCharsets.UTF_8);
   }
+
   public static String toJSONString(Object o) {
     return new String(toJSON(o), StandardCharsets.UTF_8);
   }
