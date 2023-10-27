9c5e8bdd579275c17b83a1497175e13e3f300ff6
wwang30 <wei.wang30@walmart.com>
2023-10-27 15:33:26 -0700

    comment

--- solr/solrj/src/java/org/apache/solr/common/params/CommonParams.java
+++ solr/solrj/src/java/org/apache/solr/common/params/CommonParams.java
@@ -289,7 +289,10 @@ public interface CommonParams {
   @Deprecated(since = "9.4")
   String DISABLE_REQUEST_ID = "disableRequestId";
 
-  /** Flag to disable remote stats request at query time */
+  /**
+   *  Parameter to disable the distributed term statistics request for current query when distributed IDF is enabled in solrconfig.xml
+   *  Defaults for 'false' if not specified
+   */
   String DISABLE_DISTRIB_STATS = "disableDistribStats";
 
   /** Request Purpose parameter added to each internal shard request when using debug=track */
