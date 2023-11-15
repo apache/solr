88459bd209035320f1cb5439f71b61026974e5d6
Jason Gerlowski <gerlowskija@apache.org>
2023-11-15 15:12:38 -0500

    SOLR-16835: Add approximate-/select to OAS
    Now that Solr uses its OAS to generate client bindings in multiple
    languages (Java and JavaScript, so far), users of these clients may wish
    to use them to run searches.
    
    Normally, this would require converting the `/select` endpoint to JAX-RS
    so that its inputs and outputs can be described comprehensively in our
    OAS.  However, doing this for `/select` will take some time due to the
    complexity and large degree of configurability the endpoint offers.
    
    This commit works around this limitation by creating an approximation of
    the `/select` endpoint that can appear in our OAS until the API can be
    converted to JAX-RS in earnest.  This will give generated-client users
    access to at least some query functionality in this interim.
    
    The `/select` query parameters supported in this commit were chosen
    mostly arbitrarily.  They may be added to freely as generated-client
    users run into particular needs (e.g. for setting 'faceting'
    parameters).

---
+++ solr/api/src/java/org/apache/solr/client/api/endpoint/SelectApi.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.client.api.endpoint;
+
+import static org.apache.solr.client.api.util.Constants.STORE_PATH_PREFIX;
+
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import org.apache.solr.client.api.model.SolrJerseyResponse;
+import org.apache.solr.client.api.util.StoreApiParameters;
+
+/**
+ * V2 API implementation shim for Solr's querying or otherwise inspecting documents in a core or
+ * collection.
+ *
+ * <p>Due to the complexity and configurability of Solr's '/select' endpoint, this interface doesn't
+ * attempt to be exhaustive in describing /select inputs and outputs. Rather, it exists to give
+ * Solr's OAS (and the clients generated from that) an approximate view of the endpoint until its
+ * inputs and outputs can be understood more fully.
+ */
+@Path(STORE_PATH_PREFIX + "/select")
+public interface SelectApi {
+  @GET
+  @StoreApiParameters
+  @Operation(
+      summary = "Query a Solr core or collection, or otherwise inspect its contents.",
+      tags = {"querying"})
+  SolrJerseyResponse query(
+      @QueryParam("q") String query,
+      @QueryParam("fq") List<String> filterQueries,
+      @QueryParam("fl") String fieldList,
+      @QueryParam("rows") Integer rows);
+}
