1ff010bae2407cb855a43e98229e144c54d335d2
jdyer1 <jdyer@apache.org>
2024-02-06 11:07:32 -0600

    factor jetty dependencies out of SSLConfig

--- solr/solrj/src/java/org/apache/solr/client/solrj/embedded/SSLConfig.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/embedded/SSLConfig.java
@@ -24,13 +24,7 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
  *
  * @see #setUseSSL
  */
-public class SSLConfig {
-  private boolean useSsl;
-  private boolean clientAuth;
-  private String keyStore;
-  private String keyStorePassword;
-  private String trustStore;
-  private String trustStorePassword;
+public class SSLConfig extends SSLConfigBase {
 
   /**
    * NOTE: all other settings are ignored if useSsl is false; trustStore settings are ignored if
@@ -43,45 +37,7 @@ public class SSLConfig {
       String keyStorePassword,
       String trustStore,
       String trustStorePassword) {
-    this.useSsl = useSsl;
-    this.clientAuth = clientAuth;
-    this.keyStore = keyStore;
-    this.keyStorePassword = keyStorePassword;
-    this.trustStore = trustStore;
-    this.trustStorePassword = trustStorePassword;
-  }
-
-  public void setUseSSL(boolean useSsl) {
-    this.useSsl = useSsl;
-  }
-
-  public void setClientAuth(boolean clientAuth) {
-    this.clientAuth = clientAuth;
-  }
-
-  /** All other settings on this object are ignored unless this is true */
-  public boolean isSSLMode() {
-    return useSsl;
-  }
-
-  public boolean isClientAuthMode() {
-    return clientAuth;
-  }
-
-  public String getKeyStore() {
-    return keyStore;
-  }
-
-  public String getKeyStorePassword() {
-    return keyStorePassword;
-  }
-
-  public String getTrustStore() {
-    return trustStore;
-  }
-
-  public String getTrustStorePassword() {
-    return trustStorePassword;
+    super(useSsl, clientAuth, keyStore, keyStorePassword, trustStore, trustStorePassword);
   }
 
   /**
---
+++ solr/solrj/src/java/org/apache/solr/client/solrj/embedded/SSLConfigBase.java
@@ -0,0 +1,54 @@
+package org.apache.solr.client.solrj.embedded;
+
+public class SSLConfigBase {
+    protected boolean useSsl;
+    protected boolean clientAuth;
+    protected String keyStore;
+    protected String keyStorePassword;
+    protected String trustStore;
+    protected String trustStorePassword;
+
+    public SSLConfigBase(boolean useSsl, boolean clientAuth, String keyStore, String keyStorePassword, String trustStore, String trustStorePassword) {
+        this.useSsl = useSsl;
+        this.clientAuth = clientAuth;
+        this.keyStore = keyStore;
+        this.keyStorePassword = keyStorePassword;
+        this.trustStore = trustStore;
+        this.trustStorePassword = trustStorePassword;
+    }
+
+    public void setUseSSL(boolean useSsl) {
+        this.useSsl = useSsl;
+    }
+
+    public void setClientAuth(boolean clientAuth) {
+        this.clientAuth = clientAuth;
+    }
+
+    /**
+     * All other settings on this object are ignored unless this is true
+     */
+    public boolean isSSLMode() {
+        return useSsl;
+    }
+
+    public boolean isClientAuthMode() {
+        return clientAuth;
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public String getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public String getTrustStore() {
+        return trustStore;
+    }
+
+    public String getTrustStorePassword() {
+        return trustStorePassword;
+    }
+}
