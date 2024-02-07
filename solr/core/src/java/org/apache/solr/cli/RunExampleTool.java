34d48ded8446e6f64b4c6bdba85f8f33fa99458a
Eric Pugh <epugh@opensourceconnections.com>
2024-02-07 13:19:33 -0500

    Commit is now the default behavior.

--- solr/core/src/java/org/apache/solr/cli/RunExampleTool.java
+++ solr/core/src/java/org/apache/solr/cli/RunExampleTool.java
@@ -409,7 +409,6 @@ public class RunExampleTool extends ToolBase {
         String[] args =
             new String[] {
               "post",
-              "-commit",
               "-url",
               updateUrl,
               "-type",
