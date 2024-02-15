278a1ad7c623a06b852252f27c1d61e57ac215ac
Christine Poerschke <cpoerschke@apache.org>
2024-02-15 17:30:31 +0000

    more 'needsScores' use (post origin/main merge conflict resolution)

--- solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
+++ solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
@@ -2237,7 +2237,7 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
         MaxScoreCollector maxScoreCollector = null;
         List<Collector> collectors = new ArrayList<>(Arrays.asList(topCollector, setCollector));
 
-        if ((cmd.getFlags() & GET_SCORES) != 0) {
+        if (needScores) {
           maxScoreCollector = new MaxScoreCollector();
           collectors.add(maxScoreCollector);
         }
@@ -2252,9 +2252,7 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
         assert (totalHits == set.size()) || qr.isPartialResults();
 
         topDocs = topCollector.topDocs(0, len);
-        if (cmd.getSort() != null
-            && !(cmd.getQuery() instanceof RankQuery)
-            && (cmd.getFlags() & GET_SCORES) != 0) {
+        if (cmd.getSort() != null && !(cmd.getQuery() instanceof RankQuery) && needScores) {
           TopFieldCollector.populateScores(topDocs.scoreDocs, this, query);
         }
         populateNextCursorMarkFromTopDocs(qr, cmd, topDocs);
@@ -2265,7 +2263,7 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
       } else {
         log.debug("using CollectorManager");
 
-        boolean needMaxScore = (cmd.getFlags() & GET_SCORES) != 0;
+        boolean needMaxScore = needScores;
         SearchResult searchResult =
             searchCollectorManagers(len, cmd, query, true, needMaxScore, true);
         Object[] res = searchResult.result;
@@ -2290,7 +2288,7 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
 
         populateNextCursorMarkFromTopDocs(qr, cmd, topDocs);
         //        if (cmd.getSort() != null && !(cmd.getQuery() instanceof RankQuery) &&
-        // (cmd.getFlags() & GET_SCORES) != 0) {
+        // needScores) {
         //          TopFieldCollector.populateScores(topDocs.scoreDocs, this, query);
         //        }
         // nDocsReturned = topDocs.scoreDocs.length;
