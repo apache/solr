52099130ed81da6efcf60099afdee335ae3fa19b
Rahul Goswami <rahul.goswami0206@gmail.com>
2024-02-29 11:12:42 -0500

    retain parameter.contains(`) check to avoid regex processing when backtick is not present

--- solr/solrj-streaming/src/java/org/apache/solr/client/solrj/io/stream/expr/StreamExpressionParser.java
+++ solr/solrj-streaming/src/java/org/apache/solr/client/solrj/io/stream/expr/StreamExpressionParser.java
@@ -134,14 +134,15 @@ public class StreamExpressionParser {
 
       // If contains ` replace with ", except when ` is escaped in the query as (\`)
       // This allows ` to be used as a quote character
-      // Below replacement regex does a negative lookbehind and replaces ` with " only when NOT
-      // preceded by \
-      parameter = parameter.replaceAll("(?<!\\\\)`", "\"");
-      if (0 == parameter.length()) {
-        throw new IllegalArgumentException(
-            String.format(Locale.ROOT, "'%s' is not a proper named parameter clause", working));
+      if (parameter.contains("`")) {
+        // Below replacement regex does a negative lookbehind and replaces ` with " only when NOT
+        // preceded by \
+        parameter = parameter.replaceAll("(?<!\\\\)`", "\"");
+        if (0 == parameter.length()) {
+          throw new IllegalArgumentException(
+              String.format(Locale.ROOT, "'%s' is not a proper named parameter clause", working));
+        }
       }
-
       namedParameter.setParameter(new StreamExpressionValue(parameter));
     }
 
