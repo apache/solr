d0b0e1efdb19ac2050eaa7214758ff74fd896dff
jdyer1 <jdyer@apache.org>
2024-03-25 13:29:02 -0500

    tidy

--- solr/solrj/src/java/org/apache/solr/client/solrj/util/Cancellable.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/util/Cancellable.java
@@ -18,12 +18,14 @@
 package org.apache.solr.client.solrj.util;
 
 /**
- * The return type for solrJ asynchronous requests, providing a mechanism whereby callers may request cancellation.
+ * The return type for solrJ asynchronous requests, providing a mechanism whereby callers may
+ * request cancellation.
  */
 public interface Cancellable {
 
   /**
-   * Request to cancel the asynchronous request.  This may be a no-op in some situations, for instance, if the request failed or otherwise is complete.
+   * Request to cancel the asynchronous request. This may be a no-op in some situations, for
+   * instance, if the request failed or otherwise is complete.
    */
   void cancel();
 }
