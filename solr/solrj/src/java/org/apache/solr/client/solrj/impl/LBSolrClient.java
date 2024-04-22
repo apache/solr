3edc6875e6038d43264b896fb1fc684c3ec228e3
James Dyer <jdyer@apache.org>
2024-04-22 10:36:57 -0500

    SOLR-17234: LBHttp2SolrClient does not skip "zombie" endpoints (#2411)

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBSolrClient.java
@@ -265,8 +265,7 @@ public abstract class LBSolrClient extends SolrClient {
       while (it.hasNext()) {
         endpoint = it.next();
         // if the server is currently a zombie, just skip to the next one
-        // TODO: zombieServers key is String not Endpoint.
-        EndpointWrapper wrapper = zombieServers.get(endpoint);
+        EndpointWrapper wrapper = zombieServers.get(endpoint.toString());
         if (wrapper != null) {
           final int numDeadServersToTry = req.getNumDeadServersToTry();
           if (numDeadServersToTry > 0) {
@@ -488,8 +487,7 @@ public abstract class LBSolrClient extends SolrClient {
       req.getRequest().setBasePath(baseUrl.toString());
       rsp.rsp = getClient(baseUrl).request(req.getRequest(), (String) null);
       if (isZombie) {
-        // TODO: zombieServers key is String not Endpoint.
-        zombieServers.remove(baseUrl);
+        zombieServers.remove(baseUrl.toString());
       }
     } catch (BaseHttpSolrClient.RemoteExecutionException e) {
       throw e;
