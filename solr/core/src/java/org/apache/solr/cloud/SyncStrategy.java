1e4fd2f313fe3f686eb0809f3d97925f72c267e4
iamsanjay <sanjaydutt.india@yahoo.com>
2024-05-18 16:01:16 +0530

    Revert calling withListener factory

--- solr/core/src/java/org/apache/solr/cloud/SyncStrategy.java
+++ solr/core/src/java/org/apache/solr/cloud/SyncStrategy.java
@@ -363,7 +363,6 @@ public class SyncStrategy {
               try (SolrClient client =
                   new Http2SolrClient.Builder(baseUrl)
                       .withHttpClient(solrClient)
-                      .withListenerFactory(solrClient.getListenerFactory())
                       .withConnectionTimeout(30000, TimeUnit.MILLISECONDS)
                       .withIdleTimeout(120000, TimeUnit.MILLISECONDS)
                       .build()) {
