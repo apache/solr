43475c995f8ffb6024cd7f1003c69b7d2be696ad
Eric Pugh <epugh@opensourceconnections.com>
2024-05-20 20:41:50 -0400

    precommit failures

--- solr/core/src/test/org/apache/solr/handler/component/UBIComponentStreamingQueriesTest.java
+++ solr/core/src/test/org/apache/solr/handler/component/UBIComponentStreamingQueriesTest.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import org.apache.lucene.util.IOUtils;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.request.CollectionAdminRequest;
