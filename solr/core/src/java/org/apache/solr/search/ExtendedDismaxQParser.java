69e876f604dee0a1bf09aa75ec2f3b2fd6d84dab
Andy Webb <andywebb@apache.org>
2024-06-06 17:43:33 +0100

    edismax

--- solr/core/src/java/org/apache/solr/search/ExtendedDismaxQParser.java
+++ solr/core/src/java/org/apache/solr/search/ExtendedDismaxQParser.java
@@ -98,6 +98,9 @@ public class ExtendedDismaxQParser extends QParser {
 
     /** If set to true, stopwords are removed from the query. */
     public static String STOPWORDS = "stopwords";
+
+    /** If set to true, the stop word filter applies even if all terms are stop words */
+    public static String ALWAYS_STOPWORDS = "alwaysStopwords";
   }
 
   private ExtendedDismaxConfiguration config;
@@ -408,7 +411,7 @@ public class ExtendedDismaxQParser extends QParser {
       query = up.parse(mainUserQuery);
 
       if (shouldRemoveStopFilter(config, query)) {
-        // if the query was all stop words, remove none of them
+        // if the query was all stop words, remove none of them (unless alwaysStopwords is set)
         up.setRemoveStopFilter(true);
         query = up.parse(mainUserQuery);
       }
@@ -417,6 +420,9 @@ public class ExtendedDismaxQParser extends QParser {
       up.exceptions = false;
     }
 
+    // query may have become empty if it only contained tokenising characters or due to
+    // stop word removal if alwaysStopwords is set. The query plan becomes +() rather
+    // than MatchNoDocsQuery("") - does this make any difference?
     if (query == null) {
       return null;
     }
@@ -439,11 +445,11 @@ public class ExtendedDismaxQParser extends QParser {
   /**
    * Determines if query should be re-parsed removing the stop filter.
    *
-   * @return true if there are stopwords configured and the parsed query was empty false in any
-   *     other case.
+   * @return true if there are stopwords configured, the alwaysStopwords option hasn't been set
+   *     and the parsed query was empty - return false in any other case.
    */
   protected boolean shouldRemoveStopFilter(ExtendedDismaxConfiguration config, Query query) {
-    return config.stopwords && isEmpty(query);
+    return config.stopwords && !config.alwaysStopwords && isEmpty(query);
   }
 
   private String escapeUserQuery(List<Clause> clauses) {
@@ -1700,6 +1706,8 @@ public class ExtendedDismaxQParser extends QParser {
 
     protected boolean stopwords;
 
+    protected boolean alwaysStopwords;
+
     protected boolean mmAutoRelax;
 
     protected String altQ;
@@ -1749,6 +1757,8 @@ public class ExtendedDismaxQParser extends QParser {
       qslop = solrParams.getInt(DisMaxParams.QS, 0);
 
       stopwords = solrParams.getBool(DMP.STOPWORDS, true);
+
+      alwaysStopwords = solrParams.getBool(DMP.ALWAYS_STOPWORDS, false);
 
       mmAutoRelax = solrParams.getBool(DMP.MM_AUTORELAX, false);
 
