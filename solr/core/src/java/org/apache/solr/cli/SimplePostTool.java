36f9cd8dada287f657937f1606ee557f090619a9
Sanjay Dutt <sanjaydutt@apache.org>
2024-06-21 23:07:21 +0530

    replaced few more usages of URL ctor in classes that were missing from main

--- solr/core/src/java/org/apache/solr/cli/SimplePostTool.java
+++ solr/core/src/java/org/apache/solr/cli/SimplePostTool.java
@@ -111,7 +111,7 @@ public class SimplePostTool {
   int recursive = 0;
   int delay = 0;
   String fileTypes;
-  URL solrUrl;
+  URI solrUrl;
   OutputStream out = null;
   String type;
   String format;
@@ -254,7 +254,7 @@ public class SimplePostTool {
       }
 
       urlStr = SimplePostTool.appendParam(urlStr, params);
-      URL url = new URL(urlStr);
+      URI url = new URI(urlStr);
       String user = null;
       if (url.getUserInfo() != null && url.getUserInfo().trim().length() > 0) {
         user = url.getUserInfo().split(":")[0];
@@ -291,7 +291,7 @@ public class SimplePostTool {
 
       return new SimplePostTool(
           mode, url, auto, type, format, recursive, delay, fileTypes, out, commit, optimize, args);
-    } catch (MalformedURLException e) {
+    } catch (URISyntaxException e) {
       fatal("System Property 'url' is not a valid URL: " + urlStr);
       return null;
     }
@@ -315,7 +315,7 @@ public class SimplePostTool {
    */
   public SimplePostTool(
       String mode,
-      URL url,
+      URI url,
       boolean auto,
       String type,
       String format,
@@ -406,7 +406,7 @@ public class SimplePostTool {
       }
       numPagesPosted = postWebPages(args, 0, out);
       info(numPagesPosted + " web pages indexed.");
-    } catch (MalformedURLException e) {
+    } catch (URISyntaxException e) {
       fatal("Wrong URL trying to append /extract to " + solrUrl);
     }
   }
@@ -708,8 +708,8 @@ public class SimplePostTool {
         PageFetcherResult result = pageFetcher.readPageFromUrl(url);
         if (result.httpStatus == 200) {
           url = (result.redirectUrl != null) ? result.redirectUrl : url;
-          URL postUrl =
-              new URL(
+          URI postUri =
+              URI.create(
                   appendParam(
                       solrUrl.toString(),
                       "literal.id="
@@ -723,7 +723,7 @@ public class SimplePostTool {
                   null,
                   out,
                   result.contentType,
-                  postUrl);
+                  postUri);
           if (success) {
             info("POSTed web resource " + url + " (depth: " + level + ")");
             Thread.sleep(delay * 1000L);
@@ -736,7 +736,7 @@ public class SimplePostTool {
                       new ByteArrayInputStream(
                           content.array(), content.arrayOffset(), content.limit()),
                       result.contentType,
-                      postUrl);
+                      postUri);
               subStack.addAll(children);
             }
           } else {
@@ -877,7 +877,7 @@ public class SimplePostTool {
   public void postFile(File file, OutputStream output, String type) {
     InputStream is = null;
     try {
-      URL url = solrUrl;
+      URI url = solrUrl;
       String suffix = "";
       if (auto) {
         if (type == null) {
@@ -889,7 +889,7 @@ public class SimplePostTool {
         if (type.equals("application/json") && !FORMAT_SOLR.equals(format)) {
           suffix = "/json/docs";
           String urlStr = appendUrlPath(solrUrl, suffix).toString();
-          url = new URL(urlStr);
+          url = URI.create(urlStr);
         } else if (type.equals("application/xml")
             || type.equals("text/csv")
             || type.equals("application/json")) {
@@ -908,7 +908,7 @@ public class SimplePostTool {
                 appendParam(
                     urlStr, "literal.id=" + URLEncoder.encode(file.getAbsolutePath(), UTF_8));
           }
-          url = new URL(urlStr);
+          url = URI.create(urlStr);
         }
       } else {
         if (type == null) {
@@ -924,6 +924,8 @@ public class SimplePostTool {
               + (mockMode ? " MOCK!" : ""));
       is = new FileInputStream(file);
       postData(is, file.length(), output, type, url);
+    } catch (URISyntaxException e) {
+      warn("Not valid URL");
     } catch (IOException e) {
       warn("Can't open/read file: " + file);
     } finally {
@@ -938,20 +940,15 @@ public class SimplePostTool {
   }
 
   /**
-   * Appends to the path of the URL
+   * Appends to the path of the URI
    *
-   * @param url the URL
+   * @param uri the URI
    * @param append the path to append
-   * @return the final URL version
+   * @return the final URI version
    */
-  protected static URL appendUrlPath(URL url, String append) throws MalformedURLException {
-    return new URL(
-        url.getProtocol()
-            + "://"
-            + url.getAuthority()
-            + url.getPath()
-            + append
-            + (url.getQuery() != null ? "?" + url.getQuery() : ""));
+  protected static URI appendUrlPath(URI uri, String append) throws URISyntaxException {
+    var newPath = uri.getPath() + append;
+    return new URI(uri.getScheme(), uri.getAuthority(), newPath, uri.getQuery(), uri.getFragment());
   }
 
   /**
@@ -971,7 +968,7 @@ public class SimplePostTool {
   /** Performs a simple get on the given URL */
   public void doGet(String url) {
     try {
-      doGet(new URL(url));
+      doGet(URI.create(url).toURL());
     } catch (MalformedURLException e) {
       warn("The specified URL " + url + " is not a valid URL. Please check");
     }
@@ -1000,7 +997,7 @@ public class SimplePostTool {
    * @return true if success
    */
   public boolean postData(
-      InputStream data, Long length, OutputStream output, String type, URL url) {
+      InputStream data, Long length, OutputStream output, String type, URI uri) {
     if (mockMode) {
       return true;
     }
@@ -1012,7 +1009,7 @@ public class SimplePostTool {
     HttpURLConnection urlConnection = null;
     try {
       try {
-        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection = (HttpURLConnection) uri.toURL().openConnection();
         try {
           urlConnection.setRequestMethod("POST");
         } catch (ProtocolException e) {
@@ -1311,7 +1308,7 @@ public class SimplePostTool {
         disallows = new ArrayList<>();
         URL urlRobot;
         try {
-          urlRobot = new URL(strRobot);
+          urlRobot = URI.create(strRobot).toURL();
           disallows = parseRobotsTxt(urlRobot.openStream());
         } catch (MalformedURLException e) {
           return true; // We cannot trust this robots URL, should not happen
@@ -1361,17 +1358,17 @@ public class SimplePostTool {
      * @param u the URL of the web page
      * @param is the input stream of the page
      * @param type the content-type
-     * @param postUrl the URL (typically /solr/extract) in order to pull out links
+     * @param postUri the URI (typically /solr/extract) in order to pull out links
      * @return a set of URIs parsed from the page
      */
-    protected Set<URI> getLinksFromWebPage(URL u, InputStream is, String type, URL postUrl) {
+    protected Set<URI> getLinksFromWebPage(URL u, InputStream is, String type, URI postUri) {
       Set<URI> linksFromPage = new HashSet<>();
       URL url = null;
       try {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        URL extractUrl = new URL(appendParam(postUrl.toString(), "extractOnly=true"));
-        extractUrl = new URL(appendParam(extractUrl.toString(), "wt=xml"));
-        boolean success = postData(is, null, os, type, extractUrl);
+        URI extractUri = URI.create(appendParam(postUri.toString(), "extractOnly=true"));
+        extractUri = URI.create(appendParam(extractUri.toString(), "wt=xml"));
+        boolean success = postData(is, null, os, type, extractUri);
         if (success) {
           Document d = makeDom(os.toByteArray());
           String innerXml = getXP(d, "/response/str/text()[1]", false);
