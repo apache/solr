6ab6c4abf21dbd98a9b9c23a45af67bcffaea4d8
Alexey Serba <aserba@gmail.com>
2024-07-17 12:47:10 -0700

    SOLR-10255 Add support for docValues to solr.BinaryField (#2536)
    Co-authored-by: Alexey Serba <alexeys@zillowgroup.com>

--- solr/core/src/java/org/apache/solr/schema/BinaryField.java
+++ solr/core/src/java/org/apache/solr/schema/BinaryField.java
@@ -20,7 +20,11 @@ import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+import org.apache.lucene.document.BinaryDocValuesField;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.search.SortField;
 import org.apache.lucene.util.BytesRef;
@@ -41,6 +45,11 @@ public class BinaryField extends FieldType {
       throw new SolrException(
           SolrException.ErrorCode.SERVER_ERROR,
           "Field type " + this + " is 'large'; not supported (yet)");
+    }
+    if (field.hasDocValues() && field.multiValued()) {
+      throw new SolrException(
+          SolrException.ErrorCode.SERVER_ERROR,
+          "Field type " + this + " does not support multiple doc values");
     }
   }
 
@@ -94,6 +103,10 @@ public class BinaryField extends FieldType {
       log.trace("Ignoring unstored binary field: {}", field);
       return null;
     }
+    return new org.apache.lucene.document.StoredField(field.getName(), getBytesRef(val));
+  }
+
+  private static BytesRef getBytesRef(Object val) {
     byte[] buf = null;
     int offset = 0, len = 0;
     if (val instanceof byte[]) {
@@ -112,7 +125,31 @@ public class BinaryField extends FieldType {
       len = buf.length;
     }
 
-    return new org.apache.lucene.document.StoredField(field.getName(), buf, offset, len);
+    return new BytesRef(buf, offset, len);
+  }
+
+  @Override
+  public List<IndexableField> createFields(SchemaField field, Object val) {
+    IndexableField fval = createField(field, val);
+
+    if (field.hasDocValues() && !field.multiValued()) {
+      IndexableField docval = new BinaryDocValuesField(field.getName(), getBytesRef(val));
+
+      // Only create list if we have 2 values...
+      if (fval != null) {
+        List<IndexableField> fields = new ArrayList<>(2);
+        fields.add(fval);
+        fields.add(docval);
+        return fields;
+      }
+
+      fval = docval;
+    }
+    return Collections.singletonList(fval);
+  }
+
+  @Override
+  protected void checkSupportsDocValues() { // we support DocValues
   }
 
   @Override
