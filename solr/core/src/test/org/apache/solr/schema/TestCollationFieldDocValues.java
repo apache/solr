5e0ae90e51d1cbb5fa57a828ea3e139c2d6127f8
Sanjay Dutt <sanjaydutt@apache.org>
2024-08-10 19:43:47 +0530

    SOLR-17399: Replace the use of the deprecated java.util.Locale constructor with Locale Builder API
    (cherry picked from commit b806abd3668bc249590e45a2046f8e73015296be)

--- solr/core/src/test/org/apache/solr/schema/TestCollationFieldDocValues.java
+++ solr/core/src/test/org/apache/solr/schema/TestCollationFieldDocValues.java
@@ -75,7 +75,8 @@ public class TestCollationFieldDocValues extends SolrTestCaseJ4 {
 
     // generate custom collation rules (DIN 5007-2), saving to customrules.dat
     RuleBasedCollator baseCollator =
-        (RuleBasedCollator) Collator.getInstance(new Locale("de", "DE"));
+        (RuleBasedCollator)
+            Collator.getInstance(new Locale.Builder().setLanguageTag("de-DE").build());
 
     String DIN5007_2_tailorings =
         "& ae , a\u0308 & AE , A\u0308"
