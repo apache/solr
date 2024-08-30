a23be0d509e2c1adc4f57482cab0069d17b36933
Sanjay Dutt <sanjaydutt@apache.org>
2024-08-30 11:37:55 +0530

    Removed explicit cast of HttpClient to AutoClosable

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/HttpJdkSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/HttpJdkSolrClient.java
@@ -462,14 +462,14 @@ public class HttpJdkSolrClient extends HttpSolrClientBase {
 
   @Override
   public void close() throws IOException {
-    // If used with Java 21+
-    if (httpClient instanceof AutoCloseable) {
-      try {
-        ((AutoCloseable) httpClient).close();
-      } catch (Exception e) {
-        log.warn("Could not close the http client.", e);
+    try {
+      if (httpClient != null) {
+        httpClient.close();
       }
+    } catch (Exception e) {
+      log.warn("Could not close the http client.", e);
     }
+
     httpClient = null;
 
     if (shutdownExecutor) {
