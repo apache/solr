e35207268b8b71559b9bdd8a164e9bc1f2018dfb
Houston Putman <houston@apache.org>
2024-08-30 10:40:37 -0500

    SOLR-17427: Fix url building in cli (#2678)

--- solr/core/src/java/org/apache/solr/cli/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/cli/SolrCLI.java
@@ -636,8 +636,11 @@ public class SolrCLI implements CLIO {
    */
   public static String normalizeSolrUrl(String solrUrl, boolean logUrlFormatWarning) {
     if (solrUrl != null) {
-      if (solrUrl.contains("/solr")) { //
-        String newSolrUrl = solrUrl.substring(0, solrUrl.indexOf("/solr"));
+      URI uri = URI.create(solrUrl);
+      String urlPath = uri.getRawPath();
+      if (urlPath != null && urlPath.contains("/solr")) {
+        String newSolrUrl =
+            uri.resolve(urlPath.substring(0, urlPath.lastIndexOf("/solr") + 1)).toString();
         if (logUrlFormatWarning) {
           CLIO.out(
               "WARNING: URLs provided to this tool needn't include Solr's context-root (e.g. \"/solr\"). Such URLs are deprecated and support for them will be removed in a future release. Correcting from ["
