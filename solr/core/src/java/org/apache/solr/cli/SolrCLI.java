83111fe4024b43276f2f569d629ff8724c779dd1
Christos Malliaridis <c.malliaridis@gmail.com>
2024-10-04 00:40:21 +0200

    Deprecate SolrCLI common CLI options

--- solr/core/src/java/org/apache/solr/cli/SolrCLI.java
+++ solr/core/src/java/org/apache/solr/cli/SolrCLI.java
@@ -90,88 +90,127 @@ public class SolrCLI implements CLIO {
 
   public static final String ZK_HOST = "localhost:9983";
 
-  public static final Option OPTION_ZKHOST_DEPRECATED =
-      Option.builder("zkHost")
-          .longOpt("zkHost")
-          .deprecated(
-              DeprecatedAttributes.builder()
-                  .setForRemoval(true)
-                  .setSince("9.7")
-                  .setDescription("Use --zk-host instead")
-                  .get())
-          .argName("HOST")
-          .hasArg()
-          .required(false)
-          .desc(
-              "Zookeeper connection string; unnecessary if ZK_HOST is defined in solr.in.sh; otherwise, defaults to "
-                  + ZK_HOST
-                  + '.')
-          .build();
+  /**
+   * @deprecated Use {@link CommonCLIOptions#ZK_HOST_OPTION} instead. If you need to continue
+   * support the deprecated options, use {@link CommonCLIOptions#ZK_HOST_OPTION_GROUP} that combines
+   * {@link SolrCLI#OPTION_ZKHOST_DEPRECATED} and {@link SolrCLI#OPTION_ZKHOST}.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_ZKHOST_DEPRECATED = Option.builder("zkHost")
+      .longOpt("zkHost")
+      .deprecated(
+          DeprecatedAttributes.builder()
+              .setForRemoval(true)
+              .setSince("9.7")
+              .setDescription("Use --zk-host instead")
+              .get())
+      .argName("HOST")
+      .hasArg()
+      .required(false)
+      .desc(
+          "Zookeeper connection string; unnecessary if ZK_HOST is defined in solr.in.sh; otherwise, defaults to "
+              + ZK_HOST
+              + '.')
+      .build();
 
-  public static final Option OPTION_ZKHOST =
-      Option.builder("z")
-          .longOpt("zk-host")
-          .argName("HOST")
-          .hasArg()
-          .required(false)
-          .desc(
-              "Zookeeper connection string; unnecessary if ZK_HOST is defined in solr.in.sh; otherwise, defaults to "
-                  + ZK_HOST
-                  + '.')
-          .build();
-  public static final Option OPTION_SOLRURL_DEPRECATED =
-      Option.builder("solrUrl")
-          .longOpt("solrUrl")
-          .deprecated(
-              DeprecatedAttributes.builder()
-                  .setForRemoval(true)
-                  .setSince("9.7")
-                  .setDescription("Use --solr-url instead")
-                  .get())
-          .argName("HOST")
-          .hasArg()
-          .required(false)
-          .desc(
-              "Base Solr URL, which can be used to determine the zk-host if that's not known; defaults to: "
-                  + getDefaultSolrUrl()
-                  + '.')
-          .build();
-  public static final Option OPTION_SOLRURL =
-      Option.builder("url")
-          .longOpt("solr-url")
-          .argName("HOST")
-          .hasArg()
-          .required(false)
-          .desc(
-              "Base Solr URL, which can be used to determine the zk-host if that's not known; defaults to: "
-                  + getDefaultSolrUrl()
-                  + '.')
-          .build();
-  public static final Option OPTION_VERBOSE =
-      Option.builder("v")
-          .longOpt("verbose")
-          .required(false)
-          .desc("Enable verbose command output.")
-          .build();
-  public static final Option OPTION_HELP =
-      Option.builder("h").longOpt("help").required(false).desc("Print this message.").build();
+  /**
+   * @deprecated Use {@link CommonCLIOptions#ZK_HOST_OPTION} instead. If you need to continue
+   * support the deprecated options, use {@link CommonCLIOptions#ZK_HOST_OPTION_GROUP} that combines
+   * {@link SolrCLI#OPTION_ZKHOST_DEPRECATED} and {@link SolrCLI#OPTION_ZKHOST}.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_ZKHOST = Option.builder("z")
+      .longOpt("zk-host")
+      .argName("HOST")
+      .hasArg()
+      .required(false)
+      .desc(
+          "Zookeeper connection string; unnecessary if ZK_HOST is defined in solr.in.sh; otherwise, defaults to "
+              + ZK_HOST
+              + '.')
+      .build();
 
-  public static final Option OPTION_RECURSE =
-      Option.builder("r")
-          .longOpt("recurse")
-          .required(false)
-          .desc("Apply the command recursively.")
-          .build();
+  /**
+   * @deprecated Use {@link CommonCLIOptions#SOLR_URL_OPTION} instead. If you need to continue
+   * support the deprecated options, use {@link CommonCLIOptions#SOLR_URL_OPTION_GROUP} that
+   * combines {@link SolrCLI#OPTION_SOLRURL_DEPRECATED} and {@link SolrCLI#OPTION_SOLRURL}.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_SOLRURL_DEPRECATED = Option.builder("solrUrl")
+      .longOpt("solrUrl")
+      .deprecated(
+          DeprecatedAttributes.builder()
+              .setForRemoval(true)
+              .setSince("9.7")
+              .setDescription("Use --solr-url instead")
+              .get())
+      .argName("HOST")
+      .hasArg()
+      .required(false)
+      .desc(
+          "Base Solr URL, which can be used to determine the zk-host if that's not known; defaults to: "
+              + getDefaultSolrUrl()
+              + '.')
+      .build();
 
-  public static final Option OPTION_CREDENTIALS =
-      Option.builder("u")
-          .longOpt("credentials")
-          .argName("credentials")
-          .hasArg()
-          .required(false)
-          .desc(
-              "Credentials in the format username:password. Example: --credentials solr:SolrRocks")
-          .build();
+  /**
+   * @deprecated Use {@link CommonCLIOptions#SOLR_URL_OPTION} instead. If you need to continue
+   * support the deprecated options, use {@link CommonCLIOptions#SOLR_URL_OPTION_GROUP} that
+   * combines {@link SolrCLI#OPTION_SOLRURL_DEPRECATED} and {@link SolrCLI#OPTION_SOLRURL}.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_SOLRURL = Option.builder("url")
+      .longOpt("solr-url")
+      .argName("HOST")
+      .hasArg()
+      .required(false)
+      .desc(
+          "Base Solr URL, which can be used to determine the zk-host if that's not known; defaults to: "
+              + getDefaultSolrUrl()
+              + '.')
+      .build();
+
+  /**
+   * @deprecated Use {@link CommonCLIOptions#VERBOSE_OPTION} instead.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_VERBOSE = Option.builder("v")
+      .longOpt("verbose")
+      .required(false)
+      .desc("Enable verbose command output.")
+      .build();
+
+  /**
+   * @deprecated Use {@link CommonCLIOptions#HELP_OPTION} instead.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_HELP = Option.builder("h")
+      .longOpt("help")
+      .required(false)
+      .desc("Print this message.")
+      .build();
+
+  /**
+   * @deprecated Use {@link CommonCLIOptions#RECURSE_OPTION} instead.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_RECURSE = Option.builder("r")
+      .longOpt("recurse")
+      .required(false)
+      .desc("Apply the command recursively.")
+      .build();
+
+  /**
+   * @deprecated Use {@link CommonCLIOptions#CREDENTIALS_OPTION} instead.
+   */
+  @Deprecated(since = "9.8", forRemoval = true)
+  public static final Option OPTION_CREDENTIALS = Option.builder("u")
+      .longOpt("credentials")
+      .argName("credentials")
+      .hasArg()
+      .required(false)
+      .desc("Credentials in the format username:password. Example: --credentials solr:SolrRocks")
+      .build();
 
   public static void exit(int exitStatus) {
     try {
