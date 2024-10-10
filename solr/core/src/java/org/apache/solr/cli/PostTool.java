601d5ba12f541c65818c47601eba120105db7b37
Eric Pugh <epugh@opensourceconnections.com>
2024-10-10 17:17:27 -0600

    Deprecate the -d

--- solr/core/src/java/org/apache/solr/cli/PostTool.java
+++ solr/core/src/java/org/apache/solr/cli/PostTool.java
@@ -222,6 +222,19 @@ public class PostTool extends ToolBase {
             .desc("For web crawl, how deep to go. default: 1")
             .build(),
         Option.builder("d")
+            .deprecated(
+                DeprecatedAttributes.builder()
+                    .setForRemoval(true)
+                    .setSince("9.8")
+                    .setDescription("Use --delay instead")
+                    .get())
+            .hasArg()
+            .argName("delay")
+            .required(false)
+            .desc(
+                "If recursive then delay will be the wait time between posts.  default: 10 for web, 0 for files")
+            .build(),
+        Option.builder()
             .longOpt("delay")
             .hasArg()
             .argName("delay")
