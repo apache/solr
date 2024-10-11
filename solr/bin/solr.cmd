504d7031202f337c50d591782aa383c1abef315d
Eric Pugh <epugh@opensourceconnections.com>
2024-10-11 12:39:47 -0600

    SOLR-17466: Inform 9x users about upcoming change to solrcloud default mode (#2728)
    Provides a heads up to what's coming in SOLR-17467.
    ---------
    
    Co-authored-by: Jan Høydahl <jh@cominvent.com>

--- solr/bin/solr.cmd
+++ solr/bin/solr.cmd
@@ -952,6 +952,8 @@ if !JAVA_MAJOR_VERSION! LSS 9  (
 IF NOT "%ZK_HOST%"=="" set SOLR_MODE=solrcloud
 
 IF "%SOLR_MODE%"=="solrcloud" (
+  @echo Solr will start in SolrCloud mode by default in version 10, and you will no longer need to pass in -c or --cloud flag.
+  @echo.
   IF "%ZK_CLIENT_TIMEOUT%"=="" set "ZK_CLIENT_TIMEOUT=30000"
 
   set "CLOUD_MODE_OPTS=-DzkClientTimeout=!ZK_CLIENT_TIMEOUT!"
@@ -986,6 +988,7 @@ IF "%SOLR_MODE%"=="solrcloud" (
   IF EXIST "%SOLR_HOME%\collection1\core.properties" set "CLOUD_MODE_OPTS=!CLOUD_MODE_OPTS! -Dbootstrap_confdir=./solr/collection1/conf -Dcollection.configName=myconf -DnumShards=1"
 ) ELSE (
   set CLOUD_MODE_OPTS=
+  @echo Solr will start in SolrCloud mode by default in version 10.  You will need to pass in --user-managed flag to run in User Managed (aka Standalone) mode.
   IF NOT EXIST "%SOLR_HOME%\solr.xml" (
     IF "%SOLR_SOLRXML_REQUIRED%"=="true" (
       set "SCRIPT_ERROR=Solr home directory %SOLR_HOME% must contain solr.xml!"
--- solr/packaging/test/test_start_solr.bats
+++ solr/packaging/test/test_start_solr.bats
@@ -50,6 +50,13 @@ teardown() {
 
 }
 
+@test "start provides warning about SolrCloud mode" {
+  run solr start
+  solr assert --started http://localhost:${SOLR_PORT} --timeout 5000
+  assert_output --partial 'Solr will start in SolrCloud mode by default in version 10.  You will need to pass in --standalone flag to run in Standalone mode.'
+  solr stop
+}
+
 @test "check stop command doesn't hang" {
   # for start/stop/restart we parse the args separate from picking the command
   # which means you don't get an error message for passing a start arg, like --jvm-opts to a stop commmand.
