7cf132f43db083fa1075581e9012a7a44b39256c
Christos Malliaridis <c.malliaridis@gmail.com>
2024-10-19 16:41:35 +0200

    Update UI dependencies

--- solr/compose-ui/build.gradle.kts
+++ solr/compose-ui/build.gradle.kts
@@ -16,7 +16,7 @@
  */
 
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 repositories {
