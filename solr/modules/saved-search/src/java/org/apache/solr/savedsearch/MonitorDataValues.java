d869b3325a0ed667eb848fdd08aa3194e7a16bf6
Luke Kot-Zaniewski <lkotzaniewsk@bloomberg.net>
2024-12-04 16:50:46 -0500

    monitor -> savedsearch package

---
+++ solr/modules/saved-search/src/java/org/apache/solr/savedsearch/MonitorDataValues.java
@@ -0,0 +1,76 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  * contributor license agreements.  See the NOTICE file distributed with
+ *  * this work for additional information regarding copyright ownership.
+ *  * The ASF licenses this file to You under the Apache License, Version 2.0
+ *  * (the "License"); you may not use this file except in compliance with
+ *  * the License.  You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.apache.solr.savedsearch;
+
+import java.io.IOException;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.NumericDocValues;
+import org.apache.lucene.index.SortedDocValues;
+import org.apache.lucene.monitor.MonitorFields;
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.solr.common.params.CommonParams;
+
+public class MonitorDataValues {
+
+  private final SortedDocValues queryIdIt;
+  private final SortedDocValues cacheIdIt;
+  private final SortedDocValues mqIt;
+  private final NumericDocValues versionIt;
+  private final LeafReader reader;
+  private int currentDoc;
+
+  public MonitorDataValues(LeafReaderContext context) throws IOException {
+    reader = context.reader();
+    cacheIdIt = reader.getSortedDocValues(MonitorFields.CACHE_ID);
+    queryIdIt = reader.getSortedDocValues(MonitorFields.QUERY_ID);
+    mqIt = reader.getSortedDocValues(MonitorFields.MONITOR_QUERY);
+    versionIt = reader.getNumericDocValues(CommonParams.VERSION_FIELD);
+    currentDoc = DocIdSetIterator.NO_MORE_DOCS;
+  }
+
+  public boolean advanceTo(int doc) throws IOException {
+    currentDoc = doc;
+    return cacheIdIt.advanceExact(currentDoc);
+  }
+
+  public String getQueryId() throws IOException {
+    queryIdIt.advanceExact(currentDoc);
+    return queryIdIt.lookupOrd(queryIdIt.ordValue()).utf8ToString();
+  }
+
+  public String getCacheId() throws IOException {
+    return cacheIdIt.lookupOrd(cacheIdIt.ordValue()).utf8ToString();
+  }
+
+  public String getMq() throws IOException {
+    if (mqIt != null && mqIt.advanceExact(currentDoc)) {
+      return mqIt.lookupOrd(mqIt.ordValue()).utf8ToString();
+    }
+    return reader.document(currentDoc).get(MonitorFields.MONITOR_QUERY);
+  }
+
+  public long getVersion() throws IOException {
+    if (versionIt != null && versionIt.advanceExact(currentDoc)) {
+      return versionIt.longValue();
+    }
+    return 0;
+  }
+}
---
+++ solr/modules/saved-search/src/java/org/apache/solr/savedsearch/search/PresearcherFactory.java
@@ -0,0 +1,146 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  * contributor license agreements.  See the NOTICE file distributed with
+ *  * this work for additional information regarding copyright ownership.
+ *  * The ASF licenses this file to You under the Apache License, Version 2.0
+ *  * (the "License"); you may not use this file except in compliance with
+ *  * the License.  You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.apache.solr.savedsearch.search;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.Set;
+import org.apache.lucene.monitor.CustomQueryHandler;
+import org.apache.lucene.monitor.MultipassTermFilteredPresearcher;
+import org.apache.lucene.monitor.Presearcher;
+import org.apache.lucene.monitor.TermFilteredPresearcher;
+import org.apache.lucene.monitor.TermWeightor;
+import org.apache.lucene.util.ResourceLoader;
+import org.apache.solr.savedsearch.AliasingMultipassTermFilteredPresearcher;
+import org.apache.solr.savedsearch.AliasingTermFilteredPresearcher;
+
+public class PresearcherFactory {
+
+  // The default prefix is long because we don't want it to clash
+  // with a user-defined name pattern and the longest one wins.
+  // {@link
+  // https://cwiki.apache.org/confluence/display/solr/SchemaXml#Dynamic_fields:~:text=Longer%20patterns%20will%20be%20matched%20first}
+  // In the worst case this can be overridden by the user but ideally this never comes up.
+  public static final String DEFAULT_ALIAS_PREFIX =
+      "_________________________________________________monitor_alias_";
+
+  public static final String TERM_FILTERED = TermFilteredPresearcher.class.getSimpleName();
+  public static final String MULTI_PASS_TERM_FILTERED =
+      MultipassTermFilteredPresearcher.class.getSimpleName();
+
+  private PresearcherFactory() {}
+
+  static Presearcher build(
+      ResourceLoader resourceLoader, PresearcherParameters presearcherParameters) {
+    Presearcher presearcher;
+    String type = presearcherParameters.presearcherType;
+    if (TERM_FILTERED.equals(type) || MULTI_PASS_TERM_FILTERED.equals(type)) {
+      TermWeightor weightor =
+          constructZeroArgClass(
+              resourceLoader,
+              TermWeightor.class,
+              presearcherParameters.termWeightorType,
+              TermFilteredPresearcher.DEFAULT_WEIGHTOR);
+      List<CustomQueryHandler> customQueryHandlers = List.of();
+      Set<String> filterFields = Set.of();
+      if (TERM_FILTERED.equals(type)) {
+        if (presearcherParameters.applyFieldNameAlias) {
+          presearcher =
+              AliasingTermFilteredPresearcher.build(
+                  weightor, customQueryHandlers, filterFields, presearcherParameters.aliasPrefix);
+        } else {
+          presearcher = new TermFilteredPresearcher(weightor, customQueryHandlers, filterFields);
+        }
+      } else {
+        if (presearcherParameters.applyFieldNameAlias) {
+          presearcher =
+              AliasingMultipassTermFilteredPresearcher.build(
+                  presearcherParameters.numberOfPasses,
+                  presearcherParameters.minWeight,
+                  weightor,
+                  customQueryHandlers,
+                  filterFields,
+                  presearcherParameters.aliasPrefix);
+        } else {
+          presearcher =
+              new MultipassTermFilteredPresearcher(
+                  presearcherParameters.numberOfPasses,
+                  presearcherParameters.minWeight,
+                  weightor,
+                  customQueryHandlers,
+                  filterFields);
+        }
+      }
+    } else {
+      presearcher =
+          constructZeroArgClass(resourceLoader, Presearcher.class, type, Presearcher.NO_FILTERING);
+    }
+    return presearcher;
+  }
+
+  private static <T> T constructZeroArgClass(
+      ResourceLoader loader, Class<T> expectedType, String typeName, T defaultVal) {
+    if (typeName == null) {
+      return defaultVal;
+    }
+    try {
+      Class<? extends T> implClass = loader.findClass(typeName, expectedType);
+      Constructor<? extends T> c = implClass.getConstructor();
+      return c.newInstance();
+    } catch (Exception e) {
+      throw new IllegalStateException(
+          "Could not construct custom " + expectedType.getSimpleName() + " of type " + typeName, e);
+    }
+  }
+
+  public static class PresearcherParameters {
+
+    private String presearcherType;
+    private String termWeightorType;
+    private boolean applyFieldNameAlias = false;
+    private int numberOfPasses = 0;
+    private float minWeight = 0;
+    private String aliasPrefix = DEFAULT_ALIAS_PREFIX;
+
+    public void setPresearcherType(String presearcherType) {
+      this.presearcherType = presearcherType;
+    }
+
+    public void setTermWeightorType(String termWeightorType) {
+      this.termWeightorType = termWeightorType;
+    }
+
+    public void setApplyFieldNameAlias(boolean applyFieldNameAlias) {
+      this.applyFieldNameAlias = applyFieldNameAlias;
+    }
+
+    public void setNumberOfPasses(int numberOfPasses) {
+      this.numberOfPasses = numberOfPasses;
+    }
+
+    public void setMinWeight(float minWeight) {
+      this.minWeight = minWeight;
+    }
+
+    public void setAliasPrefix(String aliasPrefix) {
+      this.aliasPrefix = aliasPrefix;
+    }
+  }
+}
