532f091ecc75f79cb4707a3903a3560dddbd567d
David Smiley <dsmiley@salesforce.com>
2024-12-10 10:57:40 -0500

    SOLR-17561: Deprecations in ClusterState, DocCollection, Replica (#2898)
    Non-essential methods that will go away in Solr 10.  Might add additional methods later to address some conveniences these offer.
    
    (cherry picked from commit 2d3a8d9954a45c0eb1bbaa77b29f51ce314c1849)

--- solr/solrj/src/java/org/apache/solr/common/cloud/Replica.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/Replica.java
@@ -304,6 +304,7 @@ public class Replica extends ZkNodeProps implements MapWriter {
     return state;
   }
 
+  @Deprecated
   public void setState(State state) {
     this.state = state;
     propMap.put(ReplicaStateProps.STATE, this.state.toString());
