24efa78ebf35dbf692d8fb588cceb1bea3e73118
David Smiley <dsmiley@salesforce.com>
2024-12-10 01:08:43 -0500

    Keep NamedList implementing SimpleMap in 9x

--- solr/solrj/src/java/org/apache/solr/common/util/NamedList.java
+++ solr/solrj/src/java/org/apache/solr/common/util/NamedList.java
@@ -30,6 +30,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.apache.solr.cluster.api.SimpleMap;
 import org.apache.solr.common.MapWriter;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.MultiMapSolrParams;
@@ -57,7 +61,7 @@ import org.apache.solr.common.params.SolrParams;
  */
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class NamedList<T>
-    implements Cloneable, Serializable, Iterable<Map.Entry<String, T>>, MapWriter {
+    implements Cloneable, Serializable, Iterable<Map.Entry<String, T>>, MapWriter, SimpleMap<T> {
 
   private static final long serialVersionUID = 1957981902839867821L;
   protected final List<Object> nvPairs;
@@ -152,6 +156,7 @@ public class NamedList<T>
   }
 
   /** The total number of name/value pairs */
+  @Override
   public int size() {
     return nvPairs.size() >> 1;
   }
@@ -243,6 +248,7 @@ public class NamedList<T>
    * @see #indexOf
    * @see #get(String,int)
    */
+  @Override
   public T get(String name) {
     return get(name, 0);
   }
@@ -815,6 +821,30 @@ public class NamedList<T>
     return this.nvPairs.equals(nl.nvPairs);
   }
 
+  @Override
+  public void abortableForEach(BiFunction<String, ? super T, Boolean> fun) {
+    int sz = size();
+    for (int i = 0; i < sz; i++) {
+      if (!fun.apply(getName(i), getVal(i))) break;
+    }
+  }
+
+  @Override
+  public void abortableForEachKey(Function<String, Boolean> fun) {
+    int sz = size();
+    for (int i = 0; i < sz; i++) {
+      if (!fun.apply(getName(i))) break;
+    }
+  }
+
+  @Override
+  public void forEachKey(Consumer<String> fun) {
+    int sz = size();
+    for (int i = 0; i < sz; i++) {
+      fun.accept(getName(i));
+    }
+  }
+
   public void forEach(BiConsumer<String, ? super T> action) {
     int sz = size();
     for (int i = 0; i < sz; i++) {
@@ -825,5 +855,10 @@ public class NamedList<T>
   @Override
   public int _size() {
     return size();
+  }
+
+  @Override
+  public void forEachEntry(BiConsumer<String, ? super T> fun) {
+    forEach(fun);
   }
 }
