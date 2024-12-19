933a242f5e0455e8de37f34847a5b6d7ebafe704
Jason Gerlowski <gerlowskija@apache.org>
2024-12-18 22:59:53 -0500

    WIP: EOD cutoff for segmentsApi

--- solr/api/src/java/org/apache/solr/client/api/endpoint/CollectionStatusApi.java
+++ solr/api/src/java/org/apache/solr/client/api/endpoint/CollectionStatusApi.java
@@ -24,6 +24,8 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import org.apache.solr.client.api.model.CollectionStatusResponse;
 
+import static org.apache.solr.client.api.endpoint.SegmentsApi.RAW_SIZE_SUMMARY_DESC;
+
 /**
  * V2 API definition for fetching collection metadata
  *
@@ -43,45 +45,17 @@ public interface CollectionStatusApi {
       @Parameter(description = "The name of the collection return metadata for", required = true)
           @PathParam("collectionName")
           String collectionName,
-      @Parameter(
-              description =
-                  "Boolean flag to include metadata (e.g. index an data directories, IndexWriter configuration, etc.) about each shard leader's core")
-          @QueryParam("coreInfo")
-          Boolean coreInfo,
+      @Parameter(description = SegmentsApi.CORE_INFO_PARAM_DESC) @QueryParam("coreInfo") Boolean coreInfo,
       @Parameter(
               description =
                   "Boolean flag to include metadata and statistics about the segments used by each shard leader.  Implicitly set to true by 'fieldInfo' and 'sizeInfo'")
           @QueryParam("segments")
           Boolean segments,
-      @Parameter(
-              description =
-                  "Boolean flag to include statistics about the indexed fields present on each shard leader. Implicitly sets the 'segments' flag to 'true'")
-          @QueryParam("fieldInfo")
-          Boolean fieldInfo,
-      @Parameter(
-              description =
-                  "Boolean flag to include simple estimates of the disk size taken up by each field (e.g. \"id\", \"_version_\") and by each index data structure (e.g. 'storedFields', 'docValues_numeric').")
-          @QueryParam("rawSize")
-          Boolean rawSize,
-      @Parameter(
-              description =
-                  "Boolean flag to include more involved estimates of the disk size taken up by index data structures, on a per-field basis (e.g. how much data does the \"id\" field contribute to 'storedField' index files).  More detail than 'rawSize', less detail than 'rawSizeDetails'.")
-          @QueryParam("rawSizeSummary")
-          Boolean rawSizeSummary,
-      @Parameter(
-              description =
-                  "Boolean flag to include detailed statistics about the disk size taken up by various fields and data structures.  More detail than 'rawSize' and 'rawSizeSummary'.")
-          @QueryParam("rawSizeDetails")
-          Boolean rawSizeDetails,
-      @Parameter(
-              description =
-                  "Percentage (between 0 and 100) of data to read when estimating index size and statistics.  Defaults to 5.0 (i.e. 5%).")
-          @QueryParam("rawSizeSamplingPercent")
-          Float rawSizeSamplingPercent,
-      @Parameter(
-              description =
-                  "Boolean flag to include information about the largest index files for each Lucene segment.  Implicitly sets the 'segment' flag to 'true'")
-          @QueryParam("sizeInfo")
-          Boolean sizeInfo)
+      @Parameter(description = SegmentsApi.FIELD_INFO_PARAM_DESC + " Implicitly sets the 'segments' flag to 'true'") @QueryParam("fieldInfo") Boolean fieldInfo,
+      @Parameter(description = SegmentsApi.RAW_SIZE_PARAM_DESC) @QueryParam("rawSize") Boolean rawSize,
+      @Parameter(description = SegmentsApi.RAW_SIZE_SUMMARY_DESC) @QueryParam("rawSizeSummary") Boolean rawSizeSummary,
+      @Parameter(description = SegmentsApi.RAW_SIZE_DETAILS_DESC) @QueryParam("rawSizeDetails") Boolean rawSizeDetails,
+      @Parameter(description = SegmentsApi.RAW_SIZE_SAMPLING_PERCENT_DESC) @QueryParam("rawSizeSamplingPercent") Float rawSizeSamplingPercent,
+      @Parameter(description = SegmentsApi.SIZE_INFO_PARAM_DESC + ". Implicitly sets the 'segment' flag to 'true'") @QueryParam("sizeInfo") Boolean sizeInfo)
       throws Exception;
 }
