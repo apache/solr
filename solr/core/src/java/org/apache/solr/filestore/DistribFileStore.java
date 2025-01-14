3531042d1b8af9acb71ec501ffc6717fb16ae7d6
Eric Pugh <epugh@opensourceconnections.com>
2025-01-14 06:50:57 -0500

    Remove incorrect TODO reference

--- solr/core/src/java/org/apache/solr/filestore/DistribFileStore.java
+++ solr/core/src/java/org/apache/solr/filestore/DistribFileStore.java
@@ -534,7 +534,6 @@ public class DistribFileStore implements FileStore {
         @SuppressWarnings({"rawtypes"})
         List myFiles = list(path, s -> true);
         for (Object f : l) {
-          // TODO: https://issues.apache.org/jira/browse/SOLR-15426
           // l should be a List<String> and myFiles should be a List<FileDetails>, so contains
           // should always return false!
           if (!myFiles.contains(f)) {
