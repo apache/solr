aeca5f9c54cf5d1d894c9229abed1b5566835634
Renato Haeberli <renato@onnet.ch>
2025-01-20 09:46:13 +0100

    SOLR-17623: adding unit test to cover all methods

--- solr/solrj/src/test/org/apache/solr/common/util/SimpleOrderedMapTest.java
+++ solr/solrj/src/test/org/apache/solr/common/util/SimpleOrderedMapTest.java
@@ -1,133 +1,125 @@
 package org.apache.solr.common.util;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Map;
+public class SimpleOrderedMapTest extends Assert {
 
-public class SimpleOrderedMapTest extends Assert
-{
-	private SimpleOrderedMap<Integer> map = new SimpleOrderedMap<>();
+  private final SimpleOrderedMap<Integer> map = new SimpleOrderedMap<>();
 
-	@Test
-	public void testPut() {
-		map.put("one", 1);
-		map.put("two", 2);              
-		
-		assertEquals(4, map.nvPairs.size());
-		assertEquals("one", map.nvPairs.get(0));
-		assertEquals(1, map.nvPairs.get(1));
-		assertEquals("two", map.nvPairs.get(2));
-		assertEquals(2, map.nvPairs.get(3));
+  @Test
+  public void testPut() {
+    map.put("one", 1);
+    map.put("two", 2);
 
-		Object one =  map.put("one", -1);
-		assertEquals(1,one);
-	}
+    assertEquals(4, map.nvPairs.size());
+    assertEquals("one", map.nvPairs.get(0));
+    assertEquals(1, map.nvPairs.get(1));
+    assertEquals("two", map.nvPairs.get(2));
+    assertEquals(2, map.nvPairs.get(3));
 
-	@Test
-	public void testContains()
-	{
-		setupData();
-		assertTrue(map.containsKey("one"));
-		assertTrue(map.containsKey("two"));
-		assertTrue(map.containsKey("three"));
-		assertFalse(map.containsKey("four"));
-	}
+    Object one = map.put("one", -1);
+    assertEquals(1, one);
+  }
 
-	@Test
-	public void testContainsWithNull() {
-		setupData();
-		assertFalse(map.containsKey(null));
+  @Test
+  public void testContains() {
+    setupData();
+    assertTrue(map.containsKey("one"));
+    assertTrue(map.containsKey("two"));
+    assertTrue(map.containsKey("three"));
+    assertFalse(map.containsKey("four"));
+  }
 
-	}
-	
+  @Test
+  public void testContainsNull() {
+    map.add(null, 1);
+    assertTrue(map.containsKey(null));
+  }
 
-	@Test
-	public void testContainsValue()
-	{
-		setupData();
-		assertTrue(map.containsValue(1));
-		assertTrue(map.containsValue(2));
-		assertTrue(map.containsValue(3));
-		assertFalse(map.containsValue(9));
-	}
-	
-	@Test
-	public void putAll(){
-		
-		map.putAll(Map.of("one", 1, "two", 2, "three", 3));
-		assertEquals(6, map.nvPairs.size());
-		assertTrue( map.nvPairs.contains("one"));
-		assertTrue( map.nvPairs.contains(1));
-		assertTrue( map.nvPairs.contains("three"));
-		assertTrue( map.nvPairs.contains(3));
-		/*assertTrue( map.nvPairs.contains(1));
-		assertEquals("one", map.nvPairs.get(0));
-		assertEquals(1, map.nvPairs.get(1));
-		assertEquals("three", map.nvPairs.get(4));
-		assertEquals(3, map.nvPairs.get(5));*/
-		
-	}
-	
-	@Test
-	public void testKeySet(){
-		setupData();
-		assertEquals(3, map.keySet().size());
-		assertTrue(map.keySet().contains("one"));
-		assertTrue(map.keySet().contains("three"));
-		assertFalse(map.keySet().contains("four"));
-	}
-	
-	@Test
-	public void testValues(){
-		setupData();
-		assertEquals(3, map.values().size());
-		assertTrue(map.values().contains(1));
-		assertTrue(map.values().contains(3));
-		assertFalse(map.values().contains(4));
-	}
-	
-	@Test
-	public void entrySet(){
-		setupData();
-		assertEquals(3, map.entrySet().size());
-		
-		
-		assertTrue(map.nvPairs.contains("one"));
-		assertTrue(map.nvPairs.contains(1));
-		assertTrue(map.nvPairs.contains("three"));
-		assertTrue(map.nvPairs.contains(3));
-	}
-	
-	
-	@Test
-	public void testGet()
-	{
-		/*setupData();
+  /***
+   * if the map contains a entry with null as value, contains(null) should be true as it is with other maps e.g. HashMap
+   */
+  @Test
+  public void testContainsValueWithNull() {
+    setupData();
+    map.add("four", null);
+    assertTrue(map.containsValue(null));
+  }
 
-		Integer i1 = map.get((Object) "one");
-		assertEquals(1, (String ) i1);
-		
-		assertTrue(map.get((Object)"one"));
-		assertTrue(map.containsValue(2));
-		assertTrue(map.containsValue(3));
-		assertFalse(map.containsValue(9));*/
-	}
-	
-	@Test
-	public void remove(){
-		
-		
-	}
-	
+  @Test
+  public void testContainsValue() {
+    setupData();
+    assertTrue(map.containsValue(1));
+    assertTrue(map.containsValue(2));
+    assertTrue(map.containsValue(3));
+    assertFalse(map.containsValue(9));
+  }
 
-	private void setupData()
-	{
-		map.add("one", 1);
-		map.add("two", 2);
-		map.add("three", 3);
+  @Test
+  public void testPutAll() {
+    setupData();
+    map.putAll(Map.of("four", 4, "five", 5, " six", 6));
+    assertEquals(12, map.nvPairs.size());
 
-	}
+    assertEquals("one", map.nvPairs.get(0));
+    assertEquals(3, map.nvPairs.get(5));
 
+    // since put all takes a unordered Map, we do not know the order of the elements
+    assertTrue(map.nvPairs.contains("one"));
+    assertTrue(map.nvPairs.contains(1));
+    assertTrue(map.nvPairs.contains("three"));
+    assertTrue(map.nvPairs.contains(3));
+    assertTrue(map.nvPairs.contains(1));
+  }
 
+  @Test
+  public void testKeySet() {
+    setupData();
+    Set<String> keys = map.keySet();
+    assertEquals(3, keys.size());
+    assertTrue(keys.contains("one"));
+    assertTrue(keys.contains("three"));
+    assertFalse(keys.contains("four"));
+  }
+
+  @Test
+  public void testValues() {
+    setupData();
+    Collection<Integer> values = map.values();
+    assertEquals(3, values.size());
+    assertTrue(values.contains(1));
+    assertTrue(values.contains(3));
+    assertFalse(values.contains(4));
+  }
+
+  @Test
+  public void entrySet() {
+    setupData();
+
+    assertEquals(3, map.entrySet().size());
+    assertTrue(map.nvPairs.contains("one"));
+    assertTrue(map.nvPairs.contains(1));
+    assertTrue(map.nvPairs.contains("three"));
+    assertTrue(map.nvPairs.contains(3));
+  }
+
+  @Test
+  public void remove() {
+    setupData();
+    Integer two = map.remove("two");
+
+    assertEquals(Integer.valueOf(2), two);
+    assertEquals(4, map.nvPairs.size());
+    assertFalse(map.containsKey("two"));
+  }
+
+  private void setupData() {
+    map.add("one", 1);
+    map.add("two", 2);
+    map.add("three", 3);
+  }
 }
