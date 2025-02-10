82c50e4291e86e773301cc71278f99f33490ad48
Sanjay Dutt <sanjaydutt@apache.org>
2025-02-10 17:38:47 +0530

    SOLR-16503: Use Http2SolrClient in SolrClientCache, SchemaDesigner (#2764)
    ---------
    Co-authored-by: David Smiley <dsmiley@apache.org>
    
    (cherry picked from commit 1bb9b84b6c43746713561d10612633b61555eef3)

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -837,7 +837,7 @@ public class CoreContainer {
     solrClientProvider =
         new HttpSolrClientProvider(cfg.getUpdateShardHandlerConfig(), solrMetricsContext);
     updateShardHandler.initializeMetrics(solrMetricsContext, "updateShardHandler");
-    solrClientCache = new SolrClientCache(updateShardHandler.getDefaultHttpClient());
+    solrClientCache = new SolrClientCache(solrClientProvider.getSolrClient());
 
     Map<String, CacheConfig> cachesConfig = cfg.getCachesConfig();
     if (cachesConfig.isEmpty()) {
