b8cabb2d27a2a222bdd210468a4ba81a063c576c
Christos Malliaridis <malliaridis@apache.org>
2025-02-18 23:53:09 +0100

    Fix UI module test paths

--- solr/ui/src/commonTest/kotlin/org/apache/solr/ui/views/components/SolrCardTest.kt
+++ solr/ui/src/commonTest/kotlin/org/apache/solr/ui/views/components/SolrCardTest.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
