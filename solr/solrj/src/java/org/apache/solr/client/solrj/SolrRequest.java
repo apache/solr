8e6a1c73a4dbe8de8c01fa4944cda533d743454a
jmuriithi3 <jmuriithi3@bloomberg.net>
2025-03-10 11:29:02 -0400

    Make getRequestType final

--- solr/solrj/src/java/org/apache/solr/client/solrj/SolrRequest.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/SolrRequest.java
@@ -204,7 +204,7 @@ public abstract class SolrRequest<T extends SolrResponse> implements Serializabl
    * special cases. If no special case is identified, {@link SolrRequest#getBaseRequestType()} is
    * returned.
    */
-  public SolrRequestType getRequestType() {
+  public final SolrRequestType getRequestType() {
     if (CommonParams.ADMIN_PATHS.contains(getPath())) {
       return SolrRequestType.ADMIN;
     } else if (this instanceof IsUpdateRequest) {
