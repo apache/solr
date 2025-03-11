c8fef1f874cb0c8345896d800cfffaee6e6655bc
Christine Poerschke <cpoerschke@apache.org>
2025-03-11 15:30:29 +0000

    SOLR-17626: add RawTFSimilarity[Factory] class(es)

---
+++ solr/core/src/java/org/apache/solr/search/similarities/RawTFSimilarity.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.search.similarities;
+
+import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.search.CollectionStatistics;
+import org.apache.lucene.search.TermStatistics;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.util.SmallFloat;
+
+/** Similarity that returns the raw TF as score. */
+public class RawTFSimilarity extends Similarity {
+  private final boolean discountOverlaps;
+
+  /** Default constructor: parameter-free */
+  public RawTFSimilarity() {
+    this(true);
+  }
+
+  /** Primary constructor. */
+  public RawTFSimilarity(boolean discountOverlaps) {
+    this.discountOverlaps = discountOverlaps;
+  }
+
+  /** Returns true if overlap tokens are discounted from the document's length. */
+  public boolean getDiscountOverlaps() {
+    return discountOverlaps;
+  }
+
+  @Override
+  public SimScorer scorer(
+      float boost, CollectionStatistics collectionStats, TermStatistics... termStats) {
+    return new SimScorer() {
+      @Override
+      public float score(float freq, long norm) {
+        return boost * freq;
+      }
+    };
+  }
+
+  @Override
+  public final long computeNorm(FieldInvertState state) {
+    final int numTerms;
+    if (state.getIndexOptions() == IndexOptions.DOCS && state.getIndexCreatedVersionMajor() >= 8) {
+      numTerms = state.getUniqueTermCount();
+    } else if (discountOverlaps) {
+      numTerms = state.getLength() - state.getNumOverlap();
+    } else {
+      numTerms = state.getLength();
+    }
+    return SmallFloat.intToByte4(numTerms);
+  }
+}
