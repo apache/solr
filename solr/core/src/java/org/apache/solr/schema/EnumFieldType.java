7687d2fa0b3066015d893aecbdf40f443cbd396b
David Smiley <dsmiley@apache.org>
2025-04-04 08:48:59 -0400

    revert change to org.apache.solr.schema.EnumFieldType.getSortField

--- solr/core/src/java/org/apache/solr/schema/EnumFieldType.java
+++ solr/core/src/java/org/apache/solr/schema/EnumFieldType.java
@@ -449,30 +449,7 @@ public class EnumFieldType extends PrimitiveFieldType {
 
   @Override
   public SortField getSortField(SchemaField field, boolean top) {
-    if (field.multiValued()) {
-      MultiValueSelector selector = field.type.getDefaultMultiValueSelectorForSort(field, top);
-      if (null != selector) {
-        final SortField result =
-            getSortedSetSortField(
-                field,
-                selector.getSortedSetSelectorType(),
-                // yes: Strings, it's how SortedSetSortField works
-                top,
-                SortField.STRING_FIRST,
-                SortField.STRING_LAST);
-        if (null == result.getMissingValue()) {
-          // special case 'enum' default behavior: assume missing values are "below" all enum values
-          result.setMissingValue(SortField.STRING_FIRST);
-        }
-        return result;
-      }
-    }
-
-    // else...
-    // either single valued, or don't support implicit multi selector
-    // (in which case let getSortField() give the error)
     final SortField result = getNumericSort(field, NumberType.INTEGER, top);
-
     if (null == result.getMissingValue()) {
       // special case 'enum' default behavior: assume missing values are "below" all enum values
       result.setMissingValue(Integer.MIN_VALUE);
