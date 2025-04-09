ed1362e3fa70de48132775e25a012c22cc46733c
Eric Pugh <epugh@opensourceconnections.com>
2025-04-09 10:24:52 -0400

    Support deprecation warning of system properties
    Converts non standard old system properties into the current standard and warns users about upgrading.

--- solr/solrj/src/test/org/apache/solr/common/util/EnvUtilsTest.java
+++ solr/solrj/src/test/org/apache/solr/common/util/EnvUtilsTest.java
@@ -19,6 +19,7 @@ package org.apache.solr.common.util;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import org.apache.solr.SolrTestCase;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -41,7 +42,7 @@ public class EnvUtilsTest extends SolrTestCase {
   @BeforeClass
   public static void beforeClass() throws Exception {
     // Make a map of some common Solr environment variables for testing, and initialize EnvUtils
-    EnvUtils.init(true, ENV);
+    EnvUtils.init(true, ENV, System.getProperties());
   }
 
   @Test
@@ -93,9 +94,19 @@ public class EnvUtilsTest extends SolrTestCase {
   public void testOverwrite() {
     EnvUtils.setProperty("solr.overwrite", "original");
     var env2 = Map.of("SOLR_OVERWRITE", "overwritten");
-    EnvUtils.init(false, env2);
+    EnvUtils.init(false, env2, new Properties());
     assertEquals("original", EnvUtils.getProperty("solr.overwrite"));
-    EnvUtils.init(true, env2);
+    EnvUtils.init(true, env2, new Properties());
     assertEquals("overwritten", EnvUtils.getProperty("solr.overwrite"));
+  }
+
+  @Test
+  public void testDeprecated() {
+    var env = Map.of("SOLR_OVERWRITE", "overwritten");
+    Properties defaultProps = new Properties();
+    defaultProps.setProperty("solrConfigSetForbiddenFileTypes", "xml,json,jar");
+
+    EnvUtils.init(false, env, defaultProps);
+    assertEquals("xml,json,jar", EnvUtils.getProperty("solr.configset.forbidden.file.types"));
   }
 }
