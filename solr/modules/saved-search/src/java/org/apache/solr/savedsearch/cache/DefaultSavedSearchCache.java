9d3c63da80f5262e70529f9df69c2406cd5416e2
Luke Kot-Zaniewski <lkotzaniewsk@bloomberg.net>
2025-06-27 18:40:48 -0400

    move regenerator into DefaultSavedSearchCache

--- solr/modules/saved-search/src/java/org/apache/solr/savedsearch/cache/DefaultSavedSearchCache.java
+++ solr/modules/saved-search/src/java/org/apache/solr/savedsearch/cache/DefaultSavedSearchCache.java
@@ -30,22 +30,28 @@ import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiPredicate;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.monitor.Visitors.QueryTermFilterVisitor;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.RamUsageEstimator;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.SolrException.ErrorCode;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.core.SolrCore;
 import org.apache.solr.metrics.MetricsMap;
 import org.apache.solr.metrics.SolrMetricsContext;
 import org.apache.solr.savedsearch.SavedSearchDataValues;
 import org.apache.solr.savedsearch.SavedSearchDataValues.QueryDisjunct;
 import org.apache.solr.savedsearch.SavedSearchDecoder;
+import org.apache.solr.savedsearch.cache.SavedSearchCache.VersionedQueryCacheEntry;
 import org.apache.solr.search.CacheRegenerator;
 import org.apache.solr.search.SolrCache;
 import org.apache.solr.search.SolrCacheBase;
 import org.apache.solr.search.SolrIndexSearcher;
 import org.apache.solr.util.IOFunction;
-import org.apache.solr.savedsearch.cache.SavedSearchCache.VersionedQueryCacheEntry;
 
 public class DefaultSavedSearchCache extends SolrCacheBase
     implements SolrCache<String, VersionedQueryCacheEntry>,
@@ -325,6 +331,69 @@ public class DefaultSavedSearchCache extends SolrCacheBase
 
     private static CurrentStats miss(CurrentStats old) {
       return new CurrentStats(old.hits, old.lookups + 1);
+    }
+  }
+
+  public static class SavedSearchLatestRegenerator implements CacheRegenerator {
+
+    private static final int MAX_BATCH_SIZE = 1 << 10;
+
+    @Override
+    public <K, V> boolean regenerateItem(
+        SolrIndexSearcher searcher,
+        SolrCache<K, V> newCache,
+        SolrCache<K, V> oldCache,
+        K oldKey,
+        V oldVal)
+        throws IOException {
+      if (!(newCache instanceof DefaultSavedSearchCache)) {
+        throw new IllegalArgumentException(
+            this.getClass().getSimpleName()
+                + " only supports "
+                + DefaultSavedSearchCache.class.getSimpleName());
+      }
+      var cache = (DefaultSavedSearchCache) newCache;
+      var reader = searcher.getIndexReader();
+      int batchSize = Math.min(MAX_BATCH_SIZE, cache.getInitialSize());
+      var topDocs =
+          new IndexSearcher(searcher.getTopReaderContext())
+              .search(versionRangeQuery(cache), batchSize)
+              .scoreDocs;
+      int batchesRemaining = cache.getInitialSize() / batchSize - 1;
+      SolrCore core = searcher.getCore();
+      SavedSearchDecoder decoder = new SavedSearchDecoder(core);
+      while (topDocs.length > 0 && batchesRemaining > 0) {
+        int docIndex = 0;
+        for (LeafReaderContext ctx : reader.leaves()) {
+          SavedSearchDataValues dataValues =
+              new SavedSearchDataValues(ctx, core.getLatestSchema().getUniqueKeyField().getName());
+          int shiftedMax = ctx.reader().maxDoc() + ctx.docBase;
+          while (docIndex < topDocs.length
+              && topDocs[docIndex].doc >= ctx.docBase
+              && topDocs[docIndex].doc < shiftedMax) {
+            int doc = topDocs[docIndex].doc - ctx.docBase;
+            docIndex++;
+            if (dataValues.advanceTo(doc)) {
+              cache.computeIfStale(dataValues, decoder);
+            }
+            cache.versionHighWaterMark =
+                Math.max(cache.versionHighWaterMark, dataValues.getVersion());
+            cache.docVisits++;
+          }
+        }
+        var scoreDoc = topDocs[topDocs.length - 1];
+        topDocs =
+            new IndexSearcher(searcher.getTopReaderContext())
+                .searchAfter(scoreDoc, versionRangeQuery(cache), batchSize)
+                .scoreDocs;
+        batchesRemaining--;
+      }
+      return false;
+    }
+
+    private static Query versionRangeQuery(DefaultSavedSearchCache cache) {
+      return LongPoint.newRangeQuery(
+          CommonParams.VERSION_FIELD, cache.versionHighWaterMark, Long.MAX_VALUE);
     }
   }
 }
