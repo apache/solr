54caed137798dc1185808c1ba399fe4fa6980eb5
Anna Ruggero <a.ruggero@sease.io>
2025-07-14 17:17:07 +0200

    Added first tests

--- solr/modules/ltr/src/test/org/apache/solr/ltr/TestFeatureVectorCache.java
+++ solr/modules/ltr/src/test/org/apache/solr/ltr/TestFeatureVectorCache.java
@@ -17,74 +17,226 @@
 package org.apache.solr.ltr;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.core.SolrCore;
+import org.apache.solr.metrics.MetricsMap;
+import org.apache.solr.metrics.SolrMetricManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 public class TestFeatureVectorCache extends TestRerankBase {
+    SolrCore core = null;
+
     @Before
     public void before() throws Exception {
         setupFeatureVectorCachetest(false);
 
-        assertU(adoc("id", "1", "title", "w1", "description", "w1", "popularity", "1"));
-        assertU(adoc("id", "2", "title", "w2", "description", "w2", "popularity", "2"));
-        assertU(adoc("id", "3", "title", "w3", "description", "w3", "popularity", "3"));
+        assertU(adoc("id", "1", "title", "w2", "description", "w2", "popularity", "0"));
         assertU(commit());
 
         loadFeatures("featurevectorcache_features.json");
         loadModels("featurevectorcache_linear_model.json");
+
+        core = solrClientTestRule.getCoreContainer().getCore(DEFAULT_TEST_CORENAME);
     }
 
     @After
     public void after() throws Exception {
+        core.close();
         aftertest();
     }
 
-    @Test
-    public void testFeatureVectorCacheLogging() throws Exception {
-        final String doc1_feature_vector =
-                FeatureLoggerTestUtils.toFeatureVector(
-                        "value_feature_1", "1.0",
-                        "efi_feature", "3.0",
-                        "match_w1_title", "1.0",
-                        "popularity_value", "1.0");
-
-        final String doc3_feature_vector =
-                FeatureLoggerTestUtils.toFeatureVector(
-                        "value_feature_1", "1.0",
-                        "efi_feature", "3.0",
-                        "match_w1_title", "0.0",
-                        "popularity_value", "3.0");
-
-        final SolrQuery query = new SolrQuery();
-        query.setQuery("*:*");
-        query.add("rows", "3");
-        query.add("fl", "[fv format=dense efi.efi_feature=3]");
-
-        // Feature vectors without caching
-        assertJQ(
-                "/query" + query.toQueryString(),
-                "/response/docs/[0]/=={'[fv]':'" + doc1_feature_vector + "'}");
-
-        // Feature vectors with caching
-        query.add("sort", "popularity desc");
-        assertJQ(
-                "/query" + query.toQueryString(),
-                "/response/docs/[0]/=={'[fv]':'" + doc3_feature_vector + "'}");
+    private static Map<String, Object> lookupFilterCacheMetrics(SolrCore core) {
+        return ((MetricsMap)
+                ((SolrMetricManager.GaugeWrapper<?>)
+                        core.getCoreMetricManager()
+                                .getRegistry()
+                                .getMetrics()
+                                .get("CACHE.searcher.featureVectorCache"))
+                        .getGauge())
+                .getValue();
     }
 
     @Test
-    public void testFeatureVectorCacheRerank() throws Exception {
+    public void testFeatureVectorCache_loggingDefaultStoreNoReranking() throws Exception {
+        final String docs0fv_dense_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "value_feature_3", "3.0", "efi_feature", "3.0",
+                "match_w1_title", "0.0", "popularity_value", "0.0");
+        final String docs0fv_sparse_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "efi_feature", "3.0");
+
+        final String docs0fv_default_csv =
+                chooseDefaultFeatureVector(docs0fv_dense_csv, docs0fv_sparse_csv);
+
         final SolrQuery query = new SolrQuery();
         query.setQuery("*:*");
         query.add("rows", "3");
-        query.add("fl", "*,score");
+        query.add("fl", "[fv efi.efi_feature=3]");
+
+        // No caching, we want to see lookups, an insertions and no hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        Map<String, Object> filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(1, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(0, (long) filterCacheMetrics.get("hits"));
+
+        query.add("sort", "popularity desc");
+        // Caching, we want to see hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(2, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(1, (long) filterCacheMetrics.get("hits"));
+    }
+
+    @Test
+    public void testFeatureVectorCache_loggingExplicitStoreNoReranking() throws Exception {
+        final String docs0fv_dense_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "match_w1_title", "0.0", "value_feature_2", "2.0");
+        final String docs0fv_sparse_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_2", "2.0");
+
+        final String docs0fv_default_csv =
+                chooseDefaultFeatureVector(docs0fv_dense_csv, docs0fv_sparse_csv);
+
+        final SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.add("rows", "3");
+        query.add("fl", "[fv store=store1 efi.efi_feature=3]");
+
+        // No caching, we want to see lookups, an insertions and no hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        Map<String, Object> filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(1, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(0, (long) filterCacheMetrics.get("hits"));
+
+        query.add("sort", "popularity desc");
+        // Caching, we want to see hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(2, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(1, (long) filterCacheMetrics.get("hits"));
+    }
+
+    @Test
+    public void testFeatureVectorCache_loggingModelStoreRerankingDifferentEfi() throws Exception {
+        final String docs0fv_dense_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "efi_feature", "3.0",
+                "match_w1_title", "0.0", "popularity_value", "0.0");
+        final String docs0fv_sparse_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "efi_feature", "3.0");
+
+        final String docs0fv_default_csv =
+                chooseDefaultFeatureVector(docs0fv_dense_csv, docs0fv_sparse_csv);
+
+        final SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.add("rows", "3");
+        query.add("fl", "[fv efi.efi_feature=3]");
         query.add("rq", "{!ltr reRankDocs=3 model=featurevectorcache_linear_model efi.efi_feature=4}");
 
-        // Feature vectors without caching
-        assertJQ("/query" + query.toQueryString(), "/response/docs/[0]/score==4.2");
+        // No caching, we want to see lookups, an insertions and no hits since the efi are different
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        Map<String, Object> filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(2, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(2, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(0, (long) filterCacheMetrics.get("hits"));
 
-        // Feature vectors with caching
-        assertJQ("/query" + query.toQueryString(), "/response/docs/[2]/score==3.1");
+        query.add("sort", "popularity desc");
+        // Caching, we want to see hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(2, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(4, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(2, (long) filterCacheMetrics.get("hits"));
+    }
+
+    @Test
+    public void testFeatureVectorCache_loggingModelStoreRerankingSameEfi() throws Exception {
+        final String docs0fv_dense_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "efi_feature", "4.0",
+                "match_w1_title", "0.0", "popularity_value", "0.0");
+        final String docs0fv_sparse_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_1", "1.0", "efi_feature", "4.0");
+
+        final String docs0fv_default_csv =
+                chooseDefaultFeatureVector(docs0fv_dense_csv, docs0fv_sparse_csv);
+
+        final SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.add("rows", "3");
+        query.add("fl", "[fv efi.efi_feature=4]");
+        query.add("rq", "{!ltr reRankDocs=3 model=featurevectorcache_linear_model efi.efi_feature=4}");
+
+        // No caching for reranking but logging should hit since we have the same feature store and efis
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        Map<String, Object> filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(2, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(1, (long) filterCacheMetrics.get("hits"));
+
+        query.add("sort", "popularity desc");
+        // Caching, we want to see hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(1, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(4, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(3, (long) filterCacheMetrics.get("hits"));
+    }
+
+    @Test
+    public void testFeatureVectorCache_loggingExplicitStoreReranking() throws Exception {
+        final String docs0fv_dense_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "match_w1_title", "0.0", "value_feature_2", "2.0");
+        final String docs0fv_sparse_csv = FeatureLoggerTestUtils.toFeatureVector(
+                "value_feature_2", "2.0");
+
+        final String docs0fv_default_csv =
+                chooseDefaultFeatureVector(docs0fv_dense_csv, docs0fv_sparse_csv);
+
+        final SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.add("rows", "3");
+        query.add("fl", "[fv store=store1 efi.efi_feature=3]");
+        query.add("rq", "{!ltr reRankDocs=3 model=featurevectorcache_linear_model efi.efi_feature=4}");
+
+        // No caching, we want to see lookups, an insertions and no hits since the efi are different
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        Map<String, Object> filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(2, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(2, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(0, (long) filterCacheMetrics.get("hits"));
+
+        query.add("sort", "popularity desc");
+        // Caching, we want to see hits
+        assertJQ(
+                "/query" + query.toQueryString(),
+                "/response/docs/[0]/=={'[fv]':'" + docs0fv_default_csv + "'}");
+        filterCacheMetrics = lookupFilterCacheMetrics(core);
+        assertEquals(2, (long) filterCacheMetrics.get("inserts"));
+        assertEquals(4, (long) filterCacheMetrics.get("lookups"));
+        assertEquals(2, (long) filterCacheMetrics.get("hits"));
     }
 }
