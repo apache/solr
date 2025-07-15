c25e6267d8ff08202094ef951da4a60dbb4fe5fe
Christos Malliaridis <c.malliaridis@gmail.com>
2025-07-15 17:13:46 +0300

    Update solr/ui/src/wasmJsMain/kotlin/org/apache/solr/ui/errors/parseError.kt
    Co-authored-by: Eric Pugh <epugh@opensourceconnections.com>

--- solr/ui/src/wasmJsMain/kotlin/org/apache/solr/ui/errors/parseError.kt
+++ solr/ui/src/wasmJsMain/kotlin/org/apache/solr/ui/errors/parseError.kt
@@ -19,7 +19,7 @@ package org.apache.solr.ui.errors
 
 actual fun parseError(error: Throwable): Throwable {
     // In JavaScript the errors do not have any type to distinguish them, so we strongly
-    // relay on the error message.
+    // rely on the error message.
     if (error.message?.contains(other = "Fail to fetch") == true) return HostNotFoundException(cause = error)
 
     // fallback
