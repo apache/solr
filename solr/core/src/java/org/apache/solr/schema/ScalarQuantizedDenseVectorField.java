33b510d300a445ff5ec1154194638f572404c5f2
kliang78 <kliang78@bloomberg.net>
2025-07-15 10:55:49 -0400

    code review changes

--- solr/core/src/java/org/apache/solr/schema/ScalarQuantizedDenseVectorField.java
+++ solr/core/src/java/org/apache/solr/schema/ScalarQuantizedDenseVectorField.java
@@ -16,103 +16,100 @@
  */
 package org.apache.solr.schema;
 
+import static java.util.Optional.ofNullable;
+import static org.apache.lucene.codecs.lucene99.Lucene99HnswVectorsFormat.DEFAULT_NUM_MERGE_WORKER;
+
+import java.util.Map;
 import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.lucene99.Lucene99HnswScalarQuantizedVectorsFormat;
 import org.apache.lucene.codecs.lucene99.Lucene99ScalarQuantizedVectorsFormat;
 import org.apache.lucene.index.VectorEncoding;
 import org.apache.lucene.index.VectorSimilarityFunction;
-import org.apache.solr.common.SolrException;
-
-import java.util.Map;
-
-import static java.util.Optional.ofNullable;
-import static org.apache.lucene.codecs.lucene99.Lucene99HnswVectorsFormat.DEFAULT_NUM_MERGE_WORKER;
 
 public class ScalarQuantizedDenseVectorField extends DenseVectorField {
-    public static final String BITS_PARAM = "bits"; //
-    public static final String CONFIDENCE_INTERVAL_PARAM = "confidenceInterval";
-    public static final String DYNAMIC_CONFIDENCE_INTERVAL_PARAM = "dynamicConfidenceInterval";
-    public static final String COMPRESS_PARAM = "compress"; // can only be enabled when bits = 4 per Lucene codec spec
+  public static final String BITS_PARAM = "bits"; //
+  public static final String CONFIDENCE_INTERVAL_PARAM = "confidenceInterval";
+  public static final String DYNAMIC_CONFIDENCE_INTERVAL_PARAM = "dynamicConfidenceInterval";
+  public static final String COMPRESS_PARAM =
+      "compress"; // can only be enabled when bits = 4 per Lucene codec spec
 
-    private static final int DEFAULT_BITS = 7; // use signed byte as default when unspecified
-    private static final Float DEFAULT_CONFIDENCE_INTERVAL = null; // use dimension scaled confidence interval
+  private static final int DEFAULT_BITS = 7; // use signed byte as default when unspecified
+  private static final Float DEFAULT_CONFIDENCE_INTERVAL =
+      null; // use dimension scaled confidence interval
 
-    /**
-     * Number of bits to use for storage
-     * Must be 4 (half-byte) or 7 (signed-byte) per Lucene codec spec
-     */
-    private int bits;
+  /**
+   * Number of bits to use for storage Must be 4 (half-byte) or 7 (signed-byte) per Lucene codec
+   * spec
+   */
+  private int bits;
 
-    /**
-     * Confidence interval to use for scalar quantization
-     * Default is calculated as `1-1/(vector_dimensions + 1)`
-     */
-    private Float confidenceInterval;
+  /**
+   * Confidence interval to use for scalar quantization Default is calculated as
+   * `1-1/(vector_dimensions + 1)`
+   */
+  private Float confidenceInterval;
 
-    /**
-     * When enabled, in conjunction with 4 bit size, will pair values into single bytes for 50% reduction in memory usage
-     * (comes at the cost of some decode speed penalty)
-     */
-    private boolean compress;
+  /**
+   * When enabled, in conjunction with 4 bit size, will pair values into single bytes for 50%
+   * reduction in memory usage (comes at the cost of some decode speed penalty)
+   */
+  private boolean compress;
 
-    public ScalarQuantizedDenseVectorField(int dimension,
-                                           VectorSimilarityFunction similarityFunction,
-                                           VectorEncoding vectorEncoding,
-                                           int bits,
-                                           Float confidenceInterval,
-                                           boolean compress) {
-        super(dimension, similarityFunction, vectorEncoding);
-        this.bits = bits;
-        this.confidenceInterval = confidenceInterval;
-        this.compress = compress;
+  public ScalarQuantizedDenseVectorField(
+      int dimension,
+      VectorSimilarityFunction similarityFunction,
+      VectorEncoding vectorEncoding,
+      int bits,
+      Float confidenceInterval,
+      boolean compress) {
+    super(dimension, similarityFunction, vectorEncoding);
+    this.bits = bits;
+    this.confidenceInterval = confidenceInterval;
+    this.compress = compress;
+  }
+
+  @Override
+  public void init(IndexSchema schema, Map<String, String> args) {
+    super.init(schema, args);
+
+    this.bits = ofNullable(args.remove(BITS_PARAM)).map(Integer::parseInt).orElse(DEFAULT_BITS);
+
+    this.compress =
+        ofNullable(args.remove(COMPRESS_PARAM)).map(Boolean::parseBoolean).orElse(false);
+
+    this.confidenceInterval =
+        ofNullable(args.remove(CONFIDENCE_INTERVAL_PARAM))
+            .map(Float::parseFloat)
+            .orElse(DEFAULT_CONFIDENCE_INTERVAL);
+
+    if (ofNullable(args.remove(DYNAMIC_CONFIDENCE_INTERVAL_PARAM))
+        .map(Boolean::parseBoolean)
+        .orElse(false)) {
+      this.confidenceInterval = Lucene99ScalarQuantizedVectorsFormat.DYNAMIC_CONFIDENCE_INTERVAL;
     }
+  }
 
-    @Override
-    public void init(IndexSchema schema, Map<String, String> args) {
-        super.init(schema, args);
+  @Override
+  public KnnVectorsFormat buildKnnVectorsFormat() {
+    return new Lucene99HnswScalarQuantizedVectorsFormat(
+        getHnswMaxConn(),
+        getHnswBeamWidth(),
+        DEFAULT_NUM_MERGE_WORKER,
+        getBits(),
+        useCompression(),
+        getConfidenceInterval(),
+        null);
+  }
 
-        this.bits = ofNullable(args.remove(BITS_PARAM))
-                .map(Integer::parseInt)
-                .orElse(DEFAULT_BITS);
+  public int getBits() {
+    return bits;
+  }
 
-        this.compress = ofNullable(args.remove(COMPRESS_PARAM))
-                .map(Boolean::parseBoolean)
-                .orElse(false);
+  public boolean useCompression() {
+    return compress;
+  }
 
-        this.confidenceInterval = ofNullable(args.remove(CONFIDENCE_INTERVAL_PARAM))
-                .map(Float::parseFloat)
-                .orElse(DEFAULT_CONFIDENCE_INTERVAL);
-
-        if (ofNullable(args.remove(DYNAMIC_CONFIDENCE_INTERVAL_PARAM))
-                .map(Boolean::parseBoolean)
-                .orElse(false)) {
-            this.confidenceInterval = Lucene99ScalarQuantizedVectorsFormat.DYNAMIC_CONFIDENCE_INTERVAL;
-        }
-    }
-
-    @Override
-    public KnnVectorsFormat buildKnnVectorsFormat() {
-        return new Lucene99HnswScalarQuantizedVectorsFormat(
-                getHnswMaxConn(),
-                getHnswBeamWidth(),
-                DEFAULT_NUM_MERGE_WORKER,
-                getBits(),
-                useCompression(),
-                getConfidenceInterval(),
-                null
-        );
-    }
-
-    public int getBits() {
-        return bits;
-    }
-
-    public boolean useCompression() {
-        return compress;
-    }
-
-    public Float getConfidenceInterval() {
-        return confidenceInterval;
-    }
-
+  public Float getConfidenceInterval() {
+    return confidenceInterval;
+  }
 }
