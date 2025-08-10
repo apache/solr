242c1fc4e8a1402fd31e7c5c837ec381ec741a89
Eric Pugh <epugh@opensourceconnections.com>
2025-08-10 17:04:44 -0400

    SOLR-17852: Migrate schema designer to filestoreapi part deux (#3031)
    Now using the FileStore API for persisting working documents used in the Schema Designer.

--- solr/core/src/test/org/apache/solr/handler/designer/TestSchemaDesignerAPI.java
+++ solr/core/src/test/org/apache/solr/handler/designer/TestSchemaDesignerAPI.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.request.CollectionAdminRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.cloud.SolrCloudTestCase;
 import org.apache.solr.common.SolrDocumentList;
@@ -72,9 +71,6 @@ public class TestSchemaDesignerAPI extends SolrCloudTestCase implements SchemaDe
     configureCluster(1)
         .addConfig(DEFAULT_CONFIGSET_NAME, ExternalPaths.DEFAULT_CONFIGSET)
         .configure();
-    // SchemaDesignerAPI depends on the blob store ".system" collection existing.
-    CollectionAdminRequest.createCollection(BLOB_STORE_ID, 1, 1).process(cluster.getSolrClient());
-    cluster.waitForActiveCollection(BLOB_STORE_ID, 1, 1);
   }
 
   @AfterClass
