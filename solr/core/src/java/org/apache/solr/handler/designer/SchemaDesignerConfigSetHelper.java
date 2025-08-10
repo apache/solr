6aa6a64aa0b8558d6c5ab85efb7541cf63b81156
Eric Pugh <epugh@opensourceconnections.com>
2025-08-10 16:33:46 -0400

    fix precommit

--- solr/core/src/java/org/apache/solr/handler/designer/SchemaDesignerConfigSetHelper.java
+++ solr/core/src/java/org/apache/solr/handler/designer/SchemaDesignerConfigSetHelper.java
@@ -500,7 +500,7 @@ class SchemaDesignerConfigSetHelper implements SchemaDesignerConstants {
                 try (InputStream is = entry.getInputStream()) {
                   docs.set((List<SolrInputDocument>) Utils.fromJavabin(is));
                 } catch (IOException e) {
-                  log.error("Error reading file content for path " + path, e);
+                  log.error("Error reading file content at path {}", path, e);
                 }
               },
               true);
