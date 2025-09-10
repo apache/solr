478374980498bbb5d4b643d7c89fded6723b7188
Jan Høydahl <janhoy@apache.org>
2025-09-10 10:59:56 +0200

    Update gradle to 8.14.3

--- build-tools/build-infra/src/main/java/org/apache/lucene/gradle/WrapperDownloader.java
+++ build-tools/build-infra/src/main/java/org/apache/lucene/gradle/WrapperDownloader.java
@@ -54,9 +54,9 @@ public class WrapperDownloader {
 
   public static void checkVersion() {
     int major = Runtime.version().feature();
-    if (major < 21 || major > 23) {
+    if (major < 21 || major > 24) {
       throw new IllegalStateException(
-          "java version must be between 21 and 23, your version: " + major);
+          "java version must be between 21 and 24, your version: " + major);
     }
   }
 
