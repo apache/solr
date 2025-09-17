f23740b5ff29e504d88df9b74ef257312d91c2d3
Christos  Malliaridis <c.malliaridis@gmail.com>
2025-09-17 23:23:36 +0300

    fixup! Apply review changes and add minor adjustments

--- solr/ui/src/commonMain/kotlin/org/apache/solr/ui/views/configsets/ConfigsetsContent.kt
+++ solr/ui/src/commonMain/kotlin/org/apache/solr/ui/views/configsets/ConfigsetsContent.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -65,7 +66,7 @@ fun ConfigsetsContent(
         )
         ConfigsetsDropdown(
             selectedConfigSet = model.selectedConfigset,
-            selectConfigset = { s: String -> component.onSelectConfigset(s) },
+            selectConfigset = component::onSelectConfigset,
             availableConfigsets = model.configsets,
         )
 
