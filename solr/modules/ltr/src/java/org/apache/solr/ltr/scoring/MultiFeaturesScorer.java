0d8f3e788504ea6c5c5bc8200c11d5b50529c58a
Anna Ruggero <a.ruggero@sease.io>
2025-09-22 14:56:15 +0200

    Added package-info and did gradle tidy

--- solr/modules/ltr/src/java/org/apache/solr/ltr/scoring/MultiFeaturesScorer.java
+++ solr/modules/ltr/src/java/org/apache/solr/ltr/scoring/MultiFeaturesScorer.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.ltr.scoring;
 
 import java.io.IOException;
@@ -15,6 +31,10 @@ import org.apache.solr.ltr.feature.extraction.MultiFeaturesExtractor;
 import org.apache.solr.ltr.model.LTRScoringModel;
 import org.apache.solr.request.SolrQueryRequest;
 
+/**
+ * This class is responsible for extracting more than one feature and using them to score the
+ * document.
+ */
 public class MultiFeaturesScorer extends FeatureTraversalScorer {
   private final DisiPriorityQueue subScorers;
   private final List<DisiWrapper> wrappers;
