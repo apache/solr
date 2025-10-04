1b67618e92744d548a5e2082975ae9ddf59bd70c
Jan Høydahl <janhoy@apache.org>
2025-10-04 23:52:58 +0200

    Docs for ExtractionRequest

--- solr/modules/extraction/src/java/org/apache/solr/handler/extraction/ExtractionRequest.java
+++ solr/modules/extraction/src/java/org/apache/solr/handler/extraction/ExtractionRequest.java
@@ -21,20 +21,39 @@ import java.util.Map;
 
 /** Immutable request info needed by extraction backends. */
 public class ExtractionRequest {
-  public final String streamType; // explicit MIME type (optional)
-  public final String resourceName; // filename hint
-  public final String contentType; // HTTP content-type header
-  public final String charset; // derived charset if available
+  public final String streamType;
+  public final String resourceName;
+  public final String contentType;
+  public final String charset;
   public final String streamName;
   public final String streamSourceInfo;
   public final Long streamSize;
-  public final String resourcePassword; // optional password for encrypted docs
-  public final java.util.LinkedHashMap<java.util.regex.Pattern, String>
-      passwordsMap; // optional passwords map
+  public final String resourcePassword;
+  public final java.util.LinkedHashMap<java.util.regex.Pattern, String> passwordsMap;
   public final String extractFormat;
+  // TODO: This is only used by TikaServerExtractionBackend.
   public final boolean recursive;
+  // TODO: This is only used by TikaServerExtractionBackend, change to `features` map?
   public final Map<String, String> tikaRequestHeaders = new HashMap<>();
 
+  /**
+   * Constructs an ExtractionRequest object containing metadata and configurations for extraction
+   * backends.
+   *
+   * @param streamType the explicit MIME type of the document (optional)
+   * @param resourceName the name of the resource, typically a filename hint
+   * @param contentType the HTTP content-type header value
+   * @param charset the derived character set of the stream if available
+   * @param streamName the name of the content stream
+   * @param streamSourceInfo additional information about the stream source
+   * @param streamSize the size of the stream in bytes
+   * @param resourcePassword an optional password used for encrypted documents
+   * @param passwordsMap an optional map of regex patterns to passwords for encrypted content
+   * @param extractFormat the desired format for extraction output
+   * @param recursive a flag indicating whether extraction should be recursive. TikaServer only
+   * @param tikaRequestHeaders optional headers to be included in requests to the extraction
+   *     service. TikaServer only
+   */
   public ExtractionRequest(
       String streamType,
       String resourceName,
