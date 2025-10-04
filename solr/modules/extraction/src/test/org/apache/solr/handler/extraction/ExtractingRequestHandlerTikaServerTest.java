4397fe6ec6c61fdec927e2a2e0b1092d7cf54981
Jan Høydahl <janhoy@apache.org>
2025-10-04 11:49:47 +0200

    Use JUnit @ClassRule for cleaning up container Add some thread names to filter

--- solr/modules/extraction/src/test/org/apache/solr/handler/extraction/ExtractingRequestHandlerTikaServerTest.java
+++ solr/modules/extraction/src/test/org/apache/solr/handler/extraction/ExtractingRequestHandlerTikaServerTest.java
@@ -24,6 +24,7 @@ import org.apache.solr.SolrIgnoredThreadsFilter;
 import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
@@ -38,7 +39,13 @@ import org.testcontainers.containers.wait.strategy.Wait;
     })
 public class ExtractingRequestHandlerTikaServerTest extends ExtractingRequestHandlerTestAbstract {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  private static GenericContainer<?> tika;
+
+  @SuppressWarnings("resource")
+  @ClassRule
+  public static GenericContainer<?> tika =
+      new GenericContainer<>("apache/tika:3.2.3.0-full")
+          .withExposedPorts(9998)
+          .waitingFor(Wait.forListeningPort());
 
   // Ignore known non-daemon threads spawned by Testcontainers and Java HttpClient in this test
   @SuppressWarnings("NewClassNamingConvention")
@@ -49,19 +56,16 @@ public class ExtractingRequestHandlerTikaServerTest extends ExtractingRequestHan
       String n = t.getName();
       return n.startsWith("testcontainers-ryuk")
           || n.startsWith("testcontainers-wait-")
+          || n.startsWith("testcontainers-pull-watchdog-")
+          || n.equals("JNA Cleaner")
           || n.startsWith("HttpClient-")
           || n.startsWith("HttpClient-TestContainers");
     }
   }
 
-  @SuppressWarnings("resource")
   @BeforeClass
   public static void beforeClassTika() {
     String baseUrl;
-    tika =
-        new GenericContainer<>("apache/tika:3.2.3.0-full")
-            .withExposedPorts(9998)
-            .waitingFor(Wait.forListeningPort());
     try {
       tika.start();
       baseUrl = "http://" + tika.getHost() + ":" + tika.getMappedPort(9998);
@@ -69,9 +73,6 @@ public class ExtractingRequestHandlerTikaServerTest extends ExtractingRequestHan
       System.setProperty("solr.test.extraction.backend", "tikaserver");
       log.info("Using extraction backend 'tikaserver'. Tika server running on {}", baseUrl);
     } catch (Throwable t) {
-      // Best-effort cleanup to avoid leaking resources if class initialization fails
-      System.clearProperty("solr.test.tikaserver.url");
-      System.clearProperty("solr.test.extraction.backend");
       // Skip tests if Docker/Testcontainers are not available in the environment
       Assume.assumeNoException("Docker/Testcontainers not available; skipping test", t);
     }
@@ -79,17 +80,6 @@ public class ExtractingRequestHandlerTikaServerTest extends ExtractingRequestHan
 
   @AfterClass
   public static void afterClassTika() {
-    if (tika != null) {
-      try {
-        tika.stop();
-      } finally {
-        try {
-          tika.close();
-        } catch (Throwable ignore2) {
-        }
-        tika = null;
-      }
-    }
     System.clearProperty("solr.test.tikaserver.url");
     System.clearProperty("solr.test.extraction.backend");
   }
