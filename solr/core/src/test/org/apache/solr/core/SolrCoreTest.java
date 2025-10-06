3b93da49910b79eb94a7c1a95b56a37dccf1bb4e
Matthew Biscocho <mlbiscoc@apache.org>
2025-10-06 19:46:13 -0400

    SOLR-17853: Migrate Plugins / Stats UI tab with Prometheus metrics (#3623)
    * Switch plugins/stats tab to use prometheus
    
    * Remove /admin/plugins handler
    
    * Changes from comments
    
    * Add JFR permission to security.policy
    
    * Remove extra space
    
    ---------
    
    Co-authored-by: Matthew Biscocho <mbiscocho@bloomberg.net>

--- solr/core/src/test/org/apache/solr/core/SolrCoreTest.java
+++ solr/core/src/test/org/apache/solr/core/SolrCoreTest.java
@@ -97,11 +97,7 @@ public class SolrCoreTest extends SolrTestCaseJ4 {
       ++ihCount;
       assertEquals(pathToClassMap.get("/admin/luke"), "solr.LukeRequestHandler");
       ++ihCount;
-      assertEquals(pathToClassMap.get("/admin/mbeans"), "solr.SolrInfoMBeanHandler");
-      ++ihCount;
       assertEquals(pathToClassMap.get("/admin/ping"), "solr.PingRequestHandler");
-      ++ihCount;
-      assertEquals(pathToClassMap.get("/admin/plugins"), "solr.PluginInfoHandler");
       ++ihCount;
       assertEquals(pathToClassMap.get("/admin/segments"), "solr.SegmentsInfoRequestHandler");
       ++ihCount;
