549074eb55e469c9bb9de50b24c5ba3cf5ed5019
Eric Pugh <epugh@opensourceconnections.com>
2025-10-06 20:47:29 -0400

    Inline the call to handleCustomAction that just throws the exception anyway.

--- solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
@@ -210,13 +210,8 @@ public class CoreAdminHandler extends RequestHandlerBase implements PermissionNa
       final String action = req.getParams().get(ACTION, STATUS.toString()).toLowerCase(Locale.ROOT);
       CoreAdminOp op = opMap.get(action);
       if (op == null) {
-        log.warn(
-            "action '{}' not found, calling custom action handler. "
-                + "If original intention was to target some custom behaviour "
-                + "use custom actions defined in 'solr.xml' instead",
-            action);
-        handleCustomAction(req, rsp);
-        return;
+        throw new SolrException(
+            SolrException.ErrorCode.BAD_REQUEST, "Unsupported operation: " + action);
       }
 
       final CallInfo callInfo = new CallInfo(this, req, rsp, op);
