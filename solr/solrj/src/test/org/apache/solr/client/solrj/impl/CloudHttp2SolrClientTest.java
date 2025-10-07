556312bbeeebce5ac950469ed5fa2cd93d95c43e
jdyer1 <jdyer@apache.org>
2025-10-07 11:21:34 -0500

    Add a doc comment expaining what this test is doing and why we are setting the sys prop

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudHttp2SolrClientTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudHttp2SolrClientTest.java
@@ -257,8 +257,13 @@ public class CloudHttp2SolrClientTest extends SolrCloudTestCase {
   @Test
   @LogLevel("org.apache.solr.servlet.HttpSolrCall=DEBUG")
   public void testHttpCspPerf() throws Exception {
+    // This ensures CH2SC is caching cluster status by counting the number of logged calls to the admin endpoint.
+    // too many calls to CLUSTERSTATUS might mean insufficient caching and peformance regressions!
     try {
+      // BaseHttpClusterStateProvider has a background job that pre-fetches data from CLUSTERSTATUS on timed intervals
+      // This can pollute this test, so we set the interval very high to prevent it from running.
       System.setProperty(SYS_PROP_CACHE_TIMEOUT_SECONDS, "" + Integer.MAX_VALUE);
+      
       String collectionName = "HTTPCSPTEST";
       CollectionAdminRequest.createCollection(collectionName, "conf", 2, 1)
           .process(cluster.getSolrClient());
