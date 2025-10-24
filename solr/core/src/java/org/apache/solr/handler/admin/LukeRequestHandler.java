d5bc27e1786b61c751983135186a3c1431d74292
Eric Pugh <epugh@opensourceconnections.com>
2025-10-24 10:51:25 -0400

    SOLR-14380: Fix "the the" pattern through our codebase. (#3798)
    ---------
    
    Co-authored-by: Christine Poerschke <cpoerschke@apache.org>

--- solr/core/src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
@@ -664,8 +664,8 @@ public class LukeRequestHandler extends RequestHandlerBase {
   }
 
   /**
-   * A helper method that attempts to determine the file length of the the segments file for the
-   * specified IndexCommit from it's Directory.
+   * A helper method that attempts to determine the file length of the segments file for the
+   * specified IndexCommit from its Directory.
    *
    * <p>If any sort of {@link IOException} occurs, this method will return "-1" and swallow the
    * exception since this may be normal if the IndexCommit is no longer "on disk". The specific type
