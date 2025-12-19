6f2e152b3d954b2cb476c091131c8060548e6aaa
Eric Pugh <epugh@opensourceconnections.com>
2025-12-18 21:10:02 -0500

    lint

--- solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
+++ solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
@@ -1372,7 +1372,7 @@ public abstract class SolrTestCaseJ4 extends SolrTestCase {
 
     SolrQueryResponse rsp = new SolrQueryResponse();
     SolrRequestHandler handler = core.getRequestHandler("/update/json");
-    
+
     try {
       SolrRequestInfo.setRequestInfo(new SolrRequestInfo(req, rsp));
       handler.handleRequest(req, rsp);
