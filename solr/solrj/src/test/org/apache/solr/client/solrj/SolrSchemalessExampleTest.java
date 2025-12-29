d24a74c31f06ab9b550c4e226ff28d6bbafdc65b
Eric Pugh <epugh@opensourceconnections.com>
2025-12-29 11:22:47 -0500

    try with resoruces is som much shorter

--- solr/solrj/src/test/org/apache/solr/client/solrj/SolrSchemalessExampleTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/SolrSchemalessExampleTest.java
@@ -61,20 +61,10 @@ public class SolrSchemalessExampleTest extends SolrExampleTestsBase {
     PathUtils.copyDirectory(ExternalPaths.DEFAULT_CONFIGSET, collection1Dir);
     Properties props = new Properties();
     props.setProperty("name", "collection1");
-    OutputStreamWriter writer = null;
-    try {
-      writer =
-          new OutputStreamWriter(
-              PathUtils.newOutputStream(collection1Dir.resolve("core.properties"), false),
-              StandardCharsets.UTF_8);
+    try (OutputStreamWriter writer = new OutputStreamWriter(
+        PathUtils.newOutputStream(collection1Dir.resolve("core.properties"), false),
+        StandardCharsets.UTF_8)) {
       props.store(writer, null);
-    } finally {
-      if (writer != null) {
-        try {
-          writer.close();
-        } catch (Exception ignore) {
-        }
-      }
     }
     solrJettyTestRule.startSolr(tempSolrHome);
   }
