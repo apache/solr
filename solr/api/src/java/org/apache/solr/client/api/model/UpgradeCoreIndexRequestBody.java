b68f5876494387b0af0f4af87d3de6b0d989e09e
Rahul Goswami <rahul.goswami0206@gmail.com>
2026-01-06 01:02:57 -0500

    Add license

--- solr/api/src/java/org/apache/solr/client/api/model/UpgradeCoreIndexRequestBody.java
+++ solr/api/src/java/org/apache/solr/client/api/model/UpgradeCoreIndexRequestBody.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.client.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,5 +25,9 @@ public class UpgradeCoreIndexRequestBody {
   @JsonProperty
   public String async;
 
-  @JsonProperty public String updateChain;
+  @Schema(
+      description =
+          "updateChain to be used for reindexing during index upgrade if you don't want to use the one used by /update by default")
+  @JsonProperty
+  public String updateChain;
 }
