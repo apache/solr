9da3ee9320edcdf1a642d0ad145d347a537c6534
Houston Putman <houston@apache.org>
2026-01-14 15:53:38 -0800

    Undo test utility change

--- solr/test-framework/src/java/org/apache/solr/cloud/SolrCloudTestCase.java
+++ solr/test-framework/src/java/org/apache/solr/cloud/SolrCloudTestCase.java
@@ -60,8 +60,9 @@ import org.apache.solr.common.cloud.SolrZkClient;
 import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.util.EnvUtils;
 import org.apache.solr.common.util.NamedList;
-import org.apache.solr.common.util.RetryUtil;
+import org.apache.solr.common.util.TimeSource;
 import org.apache.solr.embedded.JettySolrRunner;
+import org.apache.solr.util.TimeOut;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.slf4j.Logger;
@@ -420,28 +421,22 @@ public class SolrCloudTestCase extends SolrTestCaseJ4 {
   }
 
   protected CollectionAdminRequest.RequestStatusResponse waitForAsyncClusterRequest(
-      String asyncId, Duration timeout) throws InterruptedException {
+      String asyncId, Duration timeout)
+      throws InterruptedException, SolrServerException, IOException {
     final CollectionAdminRequest.RequestStatus requestStatus =
         CollectionAdminRequest.requestStatus(asyncId);
-    final AtomicReference<CollectionAdminRequest.RequestStatusResponse> rsp =
-        new AtomicReference<>();
-    RetryUtil.retryUntil(
-        "Async request " + asyncId + " did not complete within duration: " + timeout,
-        (int) (timeout.toMillis() / 50),
-        50,
-        TimeUnit.MILLISECONDS,
-        () -> {
-          try {
-            rsp.set(requestStatus.process(cluster.getSolrClient()));
-            if (rsp.get().getRequestStatus() == RequestStatusState.FAILED
-                || rsp.get().getRequestStatus() == RequestStatusState.COMPLETED) {
-              return true;
-            }
-          } catch (Exception ignored) {
-          }
-          return false;
-        });
-    return rsp.get();
+    CollectionAdminRequest.RequestStatusResponse rsp = null;
+    TimeOut timeoutCheck = new TimeOut(timeout, TimeSource.NANO_TIME);
+    while (!timeoutCheck.hasTimedOut()) {
+      rsp = requestStatus.process(cluster.getSolrClient());
+      if (rsp.getRequestStatus() == RequestStatusState.FAILED
+          || rsp.getRequestStatus() == RequestStatusState.COMPLETED) {
+        return rsp;
+      }
+      Thread.sleep(50);
+    }
+    fail("Async request " + asyncId + " did not complete within duration: " + timeout);
+    return rsp;
   }
 
   @SuppressWarnings({"rawtypes"})
