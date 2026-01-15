440e664d3ed0c4f4ff1f3b5c8b6e5ae699f8cf76
Houston Putman <houston@apache.org>
2026-01-15 12:48:48 -0800

    SOLR-18067: Fix errorprone compilation issues
    Relates to https://github.com/apache/solr/pull/4041

--- solr/solrj/src/test/org/apache/solr/client/solrj/GetByIdTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/GetByIdTest.java
@@ -45,6 +45,7 @@ public class GetByIdTest extends SolrTestCase {
   }
 
   @Before
+  @Override
   public void setUp() throws Exception {
     super.setUp();
     solrTestRule.getSolrClient().deleteByQuery("*:*");
