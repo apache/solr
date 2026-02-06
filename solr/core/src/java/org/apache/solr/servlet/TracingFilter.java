cb4b6514f973ace4b762a0f445ef961955a5f6bb
Gus Heck <gus@apache.org>
2026-02-05 19:21:15 -0500

    SOLR-18047 Move tracing initialization to TracingFilter

--- solr/core/src/java/org/apache/solr/servlet/TracingFilter.java
+++ solr/core/src/java/org/apache/solr/servlet/TracingFilter.java
@@ -17,49 +17,47 @@
 
 package org.apache.solr.servlet;
 
-import com.google.common.annotations.VisibleForTesting;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.apache.solr.common.SolrException;
+import org.apache.solr.logging.MDCLoggingContext;
+import org.apache.solr.util.tracing.TraceUtils;
 
-public class RateLimitFilter extends CoreContainerAwareHttpFilter {
-  private RateLimitManager rateLimitManager;
+public class TracingFilter extends HttpFilter {
 
   @Override
   public void init(FilterConfig config) throws ServletException {
     super.init(config);
-    RateLimitManager.Builder builder = new RateLimitManager.Builder();
-    builder.withZk(getCores().getZkController());
-    this.rateLimitManager = builder.build();
   }
 
   @Override
   protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
       throws IOException, ServletException {
-    RateLimitManager rateLimitManager = getRateLimitManager();
-    try (RequestRateLimiter.SlotReservation accepted = rateLimitManager.handleRequest(req)) {
-      if (accepted == null) {
-        res.sendError(
-            SolrException.ErrorCode.TOO_MANY_REQUESTS.code, RateLimitManager.ERROR_MESSAGE);
-        return;
+    Context context = TraceUtils.extractContext(req);
+    Span span = TraceUtils.startHttpRequestSpan(req, context);
+
+    final Thread currentThread = Thread.currentThread();
+    final String oldThreadName = currentThread.getName();
+    try (var scope = context.with(span).makeCurrent()) {
+      assert scope != null; // prevent javac warning about scope being unused
+      TraceUtils.setSpan(req, span);
+      TraceUtils.ifValidTraceId(
+          span, s -> MDCLoggingContext.setTracerId(s.getSpanContext().getTraceId()));
+      String traceId = MDCLoggingContext.getTraceId();
+      if (traceId != null) {
+        currentThread.setName(oldThreadName + "-" + traceId);
       }
       chain.doFilter(req, res);
-    } catch (InterruptedException e1) {
-      Thread.currentThread().interrupt();
-      throw new SolrException(SolrException.ErrorCode.SERVER_ERROR, e1.getMessage());
+    } finally {
+      currentThread.setName(oldThreadName);
+      TraceUtils.setHttpStatus(span, res.getStatus());
+      span.end();
     }
-  }
-
-  public RateLimitManager getRateLimitManager() {
-    return rateLimitManager;
-  }
-
-  @VisibleForTesting
-  void setRateLimitManager(RateLimitManager rateLimitManager) {
-    this.rateLimitManager = rateLimitManager;
   }
 }
