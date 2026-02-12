4328d9c485c92abab620605179c860a421ae20db
Anshum Gupta <anshum@apache.org>
2026-02-12 00:35:41 -0800

    SOLR-18086: Fix LBSolrClient behavior in case of zombie nodes (#4130)

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBSolrClient.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/LBSolrClient.java
@@ -381,6 +381,7 @@ public abstract class LBSolrClient extends SolrClient {
               skipped.add(wrapper.getEndpoint());
             }
           }
+          endpoint = null;
           continue;
         }
 
