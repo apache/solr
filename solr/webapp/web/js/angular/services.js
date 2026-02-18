9a68e2240d6b0de3eeac8d60cf5a849fbc3fa5ee
Eric Pugh <epugh@opensourceconnections.com>
2026-02-18 05:10:31 -0500

    SOLR-18120: remove unused endpoints in services.js in Solr Admin (#4138)

--- solr/webapp/web/js/angular/services.js
+++ solr/webapp/web/js/angular/services.js
@@ -99,17 +99,8 @@ solrAdminServices.factory('System',
     return $resource('admin/zookeeper', {wt:'json', _:Date.now()}, {
       "simple": {},
       "liveNodes": {params: {path: '/live_nodes'}},
-      "clusterState": {params: {detail: "true"}},
-      "detail": {params: {detail: "true", path: "@path"}},
-      "configs": {params: {detail:false, path: "/configs/"}},
-      "aliases": {params: {detail: "true", path: "/aliases.json"}, transformResponse:function(data) {
-        var znode = $.parseJSON(data).znode;
-        if (znode.data) {
-          return {aliases: $.parseJSON(znode.data).collection};
-        } else {
-          return {aliases: {}};
-        }
-      }}
+      "clusterState": {params: {detail: "true", path: "/clusterstate.json"}},
+      "detail": {params: {detail: "true", path: "@path"}}
     });
   }])
 .factory('ZookeeperStatus',
