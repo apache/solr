97032bdee99b5619aef7af2df31b6760167e4b7f
Eric Pugh <epugh@opensourceconnections.com>
2026-02-28 11:08:33 -0500

    refactor the test

--- solr/core/src/test/org/apache/solr/handler/admin/api/SplitCoreAPITest.java
+++ solr/core/src/test/org/apache/solr/handler/admin/api/SplitCoreAPITest.java
@@ -18,20 +18,16 @@ package org.apache.solr.handler.admin.api;
 
 import static org.apache.solr.core.CoreContainer.ALLOW_PATHS_SYSPROP;
 
-import java.nio.file.Path;
-import java.util.HashMap;
-import org.apache.lucene.tests.util.LuceneTestCase;
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.client.api.model.SplitCoreRequestBody;
 import org.apache.solr.common.SolrException;
-import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.EnvUtils;
-import org.apache.solr.core.CoreContainer;
-import org.apache.solr.core.SolrCore;
-import org.apache.solr.handler.admin.CoreAdminHandler;
 import org.apache.solr.request.SolrQueryRequestBase;
 import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.util.ExternalPaths;
 import org.apache.solr.util.SolrJettyTestRule;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -41,36 +37,37 @@ public class SplitCoreAPITest extends SolrTestCaseJ4 {
 
   @ClassRule public static SolrJettyTestRule solrTestRule = new SolrJettyTestRule();
 
-  private static final String CORE_NAME = DEFAULT_TEST_CORENAME;
+  private SplitCoreAPI api;
 
   @BeforeClass
   public static void beforeClass() throws Exception {
-    Path configSet = createTempDir("configSet");
-    copyMinConf(configSet);
-    EnvUtils.setProperty(ALLOW_PATHS_SYSPROP, configSet.toAbsolutePath().toString());
-    solrTestRule.startSolr(LuceneTestCase.createTempDir());
-    solrTestRule.newCollection(CORE_NAME).withConfigSet(configSet).create();
+    EnvUtils.setProperty(
+        ALLOW_PATHS_SYSPROP, ExternalPaths.SERVER_HOME.toAbsolutePath().toString());
+    solrTestRule.startSolr(createTempDir());
+    solrTestRule
+        .newCollection(DEFAULT_TEST_CORENAME)
+        .withConfigSet(ExternalPaths.DEFAULT_CONFIGSET)
+        .create();
   }
 
-  private SplitCoreAPI createApi() {
-    CoreContainer coreContainer = solrTestRule.getCoreContainer();
-    CoreAdminHandler.CoreAdminAsyncTracker asyncTracker =
-        coreContainer.getMultiCoreHandler().getCoreAdminAsyncTracker();
-    SolrCore solrCore = coreContainer.getCore(CORE_NAME);
-    SolrQueryRequestBase req =
-        new SolrQueryRequestBase(solrCore, new MapSolrParams(new HashMap<>())) {};
-    solrCore.close(); // decrement ref count; CoreContainer still holds the core
-    SolrQueryResponse rsp = new SolrQueryResponse();
-    return new SplitCoreAPI(coreContainer, asyncTracker, req, rsp);
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    api =
+        new SplitCoreAPI(
+            solrTestRule.getCoreContainer(),
+            solrTestRule.getCoreContainer().getMultiCoreHandler().getCoreAdminAsyncTracker(),
+            new SolrQueryRequestBase(null, new ModifiableSolrParams()),
+            new SolrQueryResponse());
   }
 
   @Test
   public void testSplitWithNoTargetCoreOrPathReturnsError() throws Exception {
-    SplitCoreAPI api = createApi();
     SplitCoreRequestBody requestBody = new SplitCoreRequestBody();
 
     SolrException ex =
-        assertThrows(SolrException.class, () -> api.splitCore(CORE_NAME, requestBody));
+        assertThrows(SolrException.class, () -> api.splitCore(DEFAULT_TEST_CORENAME, requestBody));
     assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex.code());
     assertTrue(
         "Expected error about missing targetCore or path",
@@ -79,9 +76,9 @@ public class SplitCoreAPITest extends SolrTestCaseJ4 {
 
   @Test
   public void testSplitWithNullRequestBodyReturnsError() throws Exception {
-    SplitCoreAPI api = createApi();
 
-    SolrException ex = assertThrows(SolrException.class, () -> api.splitCore(CORE_NAME, null));
+    SolrException ex =
+        assertThrows(SolrException.class, () -> api.splitCore(DEFAULT_TEST_CORENAME, null));
     assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex.code());
     assertTrue(
         "Expected error about missing targetCore or path",
