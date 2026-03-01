b59232242d8be37e0a932ad21ad32ac3011082e3
Eric Pugh <epugh@opensourceconnections.com>
2026-03-01 15:55:40 -0500

    lint

--- solr/core/src/java/org/apache/solr/handler/admin/HealthCheckHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/HealthCheckHandler.java
@@ -27,7 +27,6 @@ import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
