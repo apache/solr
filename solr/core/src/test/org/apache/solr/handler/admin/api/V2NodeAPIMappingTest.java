c74142675ebdabf0ebab8bf8b162223f63262af3
igiguere <igiguere@users.noreply.github.com>
2026-03-16 13:51:28 -0400

    SOLR-16458: Convert /api/node/system to JAX-RS (#4078)
    This migration to JAX-RS implicitly adds these APIs to the OAS, and ensures
    autogeneration of SolrRequest/SolrResponse types.
    
    Co-authored-by: Isabelle Giguere <igiguere71@yahoo.ca>
    Co-authored-by: Jason Gerlowski <gerlowskija@apache.org>

--- solr/core/src/test/org/apache/solr/handler/admin/api/V2NodeAPIMappingTest.java
+++ solr/core/src/test/org/apache/solr/handler/admin/api/V2NodeAPIMappingTest.java
@@ -39,7 +39,6 @@ import org.apache.solr.handler.admin.HealthCheckHandler;
 import org.apache.solr.handler.admin.InfoHandler;
 import org.apache.solr.handler.admin.LoggingHandler;
 import org.apache.solr.handler.admin.PropertiesRequestHandler;
-import org.apache.solr.handler.admin.SystemInfoHandler;
 import org.apache.solr.handler.admin.ThreadDumpHandler;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.request.SolrQueryRequestBase;
@@ -55,7 +54,6 @@ public class V2NodeAPIMappingTest extends SolrTestCaseJ4 {
   private ArgumentCaptor<SolrQueryRequest> queryRequestCaptor;
   private CoreAdminHandler mockCoresHandler;
   private InfoHandler infoHandler;
-  private SystemInfoHandler mockSystemInfoHandler;
   private LoggingHandler mockLoggingHandler;
   private PropertiesRequestHandler mockPropertiesHandler;
   private HealthCheckHandler mockHealthCheckHandler;
@@ -70,14 +68,12 @@ public class V2NodeAPIMappingTest extends SolrTestCaseJ4 {
   public void setupApiBag() {
     mockCoresHandler = mock(CoreAdminHandler.class);
     infoHandler = mock(InfoHandler.class);
-    mockSystemInfoHandler = mock(SystemInfoHandler.class);
     mockLoggingHandler = mock(LoggingHandler.class);
     mockPropertiesHandler = mock(PropertiesRequestHandler.class);
     mockHealthCheckHandler = mock(HealthCheckHandler.class);
     mockThreadDumpHandler = mock(ThreadDumpHandler.class);
     queryRequestCaptor = ArgumentCaptor.forClass(SolrQueryRequest.class);
 
-    when(infoHandler.getSystemInfoHandler()).thenReturn(mockSystemInfoHandler);
     when(infoHandler.getLoggingHandler()).thenReturn(mockLoggingHandler);
     when(infoHandler.getPropertiesHandler()).thenReturn(mockPropertiesHandler);
     when(infoHandler.getHealthCheckHandler()).thenReturn(mockHealthCheckHandler);
@@ -147,16 +143,6 @@ public class V2NodeAPIMappingTest extends SolrTestCaseJ4 {
   }
 
   @Test
-  public void testSystemInfoApiAllProperties() throws Exception {
-    final ModifiableSolrParams solrParams = new ModifiableSolrParams();
-    solrParams.add("anyParamName", "anyParamValue");
-    final SolrParams v1Params = captureConvertedSystemV1Params("/node/system", "GET", solrParams);
-
-    // All parameters are passed through to v1 API as-is.
-    assertEquals("anyParamValue", v1Params.get("anyParamName"));
-  }
-
-  @Test
   public void testHealthCheckApiAllProperties() throws Exception {
     final ModifiableSolrParams solrParams = new ModifiableSolrParams();
     solrParams.add("requireHealthyCores", "true");
@@ -173,11 +159,6 @@ public class V2NodeAPIMappingTest extends SolrTestCaseJ4 {
       throws Exception {
     return doCaptureParams(
         path, method, new ModifiableSolrParams(), v2RequestBody, mockCoresHandler);
-  }
-
-  private SolrParams captureConvertedSystemV1Params(
-      String path, String method, SolrParams inputParams) throws Exception {
-    return doCaptureParams(path, method, inputParams, null, mockSystemInfoHandler);
   }
 
   private SolrParams captureConvertedPropertiesV1Params(
@@ -232,7 +213,6 @@ public class V2NodeAPIMappingTest extends SolrTestCaseJ4 {
     apiBag.registerObject(new RejoinLeaderElectionAPI(coreHandler));
     apiBag.registerObject(new NodePropertiesAPI(infoHandler.getPropertiesHandler()));
     apiBag.registerObject(new NodeThreadsAPI(infoHandler.getThreadDumpHandler()));
-    apiBag.registerObject(new NodeSystemInfoAPI(infoHandler.getSystemInfoHandler()));
     apiBag.registerObject(new NodeHealthAPI(infoHandler.getHealthCheckHandler()));
   }
 }
