d2ed2c4354576f5df06eab83ad8f51d69a4ee20b
Rahul Goswami <rahulgoswami@apache.org>
2026-03-23 15:37:28 -0400

    SOLR-18143: Upgrade the Lucene dependency from 10.3.2 to 10.4.0 (#4195)
    Codec changes:                                                                                                                                                                                                                                             
      - SchemaCodecFactory, CuVSCodec, CuVSCodecFactory: Lucene103Codec -> Lucene104Codec                                                                                                                                                                        
      - ScalarQuantizedDenseVectorField: migrated to Lucene104HnswScalarQuantizedVectorsFormat;                                                                                                                                                                   
        confidenceInterval, compress, and dynamicConfidenceInterval                                                                                                                                                                     
        params are now deprecated; they're parsed but ignored with DeprecationLog warnings                                                                                                                                                                              
      - BinaryQuantizedDenseVectorField: replaced Lucene102HnswBinaryQuantizedVectorsFormat                                                                                                                                                                      
        with Lucene104HnswScalarQuantizedVectorsFormat using ScalarEncoding.SINGLE_BIT_QUERY_NIBBLE                                                                                                                                                              
                                                                                                                                                                                                                                                                 
      Documentation:                                                                                                                                                                                                                                             
      - Updated dense-vector-search.adoc to reflect deprecated scalar quantization params                                                                                                                                                                        
      - Added Solr 10.1 section in major-changes-in-solr-10.adoc with codec change                                                                                                                                                                               
        downgradeability warning                                                                                                                                                                                                                                 
      - Added step in dev-docs/lucene-upgrade.md to document codec changes in upgrade notes                                                                                                                                                                      
                                                                                                                                                                                                                                                                 
      Other:                                                                                                                                                                                                                                                     
        - Removed test configs for no-longer-validated confidenceInterval/compress constraints

--- solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
+++ solr/core/src/java/org/apache/solr/core/SchemaCodecFactory.java
@@ -26,8 +26,8 @@ import org.apache.lucene.codecs.KnnVectorsFormat;
 import org.apache.lucene.codecs.KnnVectorsReader;
 import org.apache.lucene.codecs.KnnVectorsWriter;
 import org.apache.lucene.codecs.PostingsFormat;
-import org.apache.lucene.codecs.lucene103.Lucene103Codec;
-import org.apache.lucene.codecs.lucene103.Lucene103Codec.Mode;
+import org.apache.lucene.codecs.lucene104.Lucene104Codec;
+import org.apache.lucene.codecs.lucene104.Lucene104Codec.Mode;
 import org.apache.lucene.codecs.lucene99.Lucene99HnswVectorsFormat;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
@@ -97,7 +97,7 @@ public class SchemaCodecFactory extends CodecFactory implements SolrCoreAware {
       log.debug("Using default compressionMode: {}", compressionMode);
     }
     codec =
-        new Lucene103Codec(compressionMode) {
+        new Lucene104Codec(compressionMode) {
           @Override
           public PostingsFormat getPostingsFormatForField(String field) {
             final SchemaField schemaField = core.getLatestSchema().getFieldOrNull(field);
