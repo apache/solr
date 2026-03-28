b6ea1072e95330489399216429c23b84f8995d06
David Smiley <dsmiley@apache.org>
2026-03-27 22:33:43 -0400

    tidy

--- solr/core/src/test/org/apache/solr/handler/admin/InfoHandlerTest.java
+++ solr/core/src/test/org/apache/solr/handler/admin/InfoHandlerTest.java
@@ -16,8 +16,8 @@
  */
 package org.apache.solr.handler.admin;
 
-import java.io.IOException;
 import io.opentelemetry.api.common.Attributes;
+import java.io.IOException;
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.core.CoreContainer;
