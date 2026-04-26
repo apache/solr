06ef39d1f0c70f9fb7f675130220eef4cd7cd558
Jan Høydahl <janhoy@apache.org>
2026-04-26 02:48:52 +0200

    Tidy

--- solr/modules/jwt-auth/src/test/org/apache/solr/security/jwt/JWTIssuerConfigTest.java
+++ solr/modules/jwt-auth/src/test/org/apache/solr/security/jwt/JWTIssuerConfigTest.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import org.apache.solr.SolrTestCase;
 import org.apache.solr.common.SolrException;
-import org.jose4j.jwk.JsonWebKey;
 import org.jose4j.jwk.JsonWebKeySet;
 import org.junit.After;
 import org.junit.Before;
