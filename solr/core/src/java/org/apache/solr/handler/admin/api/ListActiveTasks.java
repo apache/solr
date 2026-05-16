7b157910c42aa8ec30db4b69df85772dfefeea6c
jaykay12 <jalazkumar1208@gmail.com>
2026-05-16 16:27:35 +0530

    working api

--- solr/core/src/java/org/apache/solr/handler/admin/api/ListActiveTasks.java
+++ solr/core/src/java/org/apache/solr/handler/admin/api/ListActiveTasks.java
@@ -4,35 +4,26 @@ import jakarta.inject.Inject;
 import org.apache.solr.api.JerseyResource;
 import org.apache.solr.client.api.endpoint.ListActiveTasksApi;
 import org.apache.solr.client.api.model.ListActiveTaskResponse;
-import org.apache.solr.common.SolrException;
-import org.apache.solr.core.CoreContainer;
 import org.apache.solr.jersey.PermissionName;
 
 import java.util.Map;
 
-import static org.apache.solr.common.SolrException.ErrorCode.SERVER_ERROR;
 import static org.apache.solr.security.PermissionNameProvider.Name.READ_PERM;
 
 public class ListActiveTasks extends JerseyResource implements ListActiveTasksApi {
 
-  private final CoreContainer coreContainer;
-
   @Inject
-  public ListActiveTasks(CoreContainer coreContainer) {
-    this.coreContainer = coreContainer;
+  public ListActiveTasks() {
+
   }
 
   @Override
   @PermissionName(READ_PERM)
   public ListActiveTaskResponse listActiveTasks(String taskUUID) throws Exception {
-    if (coreContainer == null || coreContainer.isShutDown()) {
-      throw new SolrException(
-          SERVER_ERROR, "CoreContainer is either not initialized or shutting down");
-    }
 
     final ListActiveTaskResponse response = instantiateJerseyResponse(ListActiveTaskResponse.class);
 
-    response.taskList = Map.of("xyz","jalaz");
+    response.taskList = Map.of("xyz","jalaz", "mno", "pqr");
 
     return response;
 
