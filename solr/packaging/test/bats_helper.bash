ab8300ac0c2825316ba584e6270ec2f30e17b546
Eric Pugh <epugh@opensourceconnections.com>
2026-07-01 18:19:03 -0400

    wait_for method was accidentally removed during a preview revert of a commit

--- solr/packaging/test/bats_helper.bash
+++ solr/packaging/test/bats_helper.bash
@@ -97,6 +97,24 @@ collection_exists() {
   return 1
 }
 
+wait_for() {
+  local timeout="${1:-30}"     # Default 30 seconds timeout
+  local interval="${2:-1}"     # Default 1 second between retries
+  shift 2                      # Remove timeout and interval from args
+  local command=("$@")         # Remaining args are the command to execute
+
+  local end_time=$(($(date +%s) + timeout))
+
+  while [ $(date +%s) -lt $end_time ]; do
+    if "${command[@]}"; then
+      return 0
+    fi
+    sleep "$interval"
+  done
+
+  return 1  # Timeout reached
+}
+
 # Wait for a collection to be queryable
 wait_for_collection() {
   local collection="$1"
