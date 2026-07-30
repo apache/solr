3d97ad7ae22b9f3df86cf94197d88f85adcb0b5a
Ian Springer <ian.springer@slack-corp.com>
2026-07-30 13:49:55 -0400

    remove redundant isBlank call to optimize

--- solr/core/src/java/org/apache/solr/search/DisMaxQParser.java
+++ solr/core/src/java/org/apache/solr/search/DisMaxQParser.java
@@ -227,7 +227,7 @@ public class DisMaxQParser extends QParser {
      * matched those phrases but do match looser phrases.
      */
     String userPhraseQuery = userQuery.replace("\"", "");
-    if (userPhraseQuery.isBlank() || userPhraseQuery.trim().indexOf(' ') < 0) {
+    if (userPhraseQuery.trim().indexOf(' ') < 0) {
       return null;
     }
     return pp.parse("\"" + userPhraseQuery + "\"");
