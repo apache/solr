413a16b76aa74ade69a1bde4b342818fac8f5669
Vishnu Priya Chandra Sekar <vchandsekar@gmail.com>
2026-07-30 11:53:00 -0700

    Updated changelog and comments in HttpJettySolrClient

--- solr/solrj-jetty/src/java/org/apache/solr/client/solrj/jetty/HttpJettySolrClient.java
+++ solr/solrj-jetty/src/java/org/apache/solr/client/solrj/jetty/HttpJettySolrClient.java
@@ -301,6 +301,7 @@ public class HttpJettySolrClient extends HttpSolrClient {
         asyncTracker.getMaxRequestsQueuedPerDestination());
     httpClient.setUserAgentField(new HttpField(HttpHeader.USER_AGENT, USER_AGENT));
     httpClient.setConnectTimeout(builder.getConnectionTimeoutMillis());
+    // Idle timeout here applies only while waiting for the initial response headers.
     httpClient.setIdleTimeout(SolrHttpConstants.DEFAULT_SO_TIMEOUT);
     // note: idle & request timeouts are set per request
 
