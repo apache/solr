54758e81b4012796c03bca0b14d403e1d10204ca
David Smiley <dsmiley@apache.org>
2026-08-01 12:45:50 -0400

    build: centralize maven repo declarations (#4677)

--- solr/ui/build.gradle.kts
+++ solr/ui/build.gradle.kts
@@ -27,8 +27,9 @@ repositories {
             includeGroupAndSubgroups("com.google")
         }
     }
-    mavenCentral()
 }
+// mavenCentral / enterprise mirror, shared with the rest of the build.
+apply(from = rootProject.file("build-tools/build-infra/declare-repositories.gradle"))
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
