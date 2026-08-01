apache/solr

commit 6c793d5534160d882882f2833496ae25c1f9016b

SolrRequest.getPath setPath documentation (#4665)
And path enforcement

solr/solrj/src/java/org/apache/solr/client/solrj/SolrRequest.java
@@ -172,11 +172,20 @@ public abstract class SolrRequest<T> implements Serializable {
     this.method = method;
   }
 
+  /**
+   * The URI path to a "request handler", such as "/select". Must start with a "/". For
+   * collection/core requests, this is appended to the core/collection path; otherwise it's a node
+   * level request and thus is relative to the solr root.
+   */
   public String getPath() {
     return path;
   }
 
   public void setPath(String path) {
+    if (path == null || !path.startsWith("/")) {
+      throw new IllegalArgumentException("Must start with a '/': " + path);
+    }
+
     this.path = path;
   }
 

commit 14405d7ec176cc5db264eb4ffb70355b3916369e

SolrRequest.setPath fix; allow null (#4686)
Fixes a temporary regression from #4665

solr/solrj/src/java/org/apache/solr/client/solrj/SolrRequest.java
@@ -157,7 +157,7 @@ public abstract class SolrRequest<T> implements Serializable {
 
   public SolrRequest(METHOD m, String path, SolrRequestType requestType) {
     this.method = m;
-    this.path = path;
+    this.path = validatePath(path);
     this.requestType = requestType;
   }
 
@@ -182,11 +182,14 @@ public abstract class SolrRequest<T> implements Serializable {
   }
 
   public void setPath(String path) {
-    if (path == null || !path.startsWith("/")) {
+    this.path = validatePath(path);
+  }
+
+  private static String validatePath(String path) {
+    if (path != null && !path.startsWith("/")) {
       throw new IllegalArgumentException("Must start with a '/': " + path);
     }
-
-    this.path = path;
+    return path;
   }
 
   /**