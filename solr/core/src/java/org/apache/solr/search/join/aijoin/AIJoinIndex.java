824ed23e75f866606e38339310ab6ab3908b8d0b
Mikhail Khludnev <mkhl@apache.org>
2026-08-19 18:11:21 +0300

    import

--- solr/core/src/java/org/apache/solr/search/join/aijoin/AIJoinIndex.java
+++ solr/core/src/java/org/apache/solr/search/join/aijoin/AIJoinIndex.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
