ff141be5beca03762536f5371ed0a45fcd66fe90
Serhiy Bzhezytskyy <me@serhiy-bzhezytskyy.com>
2026-08-23 10:37:33 +0300

    SOLR-18390: also reuse getSolrClient() where it's reachable via CloudJettyRunner.jetty
    shardToJetty.get(SHARD2).get(0) is a CloudJettyRunner, which carries a
    public JettySolrRunner jetty field right next to the Replica-shaped info
    map the code was pulling a raw base_url string from. Same fix as the
    other 59 sites, just reached through a different field on the same
    object instead of a direct JettySolrRunner variable.

--- solr/core/src/test/org/apache/solr/cloud/BasicDistributedZk2Test.java
+++ solr/core/src/test/org/apache/solr/cloud/BasicDistributedZk2Test.java
@@ -445,29 +445,25 @@ public class BasicDistributedZk2Test extends AbstractFullDistribZkTestBase {
     checkShardConsistency(true, false);
 
     // try a backup command
-    try (final SolrClient client =
-        new HttpJettySolrClient.Builder(
-                (String) shardToJetty.get(SHARD2).get(0).info.get("base_url"))
-            .build()) {
-      final String backupName = "the_backup";
-      ModifiableSolrParams params = new ModifiableSolrParams();
-      params.set("command", "backup");
-      params.set("name", backupName);
-      final Path location = FilterPath.unwrap(createTempDir()).toRealPath();
-      // Allow non-standard location outside SOLR_HOME
-      jettys.forEach(j -> j.getCoreContainer().getAllowPaths().add(location));
-      params.set("location", location.toString());
+    final SolrClient client = shardToJetty.get(SHARD2).get(0).jetty.getSolrClient();
+    final String backupName = "the_backup";
+    ModifiableSolrParams params = new ModifiableSolrParams();
+    params.set("command", "backup");
+    params.set("name", backupName);
+    final Path location = FilterPath.unwrap(createTempDir()).toRealPath();
+    // Allow non-standard location outside SOLR_HOME
+    jettys.forEach(j -> j.getCoreContainer().getAllowPaths().add(location));
+    params.set("location", location.toString());
 
-      QueryRequest request = new QueryRequest(ReplicationHandler.PATH, params);
-      client.request(request, DEFAULT_TEST_COLLECTION_NAME);
+    QueryRequest request = new QueryRequest(ReplicationHandler.PATH, params);
+    client.request(request, DEFAULT_TEST_COLLECTION_NAME);
 
-      final BackupStatusChecker backupStatus =
-          new BackupStatusChecker(client, "/" + DEFAULT_TEST_COLLECTION_NAME + "/replication");
-      final String backupDirName = backupStatus.waitForBackupSuccess(backupName, 30);
-      assertTrue(
-          "Backup dir does not exist: " + backupDirName,
-          Files.exists(location.resolve(backupDirName)));
-    }
+    final BackupStatusChecker backupStatus =
+        new BackupStatusChecker(client, "/" + DEFAULT_TEST_COLLECTION_NAME + "/replication");
+    final String backupDirName = backupStatus.waitForBackupSuccess(backupName, 30);
+    assertTrue(
+        "Backup dir does not exist: " + backupDirName,
+        Files.exists(location.resolve(backupDirName)));
   }
 
   private void addNewReplica() throws Exception {
