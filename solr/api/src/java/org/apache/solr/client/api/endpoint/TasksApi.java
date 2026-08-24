a0843da2c81be1a4ffa32456435bf3f76d621459
jaykay12 <jalazkumar1208@gmail.com>
2026-08-25 03:25:12 +0530

    contract

--- solr/api/src/java/org/apache/solr/client/api/endpoint/TasksApi.java
+++ solr/api/src/java/org/apache/solr/client/api/endpoint/TasksApi.java
@@ -20,6 +20,7 @@ package org.apache.solr.client.api.endpoint;
 import static org.apache.solr.client.api.util.Constants.INDEX_PATH_PREFIX;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -47,5 +48,15 @@ public interface TasksApi {
         summary = "Status of a specific task",
         tags = {"tasks"})
     TaskStatusResponse getTaskStatus(@PathParam("taskID") String taskID) throws Exception;
+  }
+
+  @Path(INDEX_PATH_PREFIX + "/tasks/{taskID}")
+  interface Cancel {
+    @DELETE
+    @StoreApiParameters
+    @Operation(
+        summary = "Cancel any specific task",
+        tags = {"tasks"})
+    TaskStatusResponse cancelRunningTask(@PathParam("taskID") String taskID) throws Exception;
   }
 }
