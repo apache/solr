cde6efaa709d4c5591206a03a7eaa94aad886052
chan-dx <chan.dx.dev@gmail.com>
2026-08-28 17:18:51 +0100

    SOLR-18402: CloudSolrClient asks its transport if it was a comm error

--- solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudSolrClientCacheTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudSolrClientCacheTest.java
@@ -431,7 +431,7 @@ public class CloudSolrClientCacheTest extends SolrTestCaseJ4 {
 
     @Override
     public HttpSolrClient getHttpClient() {
-      throw new UnsupportedOperationException();
+      return mock(HttpSolrClient.class);
     }
 
     @FunctionalInterface
