866b1cd86302a58cdb9ee48ef9e0f86c13a63353
chan-dx <chan.dx.dev@gmail.com>
2026-08-28 17:39:34 +0100

    SOLR-18402: HttpJettySolrClient classifies its own transport failures

--- solr/solrj-jetty/src/java/org/apache/solr/client/solrj/jetty/HttpJettySolrClient.java
+++ solr/solrj-jetty/src/java/org/apache/solr/client/solrj/jetty/HttpJettySolrClient.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationTargetException;
 import java.net.ConnectException;
+import java.nio.channels.ClosedChannelException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -49,6 +50,7 @@ import org.apache.solr.client.solrj.jetty.HttpListenerFactory.RequestResponseLis
 import org.apache.solr.client.solrj.request.RequestWriter;
 import org.apache.solr.client.solrj.response.ResponseParser;
 import org.apache.solr.client.solrj.util.ClientUtils;
+import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.EnvUtils;
@@ -82,6 +84,7 @@ import org.eclipse.jetty.http.MultiPart;
 import org.eclipse.jetty.http2.client.HTTP2Client;
 import org.eclipse.jetty.http2.client.transport.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.io.ClientConnector;
+import org.eclipse.jetty.io.EofException;
 import org.eclipse.jetty.util.ssl.KeyStoreScanner;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
@@ -558,6 +561,13 @@ public class HttpJettySolrClient extends HttpSolrClient {
     try (final var derivedClient = new NoCloseHttpJettySolrClient(baseUrl, this)) {
       return clientFunction.apply(derivedClient);
     }
+  }
+
+  @Override
+  public boolean wasCommError(Throwable t) {
+    return super.wasCommError(t)
+        || SolrException.hasCause(t, EofException.class)
+        || SolrException.hasCause(t, ClosedChannelException.class);
   }
 
   @Override
