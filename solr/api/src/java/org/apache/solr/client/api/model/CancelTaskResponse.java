e4e2ac133a8d78344c331171a906e64703747957
jaykay12 <jalazkumar1208@gmail.com>
2026-08-30 00:14:32 +0530

    minimal file

--- solr/api/src/java/org/apache/solr/client/api/model/CancelTaskResponse.java
+++ solr/api/src/java/org/apache/solr/client/api/model/CancelTaskResponse.java
@@ -5,18 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class CancelTaskResponse extends SolrJerseyResponse {
 
   public enum CancellationStatus {
-    SUCCESS("success"),
-    NOT_FOUND("not_found");
-
-    private final String value;
-
-    CancellationStatus(String value) {
-      this.value = value;
-    }
-
-    public String getValue() {
-      return this.value;
-    }
+    SUCCESS,
+    NOT_FOUND
   }
 
   @JsonProperty
