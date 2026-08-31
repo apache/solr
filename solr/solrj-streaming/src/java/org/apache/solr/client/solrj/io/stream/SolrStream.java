5b9a2669744573e3526bd2463119631d169cb64b
Jason Gerlowski <gerlowskija@apache.org>
2026-08-31 09:09:28 -0400

    Address review comments, rd 1

--- solr/solrj-streaming/src/java/org/apache/solr/client/solrj/io/stream/SolrStream.java
+++ solr/solrj-streaming/src/java/org/apache/solr/client/solrj/io/stream/SolrStream.java
@@ -73,9 +73,9 @@ public class SolrStream extends TupleStream {
    * @param collectionOrCoreUrl URL of the Solr core or collection to query, typically of the form
    *     "http://host:8983/solr/myCore".
    * @param params query-parameters sent with the streaming request
-   * @deprecated since 10.1. Use base URL constructor instead.
+   * @deprecatedUse base URL constructor instead.
    */
-  @Deprecated
+  @Deprecated(since = "10.1")
   public SolrStream(String collectionOrCoreUrl, SolrParams params) {
     this.baseUrl = collectionOrCoreUrl;
     this.params = params;
@@ -89,9 +89,9 @@ public class SolrStream extends TupleStream {
    * @param path the request handler path to query (e.g. "/export"). If not provided, defaults to
    *     "/select".
    * @param params query-parameters sent with the streaming request
-   * @deprecated since 10.1. Use base URL constructor instead.
+   * @deprecatedUse base URL constructor instead.
    */
-  @Deprecated
+  @Deprecated(since = "10.1")
   public SolrStream(String collectionOrCoreUrl, String path, SolrParams params) {
     this(collectionOrCoreUrl, null, path, params);
   }
