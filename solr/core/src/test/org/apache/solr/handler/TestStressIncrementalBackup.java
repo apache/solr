da80d79ae253293f7d6b96df1c319fe43404b94e
Serhiy Bzhezytskyy <me@serhiy-bzhezytskyy.com>
2026-09-09 13:45:50 +0300

    SOLR-18358: remove the non-incremental (full-snapshot) backup path (#4808)
    Co-authored-by: Eric Pugh <epugh@opensourceconnections.com>

--- solr/core/src/test/org/apache/solr/handler/TestStressIncrementalBackup.java
+++ solr/core/src/test/org/apache/solr/handler/TestStressIncrementalBackup.java
@@ -164,7 +164,6 @@ public class TestStressIncrementalBackup extends SolrCloudTestCase {
     CollectionAdminRequest.Backup backup =
         CollectionAdminRequest.backupCollection(DEFAULT_TEST_COLLECTION_NAME, "stressBackup")
             .setLocation(backupPath.toString())
-            .setIncremental(true)
             .setMaxNumberBackupPoints(5);
     if (random().nextBoolean()) {
       try {
