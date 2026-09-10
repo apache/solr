57f810c6154fd24a986a3b03a6c7275edff3afdb
Andrzej Bialecki <andrzej.bialecki@lucidworks.com>
2026-09-10 17:54:40 +0200

    Tidy.

--- solr/modules/cross-dc/src/test/org/apache/solr/crossdc/common/MirroredSolrRequestSerializerTest.java
+++ solr/modules/cross-dc/src/test/org/apache/solr/crossdc/common/MirroredSolrRequestSerializerTest.java
@@ -217,7 +217,7 @@ public class MirroredSolrRequestSerializerTest extends SolrTestCase {
             + deserialized.getSubmitTimeNanos(),
         deserialized.getSubmitTimeNanos() >= before && deserialized.getSubmitTimeNanos() <= after);
   }
-  
+
   /**
    * Confirms the waste pattern from {@link MirroredSolrRequestSerializer#serialize(String,
    * MirroredSolrRequest)} testing both small and large documents around the 8192 bytes boundary.
