7f86809a65653b8aa81fd1d24e0de0443d4b26de
Christos Malliaridis <malliaridis@apache.org>
2026-09-21 00:40:38 +0300

    Make UI module independent sub-project (#4650)
    The approach that was picked is a completely independent gradle project. Some important changes:
    
    - Dependency resolution for the UI module is happening separately from the rest of the project, so dependencies are not aligned
    - Many build configurations that apply to most `solr` modules are removed, allowing the UI module to use simpler builds
    - The exclusion of many plugins and configurations means there are no default checks included
    
    We should evaluate before merging which of the configurations or routines we want to keep and apply to this module as well. We may also keep it as is since the Admin UI is still experimental.
    
    The module is no longer included as a configuration or task dependency as implemented before. Instead, the webapp now has to fetch the UI artifacts from https://nightlies.apache.org/solr/ui/wasm/[commit-hash]/.
    
    Developers can call `./gradlew dev -Psolr.ui.buildFromSource=true` to build the UI module locally and include the artifacts from the current state, or alternatively define a commit (and optionally a checksum for validation) in the `gradle/solr-ui.properties` to download the UI bundle from the nightlies server.
    
    A guard has been added for specific release tasks that prevents using a nightly build artifact, and forces the UI to be built from source.
    
    The jenkins job https://ci-builds.apache.org/job/Solr/job/solr-ui-wasm-assemble/ is building and deploying a version of the project. Right now it is executed manually on this branch, but it can be updated once this is merged to run from main.
    
    _I chose running the jenkins job manually so that we can pin down a commit hash and avoid spamming the nightlies with bundles. But we can switch to daily builds too if we prefer that. THe zip size is about 5MB large._
    
    **Important Note**: This PR introduces a new flag for building the UI module from source, replacing the previously configured `production` flag in `gradle.properties`. The flag defaults to `false` requiring the user to either set it to `true` or provide a commit hash for download wasmJs. The PR uses the hash from the PR branch, which will no longer match a commit hash on main once this PR is squashed and merged.
    
    See discussion https://lists.apache.org/thread/onc0zyyp8tjoj3fdg3bmpnh9cqsgjhy6
    
    (cherry picked from commit c78939a620be2f250f59a8ab7244f6953c5c024c)

---
+++ ui/webApp/build.gradle.kts
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.gradle.api.tasks.bundling.Zip
+import java.security.MessageDigest
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+//    js {
+//        browser()
+//        binaries.executable()
+//    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            commonWebpackConfig {
+                outputFileName = "webApp.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    // Serve sources to debug inside browser
+                    static(directory = project.projectDir.path)
+                }
+                // Note that webpack.config.d/ contains additional configuration
+            }
+            testTask {
+                // Explicitly disable the wasmJs browser tests, as we do not have the suitable
+                // environments right now (running only tests for JVM)
+                enabled = false
+            }
+        }
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.shared)
+            implementation(libs.kotlinx.serialization.json)
+
+            implementation(libs.compose.ui)
+
+            implementation(project.dependencies.platform(libs.ktor.bom))
+            implementation(libs.ktor.client.core)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.decompose.decompose)
+            implementation(libs.mvikotlin.main)
+            implementation(libs.mvikotlin.mvikotlin)
+        }
+    }
+}
+
+// package as a plain zip and upload to nightlies by CI
+val wasmJsBrowserDistribution = tasks.named("wasmJsBrowserDistribution")
+
+val zipWasmDist = tasks.register<Zip>("zipWasmDist") {
+  dependsOn(wasmJsBrowserDistribution)
+  from(wasmJsBrowserDistribution.map { it.outputs.files.singleFile })
+  archiveFileName.set("solr-ui-wasm.zip")
+  destinationDirectory.set(layout.buildDirectory.dir("dist"))
+  finalizedBy("checksumWasmDist")
+}
+
+tasks.register("checksumWasmDist") {
+  dependsOn(zipWasmDist)
+
+  val archiveFile = zipWasmDist.flatMap { it.archiveFile }
+  val checksumFile = archiveFile.map { it.asFile.parentFile.resolve("${it.asFile.name}.sha256") }
+
+  inputs.file(archiveFile)
+  outputs.file(checksumFile)
+
+  doLast {
+    val file = archiveFile.get().asFile
+    val checksum = MessageDigest.getInstance("SHA-256")
+      .digest(file.readBytes())
+      .joinToString("") { b -> "%02x".format(b) }
+    checksumFile.get().writeText(checksum)
+  }
+}
